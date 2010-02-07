9c49c15053b9fe529b7a1242d030776fbb1474a8
Liu Liu <liuliu.1987@gmail.com>
2010-02-07 13:50:10 -0500

    new file:   ccv_util.c

---
+++ src/ccv_util.c
@@ -0,0 +1,13 @@
+#include "ccv.h"
+
+ccv_dense_matrix_t* ccv_get_dense_matrix(ccv_matrix_t* mat)
+{
+}
+
+ccv_sparse_matrix_t* ccv_get_sparse_matrix(ccv_matrix_t* mat)
+{
+}
+
+int ccv_matrix_assert(ccv_matrix_t* mat, int type, int rows_lt = 0, int rows_gt = 0, int cols_lt = 0, int cols_gt = 0)
+{
+}
