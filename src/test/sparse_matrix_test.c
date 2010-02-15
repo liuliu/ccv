627e236a8b5654a458725bc3c7a555a9c0b27080
Liu Liu <liuliu.1987@gmail.com>
2010-02-15 01:19:41 -0500

    modified:   sparse_matrix_test.c

--- src/test/sparse_matrix_test.c
+++ src/test/sparse_matrix_test.c
@@ -30,5 +30,28 @@ int main(int argc, char** argv)
 			}
 	}
 	ccv_matrix_free(mat);
+/*	similar OpenCV code for sparse matrix, little slower than my version
+	int sizes[] = {1000, 1000};
+	CvSparseMat* mat = cvCreateSparseMat(2, sizes, CV_32SC1);
+	int k = 0;
+	for (int i = 0; i < 1000; i++)
+		for (int j = 0; j < 1000; j++)
+		{
+			cvSetReal2D(mat, i, j, k);
+			k++;
+		}
+	for (int n = 0; n < 100; n++)
+	{
+		k = 0;
+		for (int i = 0; i < 1000; i++)
+			for (int j = 0; j < 1000; j++)
+			{
+				double u = cvGetReal2D(mat, i, j);
+				if (fabs(k - u) > 1e-6)
+					printf("ERROR: %d %d %d\n", i, j, n);
+				k++;
+			}
+	}
+*/
 	return 0;
 }
