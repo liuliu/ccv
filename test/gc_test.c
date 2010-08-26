f708c77f0edffd4bd246dc579bea77de20ba283e
Liu Liu <liuliu.1987@gmail.com>
2010-08-26 11:45:40 +0800

    added the gc_test

---
+++ test/gc_test.c
@@ -0,0 +1,25 @@
+#include "ccv.h"
+#include <assert.h>
+
+int main(int argc, char** argv)
+{
+	int i;
+	for (i = 0; i < 50000; i++)
+	{
+		ccv_dense_matrix_t* dmt = ccv_dense_matrix_new(1, 1, CCV_32S | CCV_C1, NULL, NULL);
+		dmt->data.i[0] = i;
+		ccv_matrix_generate_signature((const char*)&i, 4, dmt->sig, NULL);
+		ccv_matrix_free(dmt);
+	}
+	// ccv_garbage_collect();
+	for (i = 0; i < 50000; i++)
+	{
+		int sig[5];
+		ccv_matrix_generate_signature((const char*)&i, 4, sig, NULL);
+		ccv_dense_matrix_t* dmt = ccv_dense_matrix_new(1, 1, CCV_32S | CCV_C1, NULL, sig);
+		assert(i == dmt->data.i[0]);
+		ccv_matrix_free(dmt);
+	}
+	ccv_garbage_collect();
+	return 0;
+}
