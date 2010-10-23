8c3b01238a7781bbc08866b86173fcec15f13d7e
Liu Liu <liuliu.1987@gmail.com>
2010-10-23 05:27:58 -0400

    if homest doesn't exist, just draw the connection lines

--- bin/siftdraw.rb
+++ bin/siftdraw.rb
@@ -5,6 +5,7 @@ matches = File.new("/tmp/matches.txt", "w+") if ARGV.length == 4
 
 object_size = Array.new
 image_size = Array.new
+pairs = Array.new
 
 STDIN.each_line do |line|
 	print line
@@ -13,10 +14,12 @@ STDIN.each_line do |line|
 	if args[0].include? "x"
 		object_size = args[0].split("x")
 		object_size = {:width => object_size[0].to_i, :height => object_size[1].to_i}
-		image_size = args[1].split("x")
+		image_size = args[2].split("x")
 		image_size = {:width => image_size[0].to_i, :height => image_size[1].to_i}
 	else
 		if matches.nil?
+			pairs.push({:object => {:x => args[0].to_f, :y => args[1].to_f},
+						:image => {:x => args[3].to_f, :y => args[4].to_f}})
 		else
 			matches.puts args[0] + " " + args[1] + " " + args[3] + " " + args[4] + "\n"
 		end
@@ -24,6 +27,13 @@ STDIN.each_line do |line|
 end
 
 if matches.nil?
+	%x[#{sprintf("convert %s -extent %dx%d %s", ARGV[0], [object_size[:width], image_size[:width]].max, object_size[:height] + image_size[:height], ARGV[2])}]
+	%x[#{sprintf("composite -gravity southWest %s %s %s", ARGV[1], ARGV[2], ARGV[2])}]
+	lines = ""
+	pairs.each do |pair|
+		lines += sprintf("-draw \"line %d,%d,%d,%d\" ", pair[:object][:x], pair[:object][:y], pair[:image][:x], pair[:image][:y] + object_size[:height])
+	end
+	%x[convert #{ARGV[2]} -stroke red #{lines}#{ARGV[2]}]
 else
 	matches.close
 	output = %x[#{ARGV[3] + " /tmp/matches.txt"}]
