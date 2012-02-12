00da73dd42ccb6dd6fca7bd403597df9fe258311
Max Maischein <corion@corion.net>
2012-02-12 18:31:39 +0100

    Now "dmake -f Makefile.mk all" also works

--- bin/Makefile.mk
+++ bin/Makefile.mk
@@ -1,8 +1,7 @@
-CC = gcc
+CC       = gcc
 CXXFLAGS = -mno-cygwin -O3 -msse2 -Wall -std=c99 -fms-extensions -pedantic -I"../lib"
-LDFLAGS= -L"../lib" -L"/strawberry/c/lib" -lccv -ljpeg -lpng -lz -lm -lws2_32
+LDFLAGS  = -L"../lib" -L"/strawberry/c/lib" -lccv -ljpeg -lpng -lz -lm -lws2_32
 
-_EXE = .exe
 o = .o
 a = .a
 
@@ -10,25 +9,25 @@ RM_F = del
 
 .SUFFIXES : .c .i $(o) .dll $(a) .exe .rc .res
 
+.PHONY : all .exe
 
-TARGETS = bbffmt siftmatch bbfcreate bbfdetect swtdetect swtcreate dpmdetect convert
+TARGETS = bbffmt siftmatch bbfcreate bbfdetect swtdetect swtcreate dpmdetect
+
+# Currently broken
+# convert
 
 all: ../lib/libccv.a $(TARGETS)
 
 clean:
 	$(RM_F) *$o ../lib/*$o ../lib/3rdparty/*$o ../lib/libccv$a $(TARGETS)
 
-$(TARGETS) : $@$_EXE
+$(TARGETS) : $(TARGETS:+".exe")
+	+@echo.
 
-%.exe : %$o '../lib/libccv.a'
+%.exe : %.o '../lib/libccv.a'
 	$(CC) $< -o $@ $(LDFLAGS) 
 
-#$(TARGETS) : $@$(_OBJ) '../lib/libccv.a'
-#	echo exe
-#	$(CC) $(LDFLAGS) -o $@$(_EXE) $<
-	
-
-../lib/libccv.a:
+../lib/libccv.a: ../lib/ccv.h
 	+cd ..\lib && $(MAKE) libccv.a
 
 %.o: %.c '../lib/ccv.h'
