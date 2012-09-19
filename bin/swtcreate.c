e168b8271bc465536ec4f0365119c4640562bb34
Liu Liu <i@liuliu.me>
2012-09-18 23:57:01 -0400

    changed precision / recall measure to DetEval one

--- bin/swtcreate.c
+++ bin/swtcreate.c
@@ -2,12 +2,53 @@
 #include "ccv_internal.h"
 #include <sys/time.h>
 #include <ctype.h>
+#include <getopt.h>
 
-// compute f-rate of swt
+static void exit_with_help()
+{
+	printf(
+	"\n  \033[1mUSAGE\033[0m\n\n    swtreate input-text [OPTION...]\n\n"
+	"  \033[1mARGUMENTS\033[0m\n\n"
+	"    input-text: text file contains a list of text locations in format:\n"
+	"                <file name>\\newline\n"
+	"                x y width height <of one text location>\\newline\n"
+	"                x y width height <of another text location>\\newline\n\n"
+	"  \033[1mOPTIONS\033[0m\n\n"
+	"    --size : the window size of canny filter [DEFAULT TO 1,2,3]\n"
+	"    --low-thresh : the low threshold of canny filter [DEFAULT TO 50,1,150]\n"
+	"    --high-thresh : the high threshold of canny filter [DEFAULT TO 200,1,350]\n"
+	"    --max-height : the maximal height of letter [DEFAULT TO 500,1,500]\n"
+	"    --min-height : the minimal height of letter [DEFAULT TO 5,1,30]\n"
+	"    --min-area : the minimal area of letter [DEFAULT TO 10,1,100]\n"
+	"    --aspect-ratio : the aspect ratio of letter [DEFAULT TO 5,1,15]\n"
+	"    --std-ratio : the maximal std to mean ratio of letter [DEFAULT TO 0.1,0.01,1.0]\n"
+	"    --thickness-ratio : the maximal allowance of thickness change between two letters [DEFAULT TO 1,0.1,2]\n"
+	"    --height-ratio : the maximal allowance of height change between two letters [DEFAULT TO 1,0.1,3]\n"
+	"    --intensity-thresh : how much intensity tolerance between two letters [DEFAULT TO 1,1,50]\n"
+	"    --letter-occlude-thresh : how many letters one letter rectangle can occlude [DEFAULT TO 0,1,5]\n"
+	"    --distance-ratio : the distance between two letters comparing to their width [DEFAULT TO 1,0.1,5]\n"
+	"    --intersect-ratio : how much in the y-axis two letters intersect with each other [DEFAULT TO 0,0.1,5]\n"
+	"    --letter-thresh : how many letters in minimal should one text line contains [DEFAULT TO 0,1,5]\n"
+	"    --elongate-ratio : what's the minimal ratio between text line's width and height [DEFAULT TO 0.1,0.1,2.5]\n"
+	"    --breakdown-ratio : what's the ratio for text line to break down into words [DEFAULT TO 0.5,0.01,1.5]\n"
+	"    --breakdown : support to break text lines down to words [DEFAULT TO 1]\n"
+	"    --iterations : how many iterations for the search [DEFAULT TO 10]\n"
+	"    --base-dir : change the base directory so that the program can read images from there\n"
+	);
+	exit(-1);
+}
+
+static double one_g = 0.8;
+static double one_d = 0.4;
+static double om_one = 0.8;
+static double center_diff_thr = 1.0;
+
+// compute hmean of swt
 static double _ccv_evaluate_swt(ccv_array_t* images, ccv_array_t* gt, double a, ccv_swt_param_t params, double* precision, double* recall)
 {
 	int i, j, k;
-	double total_f = 0, total_precision = 0, total_recall = 0;
+	int total_detected = 0, total_truth = 0;
+	double total_precision = 0, total_recall = 0;
 	for (i = 0; i < images->rnum; i++)
 	{
 		char* name = *(char**)ccv_array_get(images, i);
@@ -16,61 +57,392 @@ static double _ccv_evaluate_swt(ccv_array_t* images, ccv_array_t* gt, double a, 
 		ccv_read(name, &image, CCV_IO_GRAY | CCV_IO_ANY_FILE);
 		ccv_array_t* words = ccv_swt_detect_words(image, params);
 		ccv_matrix_free(image);
-		double f = 0, precision = 0, recall = 0;
-		for (j = 0; j < words->rnum; j++)
+		if (words->rnum == 0 || truth->rnum == 0)
 		{
-			ccv_rect_t* estimate = (ccv_rect_t*)ccv_array_get(words, j);
-			int match = 0;
-			for (k = 0; k < truth->rnum; k++)
-			{
-				ccv_rect_t* target = (ccv_rect_t*)ccv_array_get(truth, k);
-				match = ccv_max(match, ccv_max(ccv_min(target->x + target->width, estimate->x + estimate->width) - ccv_max(target->x, estimate->x), 0) * ccv_max(ccv_min(target->y + target->height, estimate->y + estimate->height) - ccv_max(target->y, estimate->y), 0));
-			}
-			precision += (double)match / (double)(estimate->width * estimate->height);
+			ccv_array_free(words);
+			continue;
 		}
-		if (words->rnum > 0)
-			precision /= words->rnum;
+		total_detected += words->rnum;
+		total_truth += truth->rnum;
+		int* cG = (int*)ccmalloc(sizeof(int) * truth->rnum);
+		int* cD = (int*)ccmalloc(sizeof(int) * words->rnum);
+		memset(cG, 0, sizeof(int) * truth->rnum);
+		memset(cD, 0, sizeof(int) * words->rnum);
+		double* mG = (double*)ccmalloc(sizeof(double) * truth->rnum * words->rnum);
+		double* mD = (double*)ccmalloc(sizeof(double) * truth->rnum * words->rnum);
+		memset(mG, 0, sizeof(double) * truth->rnum * words->rnum);
+		memset(mD, 0, sizeof(double) * truth->rnum * words->rnum);
 		for (j = 0; j < truth->rnum; j++)
 		{
-			ccv_rect_t* target = (ccv_rect_t*)ccv_array_get(truth, j);
-			int match = 0;
+			ccv_rect_t* rect = (ccv_rect_t*)ccv_array_get(truth, j);
 			for (k = 0; k < words->rnum; k++)
 			{
-				ccv_rect_t* estimate = (ccv_rect_t*)ccv_array_get(words, k);
-				match = ccv_max(match, ccv_max(ccv_min(target->x + target->width, estimate->x + estimate->width) - ccv_max(target->x, estimate->x), 0) * ccv_max(ccv_min(target->y + target->height, estimate->y + estimate->height) - ccv_max(target->y, estimate->y), 0));
+				ccv_rect_t* target = (ccv_rect_t*)ccv_array_get(words, k);
+				int match = ccv_max(ccv_min(target->x + target->width, rect->x + rect->width) - ccv_max(target->x, rect->x), 0) * ccv_max(ccv_min(target->y + target->height, rect->y + rect->height) - ccv_max(target->y, rect->y), 0);
+				if (match > 0)
+				{
+					mG[j * words->rnum + k] = (double)match / (double)(rect->width * rect->height);
+					mD[k * truth->rnum + j] = (double)match / (double)(target->width * target->height);
+					++cG[j];
+					++cD[k];
+				}
 			}
-			recall += (double)match / (double)(target->width * target->height);
+		}
+		unsigned char* tG = (unsigned char*)ccmalloc(truth->rnum);
+		unsigned char* tD = (unsigned char*)ccmalloc(words->rnum);
+		memset(tG, 0, truth->rnum);
+		memset(tD, 0, words->rnum);
+		// one to one match
+		for (j = 0; j < truth->rnum; j++)
+		{
+			if (cG[j] != 1)
+				continue;
+			ccv_rect_t* rect = (ccv_rect_t*)ccv_array_get(truth, j);
+			for (k = 0; k < words->rnum; k++)
+			{
+				if (cD[j] != 1)
+					continue;
+				ccv_rect_t* target = (ccv_rect_t*)ccv_array_get(words, k);
+				if (mG[j * words->rnum + k] >= one_g && mD[k * truth->rnum + j] >= one_d)
+				{
+					double dx = (target->x + target->width * 0.5) - (rect->x + rect->width * 0.5);
+					double dy = (target->y + target->height * 0.5) - (rect->y + rect->height * 0.5);
+					double d = sqrt(dx * dx + dy * dy) * 2.0 / (sqrt(target->width * target->width + target->height * target->height) + sqrt(rect->width * rect->width + rect->height * rect->height));
+					if (d < center_diff_thr)
+					{
+						total_recall += 1.0;
+						total_precision += 1.0;
+						tG[j] = tD[k] = 1;
+					}
+				}
+			}
+		}
+		int* many = (int*)ccmalloc(sizeof(int) * ccv_max(words->rnum, truth->rnum));
+		// one to many match, starts with ground truth
+		for (j = 0; j < truth->rnum; j++)
+		{
+			if (tG[j])
+				continue;
+			double one_sum = 0;
+			int no_many = 0;
+			for (k = 0; k < words->rnum; k++)
+			{
+				if (tD[k])
+					continue;
+				double many_single = mD[k * truth->rnum + j];
+				if (many_single >= one_d)
+				{
+					one_sum += mG[j * words->rnum + k];
+					many[no_many] = k;
+					++no_many;
+				}
+			}
+			if (no_many == 1)
+			{
+				// degrade to one to one match
+				if (mG[j * words->rnum + many[0]] >= one_g && mD[many[0] * truth->rnum + j] >= one_d)
+				{
+					total_recall += 1.0;
+					total_precision += 1.0;
+					tG[j] = tD[many[0]] = 1;
+				}
+			} else if (one_sum >= one_g) {
+				for (k = 0; k < no_many; k++)
+					tD[many[k]] = 1;
+				total_recall += om_one;
+				total_precision += om_one * no_many;
+			}
+		}
+		// one to many match, with estimate
+		for (k = 0; k < words->rnum; k++)
+		{
+			if (tD[k])
+				continue;
+			double one_sum = 0;
+			int no_many = 0;
+			for (j = 0; j < truth->rnum; j++)
+			{
+				if (tG[j])
+					continue;
+				double many_single = mG[j * words->rnum + k];
+				if (many_single >= one_g)
+				{
+					one_sum += mD[k * truth->rnum + j];
+					many[no_many] = j;
+					++no_many;
+				}
+			}
+			if (no_many == 1)
+			{
+				// degrade to one to one match
+				if (mG[many[0] * words->rnum + k] >= one_g && mD[k * truth->rnum + many[0]] >= one_d)
+				{
+					total_recall += 1.0;
+					total_precision += 1.0;
+					tG[many[0]] = tD[k] = 1;
+				}
+			} else if (one_sum >= one_g) {
+				for (j = 0; j < no_many; j++)
+					tG[many[j]] = 1;
+				total_recall += om_one * no_many;
+				total_precision += om_one;
+			}
 		}
 		ccv_array_free(words);
-		if (truth->rnum > 0)
-			recall /= truth->rnum;
-		if (precision > 0 && recall > 0)
-			f = 1 / (a / precision + (1 - a) / recall);
-		total_f += f;
-		total_precision += precision;
-		total_recall += recall;
+		ccfree(many);
+		ccfree(tG);
+		ccfree(tD);
+		ccfree(cG);
+		ccfree(cD);
+		ccfree(mG);
+		ccfree(mD);
 	}
-	total_f /= images->rnum;
-	total_precision /= images->rnum;
-	total_recall /= images->rnum;
+	total_precision /= total_detected;
+	total_recall /= total_truth;
 	if (precision)
 		*precision = total_precision;
 	if (recall)
 		*recall = total_recall;
-	return total_f;
+	return 1.0 / (a / total_precision + (1.0 - a) / total_recall);
 }
 
 typedef struct {
 	double min_value;
 	double max_value;
 	double step;
+	int enable;
 } ccv_swt_range_t;
+
+static void decode_range(const char* arg, ccv_swt_range_t* range)
+{
+}
 
 int main(int argc, char** argv)
 {
+	static struct option swt_options[] = {
+		/* help */
+		{"help", 0, 0, 0},
+		/* optional parameters */
+		{"size", 1, 0, 0},
+		{"low-thresh", 1, 0, 0},
+		{"high-thresh", 1, 0, 0},
+		{"max-height", 1, 0, 0},
+		{"min-height", 1, 0, 0},
+		{"min-area", 1, 0, 0},
+		{"aspect-ratio", 1, 0, 0},
+		{"std-ratio", 1, 0, 0},
+		{"thickness-ratio", 1, 0, 0},
+		{"height-ratio", 1, 0, 0},
+		{"intensity-thresh", 1, 0, 0},
+		{"letter-occlude-thresh", 1, 0, 0},
+		{"distance-ratio", 1, 0, 0},
+		{"intersect-ratio", 1, 0, 0},
+		{"letter-thresh", 1, 0, 0},
+		{"elongate-ratio", 1, 0, 0},
+		{"breakdown-ratio", 1, 0, 0},
+		{"breakdown", 1, 0, 0},
+		{"iterations", 1, 0, 0},
+		{"base-dir", 1, 0, 0},
+		{0, 0, 0, 0}
+	};
+	if (argc <= 1)
+		exit_with_help();
+	ccv_swt_param_t params = {
+		.interval = 5,
+		.same_word_thresh = { 0.5, 0.9 },
+		.min_neighbors = 2,
+		.scale_invariant = 1,
+		.size = 3,
+		.low_thresh = 65,
+		.high_thresh = 212,
+		.max_height = 500,
+		.min_height = 14,
+		.min_area = 94,
+		.letter_occlude_thresh = 4,
+		.aspect_ratio = 9,
+		.std_ratio = 0.95,
+		.thickness_ratio = 1.7,
+		.height_ratio = 1.8,
+		.intensity_thresh = 34,
+		.distance_ratio = 3.3,
+		.intersect_ratio = 1.2,
+		.letter_thresh = 4,
+		.elongate_ratio = 2.4,
+		.breakdown = 1,
+		.breakdown_ratio = 0.82,
+	};
+	ccv_swt_range_t size_range = {
+		.min_value = 1,
+		.max_value = 3,
+		.step = 2,
+		.enable = 1,
+	};
+	ccv_swt_range_t low_thresh_range = {
+		.min_value = 50,
+		.max_value = 150,
+		.step = 1,
+		.enable = 1,
+	};
+	ccv_swt_range_t high_thresh_range = {
+		.min_value = 200,
+		.max_value = 350,
+		.step = 1,
+		.enable = 1,
+	};
+	ccv_swt_range_t max_height_range = {
+		.min_value = 500,
+		.max_value = 500,
+		.step = 1,
+		.enable = 1,
+	};
+	ccv_swt_range_t min_height_range = {
+		.min_value = 5,
+		.max_value = 30,
+		.step = 1,
+		.enable = 1,
+	};
+	ccv_swt_range_t min_area_range = {
+		.min_value = 10,
+		.max_value = 100,
+		.step = 1,
+		.enable = 1,
+	};
+	ccv_swt_range_t letter_occlude_thresh_range = {
+		.min_value = 0,
+		.max_value = 5,
+		.step = 1,
+		.enable = 1,
+	};
+	ccv_swt_range_t aspect_ratio_range = {
+		.min_value = 5,
+		.max_value = 15,
+		.step = 1,
+		.enable = 1,
+	};
+	ccv_swt_range_t std_ratio_range = {
+		.min_value = 0.1,
+		.max_value = 1.0,
+		.step = 0.01,
+		.enable = 1,
+	};
+	ccv_swt_range_t thickness_ratio_range = {
+		.min_value = 1.0,
+		.max_value = 2.0,
+		.step = 0.1,
+		.enable = 1,
+	};
+	ccv_swt_range_t height_ratio_range = {
+		.min_value = 1.0,
+		.max_value = 3.0,
+		.step = 0.1,
+		.enable = 1,
+	};
+	ccv_swt_range_t intensity_thresh_range = {
+		.min_value = 1,
+		.max_value = 50,
+		.step = 1,
+		.enable = 1,
+	};
+	ccv_swt_range_t distance_ratio_range = {
+		.min_value = 1.0,
+		.max_value = 5.0,
+		.step = 0.1,
+		.enable = 1,
+	};
+	ccv_swt_range_t intersect_ratio_range = {
+		.min_value = 0.0,
+		.max_value = 5.0,
+		.step = 0.1,
+		.enable = 1,
+	};
+	ccv_swt_range_t letter_thresh_range = {
+		.min_value = 0,
+		.max_value = 5,
+		.step = 1,
+		.enable = 1,
+	};
+	ccv_swt_range_t elongate_ratio_range = {
+		.min_value = 0.1,
+		.max_value = 2.5,
+		.step = 0.1,
+		.enable = 1,
+	};
+	ccv_swt_range_t breakdown_ratio_range = {
+		.min_value = 0.5,
+		.max_value = 1.5,
+		.step = 0.01,
+		.enable = 1,
+	};
+	int i, k, iterations = 10;
+	while (getopt_long_only(argc - 1, argv + 1, "", swt_options, &k) != -1)
+	{
+		switch (k)
+		{
+			case 0:
+				exit_with_help();
+			case 1:
+				decode_range(optarg, &size_range);
+				break;
+			case 2:
+				decode_range(optarg, &low_thresh_range);
+				break;
+			case 3:
+				decode_range(optarg, &high_thresh_range);
+				break;
+			case 4:
+				decode_range(optarg, &max_height_range);
+				break;
+			case 5:
+				decode_range(optarg, &min_height_range);
+				break;
+			case 6:
+				decode_range(optarg, &min_area_range);
+				break;
+			case 7:
+				decode_range(optarg, &aspect_ratio_range);
+				break;
+			case 8:
+				decode_range(optarg, &std_ratio_range);
+				break;
+			case 9:
+				decode_range(optarg, &thickness_ratio_range);
+				break;
+			case 10:
+				decode_range(optarg, &height_ratio_range);
+				break;
+			case 11:
+				decode_range(optarg, &intensity_thresh_range);
+				break;
+			case 12:
+				decode_range(optarg, &letter_occlude_thresh_range);
+				break;
+			case 13:
+				decode_range(optarg, &distance_ratio_range);
+				break;
+			case 14:
+				decode_range(optarg, &intersect_ratio_range);
+				break;
+			case 15:
+				decode_range(optarg, &letter_thresh_range);
+				break;
+			case 16:
+				decode_range(optarg, &elongate_ratio_range);
+				break;
+			case 17:
+				decode_range(optarg, &breakdown_ratio_range);
+				break;
+			case 18:
+				params.breakdown = !!atoi(optarg);
+				break;
+			case 19:
+				iterations = atoi(optarg);
+				break;
+			case 20:
+				chdir(optarg);
+				break;
+		}
+	}
 	FILE* r = fopen(argv[1], "rt");
-	if (argc == 3)
-		chdir(argv[2]);
 	ccv_enable_cache(1024 * 1024 * 1024);
 	ccv_array_t* aof = ccv_array_new(sizeof(char*), 64, 0);
 	ccv_array_t* aow = ccv_array_new(sizeof(ccv_array_t*), 64, 0);
@@ -104,139 +476,31 @@ int main(int argc, char** argv)
 	}
 	free(file);
 	printf("loaded %d images for parameter search\n", aof->rnum);
-	int i;
-	ccv_swt_param_t params = {
-		.interval = 0,
-		.same_word_thresh = { 0.5, 0.9 },
-		.min_neighbors = 0,
-		.scale_invariant = 0,
-		.size = 3,
-		.low_thresh = 74,
-		.high_thresh = 212,
-		.max_height = 500,
-		.min_height = 12,
-		.min_area = 95,
-		.letter_occlude_thresh = 4,
-		.aspect_ratio = 6,
-		.std_ratio = 0.78,
-		.thickness_ratio = 1.8,
-		.height_ratio = 2.0,
-		.intensity_thresh = 33,
-		.distance_ratio = 3.4,
-		.intersect_ratio = 1.2,
-		.letter_thresh = 3,
-		.elongate_ratio = 1.3,
-		.breakdown = 1,
-		.breakdown_ratio = 1.0,
-	};
-	ccv_swt_range_t size_range = {
-		.min_value = 1,
-		.max_value = 3,
-		.step = 2,
-	};
-	ccv_swt_range_t low_thresh_range = {
-		.min_value = 50,
-		.max_value = 150,
-		.step = 1,
-	};
-	ccv_swt_range_t high_thresh_range = {
-		.min_value = 200,
-		.max_value = 350,
-		.step = 1,
-	};
-	ccv_swt_range_t max_height_range = {
-		.min_value = 500,
-		.max_value = 500,
-		.step = 1,
-	};
-	ccv_swt_range_t min_height_range = {
-		.min_value = 5,
-		.max_value = 30,
-		.step = 1,
-	};
-	ccv_swt_range_t letter_occlude_thresh_range = {
-		.min_value = 0,
-		.max_value = 5,
-		.step = 1,
-	};
-	ccv_swt_range_t min_area_range = {
-		.min_value = 10,
-		.max_value = 100,
-		.step = 1,
-	};
-	ccv_swt_range_t aspect_ratio_range = {
-		.min_value = 5,
-		.max_value = 15,
-		.step = 1,
-	};
-	ccv_swt_range_t std_ratio_range = {
-		.min_value = 0.1,
-		.max_value = 1.0,
-		.step = 0.01,
-	};
-	ccv_swt_range_t thickness_ratio_range = {
-		.min_value = 1.0,
-		.max_value = 2.0,
-		.step = 0.1,
-	};
-	ccv_swt_range_t height_ratio_range = {
-		.min_value = 1.0,
-		.max_value = 3.0,
-		.step = 0.1,
-	};
-	ccv_swt_range_t intensity_thresh_range = {
-		.min_value = 1,
-		.max_value = 50,
-		.step = 1,
-	};
-	ccv_swt_range_t distance_ratio_range = {
-		.min_value = 1.0,
-		.max_value = 5.0,
-		.step = 0.1,
-	};
-	ccv_swt_range_t intersect_ratio_range = {
-		.min_value = 0.0,
-		.max_value = 5.0,
-		.step = 0.1,
-	};
-	ccv_swt_range_t letter_thresh_range = {
-		.min_value = 0,
-		.max_value = 5,
-		.step = 1,
-	};
-	ccv_swt_range_t elongate_ratio_range = {
-		.min_value = 0.1,
-		.max_value = 2.5,
-		.step = 0.1,
-	};
-	ccv_swt_range_t breakdown_ratio_range = {
-		.min_value = 0.5,
-		.max_value = 1.5,
-		.step = 0.01,
-	};
 	double best_f = 0, best_precision = 0, best_recall = 0;
 	double a = 0.5;
 	double v;
 	ccv_swt_param_t best_params = params;
 #define optimize(parameter, type, rounding) \
-	params = best_params; \
-	for (v = parameter##_range.min_value; v <= parameter##_range.max_value; v += parameter##_range.step) \
+	if (parameter##_range.enable) \
 	{ \
-		params.parameter = (type)(v + rounding); \
-		double f, recall, precision; \
-		f = _ccv_evaluate_swt(aof, aow, a, params, &precision, &recall); \
-		if (f > best_f) \
+		params = best_params; \
+		for (v = parameter##_range.min_value; v <= parameter##_range.max_value; v += parameter##_range.step) \
 		{ \
-			best_params = params; \
-			best_f = f; \
-			best_precision = precision; \
-			best_recall = recall; \
+			params.parameter = (type)(v + rounding); \
+			double f, recall, precision; \
+			f = _ccv_evaluate_swt(aof, aow, a, params, &precision, &recall); \
+			if (f > best_f) \
+			{ \
+				best_params = params; \
+				best_f = f; \
+				best_precision = precision; \
+				best_recall = recall; \
+			} \
+			FLUSH("current hmean : %.2lf%%, precision : %.2lf%%, recall : %.2lf%% ; best hmean : %.2lf%%, precision : %.2lf%%, recall : %.2lf%% ; at " #parameter " = %lg (%lg <- [%lg, %lg])", f * 100, precision * 100, recall * 100, best_f * 100, best_precision * 100, best_recall * 100, (double)best_params.parameter, v, parameter##_range.min_value, parameter##_range.max_value); \
 		} \
-		FLUSH("current f : %.2lf%%, precision : %.2lf%%, recall : %.2lf%% ; best f : %.2lf%%, precision : %.2lf%%, recall : %.2lf%% ; at " #parameter " = %lg (%lg <- [%lg, %lg])", f * 100, precision * 100, recall * 100, best_f * 100, best_precision * 100, best_recall * 100, (double)best_params.parameter, v, parameter##_range.min_value, parameter##_range.max_value); \
-	} \
-	printf("\n");
-	int max_round = 10;
-	for (i = 0; i < max_round; i++)
+		printf("\n"); \
+	}
+	for (i = 0; i < iterations; i++)
 	{
 		optimize(size, int, 0.5);
 		optimize(low_thresh, int, 0.5);
@@ -255,7 +519,7 @@ int main(int argc, char** argv)
 		optimize(letter_thresh, int, 0.5);
 		optimize(elongate_ratio, double, 0);
 		optimize(breakdown_ratio, double, 0);
-		printf("At round %d(of %d) : best parameters for swt is:\n"
+		printf("At iteration %d(of %d) : best parameters for swt is:\n"
 			   "\tsize = %d\n"
 			   "\tlow_thresh = %d\n"
 			   "\thigh_thresh = %d\n"
@@ -273,7 +537,7 @@ int main(int argc, char** argv)
 			   "\tletter_thresh = %d\n"
 			   "\telongate_ratio = %lf\n"
 			   "\tbreakdown_ratio = %lf\n",
-			   i + 1, max_round,
+			   i + 1, iterations,
 			   best_params.size,
 			   best_params.low_thresh,
 			   best_params.high_thresh,
--- bin/swtvldtr.rb
+++ bin/swtvldtr.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby
 
+# to make a better roc graph, I uses what DetEval do for precision and recall computation
+
 exit unless ARGV.length == 2
+
+one_g = 0.8
+one_d = 0.4
+om_one = 0.8
+center_diff_thr = 1.0
 
 truth = Hash.new
 file = File.new(ARGV[0])
@@ -27,34 +34,118 @@ file.each do |line|
 	end
 end
 
-total = 0
-precision = 0
-estimate.each do |fn, rects|
-	rects.each do |rect|
-		match = 0
-		next unless estimate.has_key? fn
-		truth[fn].each do |target|
-			match = [[[target[:x] + target[:width], rect[:x] + rect[:width]].min - [target[:x], rect[:x]].max, 0].max * [[target[:y] + target[:height], rect[:y] + rect[:height]].min - [target[:y], rect[:y]].max, 0].max, match].max
-		end
-		precision += match.to_f / (rect[:width] * rect[:height]).to_f
-		total += 1
-	end
-end
-
-print "precision: " + (((precision.to_f / total.to_f) * 10000).round / 100).to_s + "%\n"
-
-total = 0
 recall = 0
+precision = 0
 truth.each do |fn, rects|
-	rects.each do |rect|
-		match = 0
-		next unless estimate.has_key? fn
-		estimate[fn].each do |target|
-			match = [[[target[:x] + target[:width], rect[:x] + rect[:width]].min - [target[:x], rect[:x]].max, 0].max * [[target[:y] + target[:height], rect[:y] + rect[:height]].min - [target[:y], rect[:y]].max, 0].max, match].max
+	next unless estimate.has_key? fn
+	cG = Array.new rects.count, 0
+	cD = Array.new estimate[fn].count, 0
+	mG = Array.new rects.count do Array.new estimate[fn].count, 0 end
+	mD = Array.new estimate[fn].count do Array.new rects.count, 0 end
+	rects.each_index do |i|
+		rect = rects[i]
+		estimate[fn].each_index do |j|
+			target = estimate[fn][j]
+			match = [[target[:x] + target[:width], rect[:x] + rect[:width]].min - [target[:x], rect[:x]].max, 0].max * [[target[:y] + target[:height], rect[:y] + rect[:height]].min - [target[:y], rect[:y]].max, 0].max
+			if match > 0.0001
+				mG[i][j] = match / (rect[:width] * rect[:height])
+				mD[j][i] = match / (target[:width] * target[:height])
+				cG[i] += 1
+				cD[j] += 1
+			end
 		end
-		recall += match.to_f / (rect[:width] * rect[:height]).to_f
-		total += 1
+	end
+	tG = Array.new rects.count, false
+	tD = Array.new estimate[fn].count, false
+	# one to one match
+	rects.each_index do |i|
+		rect = rects[i]
+		next if cG[i] != 1
+		estimate[fn].each_index do |j|
+			target = estimate[fn][j]
+			next if cD[j] != 1
+			if mG[i][j] >= one_g and mD[j][i] >= one_d
+				dx = (target[:x] + target[:width] * 0.5) - (rect[:x] + rect[:width] * 0.5)
+				dy = (target[:y] + target[:height] * 0.5) - (rect[:y] + rect[:height] * 0.5)
+				d = Math.sqrt(dx**2 + dy**2) * 2.0 / (Math.sqrt(target[:width]**2 + target[:height]**2) + Math.sqrt(rect[:width]**2 + rect[:height]**2))
+				if d < center_diff_thr
+					recall += 1.0
+					precision += 1.0
+					tG[i] = tD[j] = true
+				end
+			end
+		end
+	end
+	# one to many match, starts with ground truth
+	rects.each_index do |i|
+		next if tG[i]
+		one_sum = 0
+		many = Array.new
+		estimate[fn].each_index do |j|
+			next if tD[j]
+			many_single = mD[j][i]
+			if many_single >= one_d
+				one_sum += mG[i][j]
+				many << j
+			end
+		end
+		if many.count == 1
+			# only one qualified, degrade to one to one match
+			if mG[i][many[0]] >= one_g and mD[many[0]][i] >= one_d
+				recall += 1.0
+				precision += 1.0
+				tG[i] = tD[many[0]] = true
+			end
+		elsif one_sum >= one_g
+			many.each do |j|
+				tD[j] = true
+			end
+			recall += om_one
+			precision += om_one * many.count
+		end
+	end
+	# one to many match, with estimate
+	estimate[fn].each_index do |j|
+		next if tD[j]
+		one_sum = 0
+		many = Array.new
+		rects.each_index do |i|
+			next if tG[i]
+			many_single = mG[i][j]
+			if many_single >= one_g
+				one_sum += mD[j][i]
+				many << i
+			end
+		end
+		if many.count == 1
+			# only one qualified, degrade to one to one match
+			if mG[many[0]][j] >= one_g and mD[j][many[0]] >= one_d
+				recall += 1.0
+				precision += 1.0
+				tG[many[0]] = tD[j] = true
+			end
+		elsif one_sum >= one_d
+			many.each do |i|
+				tG[i] = true
+			end
+			recall += om_one * many.count
+			precision += om_one
+		end
 	end
 end
 
-print "recall: " + (((recall.to_f / total.to_f) * 10000).round / 100).to_s + "%\n"
+total = 0
+estimate.each do |fn, rects|
+	total += rects.count
+end
+precision = precision.to_f / total.to_f
+
+total = 0
+truth.each do |fn, rects|
+	total += rects.count
+end
+recall = recall.to_f / total.to_f
+
+print "precision: " + ((precision * 10000).round / 100).to_s + "%\n"
+print "recall: " + ((recall * 10000).round / 100).to_s + "%\n"
+print "hmean: " + ((2.0 * precision * recall / (precision + recall) * 10000).round / 100).to_s + "%\n"
