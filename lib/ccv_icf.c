3676d5bd5456300053b265a342c86730ad676318
Liu Liu <i@liuliu.me>
2012-12-05 01:00:11 -0500

    add ccv_icf to unstable branch

---
+++ lib/ccv_icf.c
@@ -0,0 +1,7 @@
+#include "ccv.h"
+#include "ccv_internal.h"
+
+ccv_array_t* ccv_icf_detect_objects(ccv_dense_matrix_t* a, ccv_icf_classifier_t** classifier, int count, ccv_icf_param_t params)
+{
+	return 0;
+}
