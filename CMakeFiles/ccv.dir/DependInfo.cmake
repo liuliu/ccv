af73e2882053e954811afdb422fbd986f3bdcbfa
gui2 <Gui@guido-pusiols-MacBook-Pro.local>
2014-03-16 09:23:27 -0700

    updates

---
+++ CMakeFiles/ccv.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/Gui/ccv/lib/3rdparty/dsfmt/dSFMT.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/dsfmt/dSFMT.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/kissfft/kiss_fft.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/kissfft/kiss_fft.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/kissfft/kiss_fftnd.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/kissfft/kiss_fftnd.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/kissfft/kiss_fftndr.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/kissfft/kiss_fftndr.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/kissfft/kiss_fftr.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/kissfft/kiss_fftr.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/kissfft/kissf_fft.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/kissfft/kissf_fft.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/kissfft/kissf_fftnd.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/kissfft/kissf_fftnd.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/kissfft/kissf_fftndr.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/kissfft/kissf_fftndr.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/kissfft/kissf_fftr.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/kissfft/kissf_fftr.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/sfmt/SFMT.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/sfmt/SFMT.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/sha1/sha1.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/sha1/sha1.c.o"
+  "/Users/Gui/ccv/lib/3rdparty/sqlite3/sqlite3.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/3rdparty/sqlite3/sqlite3.c.o"
+  "/Users/Gui/ccv/lib/ccv_algebra.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_algebra.c.o"
+  "/Users/Gui/ccv/lib/ccv_basic.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_basic.c.o"
+  "/Users/Gui/ccv/lib/ccv_bbf.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_bbf.c.o"
+  "/Users/Gui/ccv/lib/ccv_cache.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_cache.c.o"
+  "/Users/Gui/ccv/lib/ccv_classic.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_classic.c.o"
+  "/Users/Gui/ccv/lib/ccv_convnet.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_convnet.c.o"
+  "/Users/Gui/ccv/lib/ccv_daisy.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_daisy.c.o"
+  "/Users/Gui/ccv/lib/ccv_dpm.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_dpm.c.o"
+  "/Users/Gui/ccv/lib/ccv_ferns.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_ferns.c.o"
+  "/Users/Gui/ccv/lib/ccv_icf.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_icf.c.o"
+  "/Users/Gui/ccv/lib/ccv_io.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_io.c.o"
+  "/Users/Gui/ccv/lib/ccv_memory.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_memory.c.o"
+  "/Users/Gui/ccv/lib/ccv_mser.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_mser.c.o"
+  "/Users/Gui/ccv/lib/ccv_numeric.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_numeric.c.o"
+  "/Users/Gui/ccv/lib/ccv_resample.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_resample.c.o"
+  "/Users/Gui/ccv/lib/ccv_sift.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_sift.c.o"
+  "/Users/Gui/ccv/lib/ccv_sparse_coding.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_sparse_coding.c.o"
+  "/Users/Gui/ccv/lib/ccv_swt.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_swt.c.o"
+  "/Users/Gui/ccv/lib/ccv_tld.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_tld.c.o"
+  "/Users/Gui/ccv/lib/ccv_transform.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_transform.c.o"
+  "/Users/Gui/ccv/lib/ccv_util.c" "/Users/Gui/ccv/CMakeFiles/ccv.dir/lib/ccv_util.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "HAVE_LIBJPEG"
+  "HAVE_LIBPNG"
+  "HAVE_FFTW3"
+  "HAVE_LIBLINEAR"
+  "HAVE_CBLAS"
+  "HAVE_AVCODEC"
+  "HAVE_AVFORMAT"
+  "HAVE_SWSCALE"
+  "HAVE_SSE2"
+  "USE_DISPATCH"
+  "HAVE_LIBJPEG"
+  "HAVE_LIBPNG"
+  "HAVE_FFTW3"
+  "HAVE_LIBLINEAR"
+  "HAVE_CBLAS"
+  "HAVE_AVCODEC"
+  "HAVE_AVFORMAT"
+  "HAVE_SWSCALE"
+  "HAVE_SSE2"
+  "USE_DISPATCH"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/System/Library/Frameworks/vecLib.framework/Headers"
+  "/opt/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
