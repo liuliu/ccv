98d610b949ce64d5733647859e1c023f4c222c40
Mahmoud Lababidi <lababidi@gmail.com>
2015-04-20 14:06:58 -0400

    set CNNDraw values to original values

--- bin/cnndraw.rb
+++ bin/cnndraw.rb
@@ -13,8 +13,8 @@ words.each_line do |line|
 end
 
 draw = ""
-x = 150
-y = 300
+x = 15
+y = 30
 separation = 50
 bar_width = 150
 bar_height = 16
@@ -27,9 +27,7 @@ STDIN.each_line do |line|
         draw += sprintf("-fill none -strokewidth 1 -stroke DodgerBlue -draw \"rectangle %d,%d,%d,%d\" ", x, y, bar_width+x, y + bar_height)
         draw += sprintf("-fill DodgerBlue -draw \"rectangle %d,%d,%d,%d\" -strokewidth 0 -stroke none ", x, y, frac_bar_width, y + bar_height)
         draw += sprintf("-fill white -pointsize 36 -annotate +%d+%d \"%s\" ", x, y , labels[args[i * 2]])
-		#draw += sprintf("-fill none -strokewidth 1 -stroke DodgerBlue -draw \"rectangle 15,%d,165,%d\" -fill DodgerBlue -draw \"rectangle 15,%d,%d,%d\" -strokewidth 0 -stroke none -fill white -annotate 'text 18,%d \"%s\"' ", y, y + 16, y, (args[i * 2 + 1].to_f * 150).to_i + 15, y + 16, y + 13, labels[args[i * 2]])
 		y += separation
 	end
 end
-print draw
 %x[#{sprintf("convert %s %s%s", ARGV[1], draw, ARGV[2])}]
