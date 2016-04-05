60b1a8ab18303921f5522903478a493146250d8f
Liu Liu <i@liuliu.me>
2016-04-05 01:59:07 -0400

    Add this new file.

---
+++ bin/nnc/conv_opt.c
@@ -0,0 +1,60 @@
+#include <ccv.h>
+#include <nnc/ccv_nnc.h>
+#include <nnc/ccv_nnc_easy.h>
+#include <3rdparty/dsfmt/dSFMT.h>
+#include <sys/time.h>
+#include <ctype.h>
+
+static unsigned int get_current_time(void)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+#define INPUT_DIM (3)
+#define OUTPUT_DIM (128)
+
+#define INPUT_SIZE (224)
+#define OUTPUT_SIZE (112)
+
+#define KERNEL_SIZE (7)
+
+int main(int argc, char** argv)
+{
+	ccv_nnc_init();
+	ccv_nnc_tensor_t* a = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(INPUT_DIM, INPUT_SIZE, INPUT_SIZE), 0);
+	ccv_nnc_tensor_t* b = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(OUTPUT_DIM, OUTPUT_SIZE, OUTPUT_SIZE), 0);
+	ccv_nnc_cmd_t cmd = ccv_nnc_cmd(CCV_NNC_COMPUTE_CONVOLUTIONAL_FORWARD, 0, CMD_CONVOLUTIONAL(OUTPUT_DIM, INPUT_DIM, KERNEL_SIZE, KERNEL_SIZE), 0);
+	ccv_nnc_hint_t hint = ccv_nnc_hint_guess(cmd.info, &a->info, 1, &b->info, 1);
+	ccv_nnc_tensor_t* w = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(INPUT_DIM, KERNEL_SIZE, KERNEL_SIZE, OUTPUT_DIM), 0);
+	ccv_nnc_tensor_t* bias = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(OUTPUT_DIM), 0);
+	// configure the inlets.
+	dsfmt_t dsfmt;
+	dsfmt_init_gen_rand(&dsfmt, 0);
+	int i;
+	for (i = 0; i < INPUT_DIM * KERNEL_SIZE * KERNEL_SIZE * OUTPUT_DIM; i++)
+		w->data.f32[i] = dsfmt_genrand_open_close(&dsfmt) / (INPUT_DIM * KERNEL_SIZE * KERNEL_SIZE);
+	for (i = 0; i < KERNEL_SIZE * KERNEL_SIZE * INPUT_DIM; i++)
+		a->data.f32[i] = dsfmt_genrand_open_close(&dsfmt);
+	for (i = 0; i < OUTPUT_DIM; i++)
+		bias->data.f32[i] = (float)i / OUTPUT_DIM;
+	unsigned int elapsed_time = get_current_time();
+	ccv_nnc_cmd_exec(cmd, hint, 0, TENSOR_LIST(a, w, bias), TENSOR_LIST(b));
+	elapsed_time = get_current_time() - elapsed_time;
+	printf("%u ms for ref\n", elapsed_time);
+	ccv_nnc_tensor_t* c = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(OUTPUT_DIM, OUTPUT_SIZE, OUTPUT_SIZE), 0);
+	cmd.backend = 0; // CCV_NNC_BACKEND_CPU_OPT = 0
+	cmd.algorithm = 0; // CCV_NNC_CMD_OPT_CONV_ALGO_DC
+	elapsed_time = get_current_time();
+	ccv_nnc_cmd_exec(cmd, hint, 0, TENSOR_LIST(a, w, bias), TENSOR_LIST(c));
+	elapsed_time = get_current_time() - elapsed_time;
+	printf("%u ms for optimized\n", elapsed_time);
+	//for (i = 0; i < OUTPUT_DIM * OUTPUT_SIZE * OUTPUT_SIZE; i++)
+	//	printf("%f %f\n", b->data.f32[i], c->data.f32[i]);
+	ccv_nnc_tensor_free(c);
+	ccv_nnc_tensor_free(bias);
+	ccv_nnc_tensor_free(w);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(a);
+}
