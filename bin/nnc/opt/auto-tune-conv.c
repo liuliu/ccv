b257e24f555ecb31decb39f72e810405dc060f65
Liu Liu <i@liuliu.me>
2016-04-13 02:11:48 -0400

    Add missing file.

---
+++ bin/nnc/opt/auto-tune-conv.c
@@ -0,0 +1,62 @@
+#include <ccv.h>
+#include <ccv_internal.h>
+#include <nnc/ccv_nnc.h>
+#include <nnc/ccv_nnc_easy.h>
+#include <3rdparty/dsfmt/dSFMT.h>
+#include <sys/time.h>
+#include <ctype.h>
+
+static unsigned int get_current_time(void)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+#define INPUT_DIM (3)
+#define OUTPUT_DIM (128)
+
+#define INPUT_SIZE (224)
+#define OUTPUT_SIZE (224)
+
+#define KERNEL_SIZE (3)
+
+int main(int argc, char** argv)
+{
+	ccv_nnc_init();
+	ccv_nnc_tensor_t* a = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(INPUT_DIM, INPUT_SIZE, INPUT_SIZE), 0);
+	ccv_nnc_tensor_t* b = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(OUTPUT_DIM, OUTPUT_SIZE, OUTPUT_SIZE), 0);
+	ccv_nnc_cmd_t cmd = ccv_nnc_cmd(CCV_NNC_COMPUTE_CONVOLUTIONAL_FORWARD, 0, CMD_CONVOLUTIONAL(OUTPUT_DIM, INPUT_DIM, KERNEL_SIZE, KERNEL_SIZE), 0);
+	ccv_nnc_hint_t hint = ccv_nnc_hint_guess(cmd.info, &a->info, 1, &b->info, 1);
+	assert(ccv_nnc_hint_verify(hint, cmd.info, a->info, b->info) == 0);
+	ccv_nnc_tensor_t* w = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(INPUT_DIM, KERNEL_SIZE, KERNEL_SIZE, OUTPUT_DIM), 0);
+	ccv_nnc_tensor_t* bias = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(OUTPUT_DIM), 0);
+	// configure the inlets.
+	dsfmt_t dsfmt;
+	dsfmt_init_gen_rand(&dsfmt, 0);
+	int i;
+	for (i = 0; i < INPUT_DIM * KERNEL_SIZE * KERNEL_SIZE * OUTPUT_DIM; i++)
+		w->data.f32[i] = dsfmt_genrand_open_close(&dsfmt) / (INPUT_DIM * KERNEL_SIZE * KERNEL_SIZE);
+	for (i = 0; i < INPUT_SIZE * INPUT_SIZE * INPUT_DIM; i++)
+		a->data.f32[i] = dsfmt_genrand_open_close(&dsfmt);
+	for (i = 0; i < OUTPUT_DIM; i++)
+		bias->data.f32[i] = (float)i / OUTPUT_DIM;
+	unsigned int elapsed_time = get_current_time();
+	ccv_nnc_cmd_exec(cmd, hint, 0, TENSOR_LIST(a, w, bias), TENSOR_LIST(b));
+	elapsed_time = get_current_time() - elapsed_time;
+	printf("%u ms for ref\n", elapsed_time);
+	ccv_nnc_tensor_t* c = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(OUTPUT_DIM, OUTPUT_SIZE, OUTPUT_SIZE), 0);
+	ccv_nnc_cmd_t tuned_cmd = ccv_nnc_cmd_autotune(cmd, hint, 0, TENSOR_LIST(a, w, bias), TENSOR_LIST(c));
+	elapsed_time = get_current_time();
+	ccv_nnc_cmd_exec(tuned_cmd, hint, 0, TENSOR_LIST(a, w, bias), TENSOR_LIST(c));
+	elapsed_time = get_current_time() - elapsed_time;
+	printf("%u ms for auto-tuned, backend %d, algorithm %d\n", elapsed_time, tuned_cmd.backend, tuned_cmd.algorithm);
+	for (i = 0; i < OUTPUT_DIM * OUTPUT_SIZE * OUTPUT_SIZE; i++)
+		if (fabs(b->data.f32[i] - c->data.f32[i]) > 1e-5)
+			printf("%d %f %f\n", i, b->data.f32[i], c->data.f32[i]);
+	ccv_nnc_tensor_free(c);
+	ccv_nnc_tensor_free(bias);
+	ccv_nnc_tensor_free(w);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(a);
+}
---
+++ bin/nnc/opt/auto-tune-fc.c
@@ -0,0 +1,84 @@
+#include <ccv.h>
+#include <ccv_internal.h>
+#include <nnc/ccv_nnc.h>
+#include <nnc/ccv_nnc_easy.h>
+#include <3rdparty/dsfmt/dSFMT.h>
+#include <sys/time.h>
+#include <ctype.h>
+
+static unsigned int get_current_time(void)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+#define INPUT_DIM (4096)
+#define OUTPUT_DIM (4096)
+
+int main(int argc, char** argv)
+{
+	ccv_nnc_init();
+	ccv_nnc_tensor_t* a = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(INPUT_DIM), 0);
+	ccv_nnc_tensor_t* b = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(OUTPUT_DIM), 0);
+	ccv_nnc_cmd_t forw_cmd = ccv_nnc_cmd(CCV_NNC_COMPUTE_FULL_CONNECT_FORWARD, 0, CMD_FULL_CONNECT(OUTPUT_DIM), 0);
+	ccv_nnc_tensor_t* w = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(INPUT_DIM, OUTPUT_DIM), 0);
+	ccv_nnc_tensor_t* bias = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(OUTPUT_DIM), 0);
+	// configure the inlets.
+	dsfmt_t dsfmt;
+	dsfmt_init_gen_rand(&dsfmt, 0);
+	int i;
+	for (i = 0; i < INPUT_DIM * OUTPUT_DIM; i++)
+		w->data.f32[i] = dsfmt_genrand_open_close(&dsfmt) / INPUT_DIM;
+	for (i = 0; i < INPUT_DIM; i++)
+		a->data.f32[i] = dsfmt_genrand_open_close(&dsfmt);
+	for (i = 0; i < OUTPUT_DIM; i++)
+		bias->data.f32[i] = (float)i / OUTPUT_DIM;
+	unsigned int elapsed_time = get_current_time();
+	ccv_nnc_cmd_exec(forw_cmd, ccv_nnc_default_hint, 0, TENSOR_LIST(a, w, bias), TENSOR_LIST(b));
+	elapsed_time = get_current_time() - elapsed_time;
+	printf("forw %u ms for ref\n", elapsed_time);
+	ccv_nnc_tensor_t* c = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(OUTPUT_DIM), 0);
+	ccv_nnc_cmd_t tuned_forw_cmd = ccv_nnc_cmd_autotune(forw_cmd, ccv_nnc_default_hint, 0, TENSOR_LIST(a, w, bias), TENSOR_LIST(c));
+	elapsed_time = get_current_time();
+	ccv_nnc_cmd_exec(tuned_forw_cmd, ccv_nnc_default_hint, 0, TENSOR_LIST(a, w, bias), TENSOR_LIST(c));
+	elapsed_time = get_current_time() - elapsed_time;
+	printf("forw %u ms for auto-tuned, backend %d, algorithm %d\n", elapsed_time, tuned_forw_cmd.backend, tuned_forw_cmd.algorithm);
+	for (i = 0; i < OUTPUT_DIM; i++)
+		if (fabs(b->data.f32[i] - c->data.f32[i]) > 1e-5)
+			printf("forw output[%d]: %f %f\n", i, b->data.f32[i], c->data.f32[i]);
+	ccv_nnc_tensor_t* dw = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(INPUT_DIM, OUTPUT_DIM), 0);
+	ccv_nnc_tensor_t* h = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(INPUT_DIM), 0);
+	ccv_nnc_cmd_t back_cmd = ccv_nnc_cmd(CCV_NNC_COMPUTE_FULL_CONNECT_BACKWARD, 0, CMD_FULL_CONNECT(OUTPUT_DIM), 0);
+	elapsed_time = get_current_time();
+	ccv_nnc_cmd_exec(back_cmd, ccv_nnc_default_hint, 0, TENSOR_LIST(b, a, w), TENSOR_LIST(dw, bias, h));
+	elapsed_time = get_current_time() - elapsed_time;
+	printf("back %u ms for ref\n", elapsed_time);
+	ccv_nnc_tensor_t* dwc = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(INPUT_DIM, OUTPUT_DIM), 0);
+	ccv_nnc_tensor_t* hc = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(INPUT_DIM), 0);
+	ccv_nnc_tensor_t* biasc = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(OUTPUT_DIM), 0);
+	ccv_nnc_cmd_t tuned_back_cmd = ccv_nnc_cmd_autotune(back_cmd, ccv_nnc_default_hint, 0, TENSOR_LIST(b, a, w), TENSOR_LIST(dwc, biasc, hc));
+	elapsed_time = get_current_time();
+	ccv_nnc_cmd_exec(tuned_back_cmd, ccv_nnc_default_hint, 0, TENSOR_LIST(b, a, w), TENSOR_LIST(dwc, biasc, hc));
+	elapsed_time = get_current_time() - elapsed_time;
+	printf("back %u ms for auto-tuned, backend %d, algorithm %d\n", elapsed_time, tuned_back_cmd.backend, tuned_back_cmd.algorithm);
+	for (i = 0; i < OUTPUT_DIM; i++)
+		if (fabs(bias->data.f32[i] - biasc->data.f32[i]) > 1e-5)
+			printf("back bias[%d]: %f %f\n", i, bias->data.f32[i], biasc->data.f32[i]);
+	for (i = 0; i < INPUT_DIM * OUTPUT_DIM; i++)
+		if (fabs(dw->data.f32[i] - dwc->data.f32[i]) > 1e-5)
+			printf("back dw[%d]: %f %f\n", i, dw->data.f32[i], dwc->data.f32[i]);
+	for (i = 0; i < INPUT_DIM; i++)
+		if (fabs(h->data.f32[i] - hc->data.f32[i]) > 1e-5)
+			printf("back h[%d]: %f %f\n", i, h->data.f32[i], hc->data.f32[i]);
+	ccv_nnc_tensor_free(biasc);
+	ccv_nnc_tensor_free(dwc);
+	ccv_nnc_tensor_free(hc);
+	ccv_nnc_tensor_free(c);
+	ccv_nnc_tensor_free(bias);
+	ccv_nnc_tensor_free(w);
+	ccv_nnc_tensor_free(dw);
+	ccv_nnc_tensor_free(h);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(a);
+}
