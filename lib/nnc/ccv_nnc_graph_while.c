cdd0ca90b142487afd9ddc391a29cf5c3957a65c
Liu Liu <i@liuliu.me>
2017-01-26 02:54:55 -0500

    Unit test for simple while graph.

--- lib/nnc/ccv_nnc_graph_while.c
+++ lib/nnc/ccv_nnc_graph_while.c
@@ -30,6 +30,19 @@ ccv_nnc_graph_exec_t ccv_nnc_graph_while(ccv_nnc_graph_t* const graph, uint32_t 
 	assert(conditional_size > 0);
 	while_graph->conditional_size = conditional_size;
 	while_graph->conditionals = (ccv_nnc_graph_exec_t*)((while_graph->conditionals) ? ccrealloc(while_graph->conditionals, sizeof(ccv_nnc_graph_exec_t) * conditional_size) : ccmalloc(sizeof(ccv_nnc_graph_exec_t) * conditional_size));
+	if (!while_graph->sources)
+		while_graph->sources = ccv_array_new(sizeof(ccv_nnc_graph_exec_t), source_size, 0);
+	else
+		ccv_array_clear(while_graph->sources);
+	int i;
+	for (i = 0; i < source_size; i++)
+		ccv_array_push(while_graph->sources, sources + i);
+	if (!while_graph->destinations)
+		while_graph->destinations = ccv_array_new(sizeof(ccv_nnc_graph_exec_t), destination_size, 0);
+	else
+		ccv_array_clear(while_graph->destinations);
+	for (i = 0; i < destination_size; i++)
+		ccv_array_push(while_graph->destinations, destinations + i);
 	memcpy(while_graph->conditionals, conditionals, sizeof(ccv_nnc_graph_exec_t) * conditional_size);
 	ccv_array_push(graph->sub_graphs, &while_graph);
 	while_exec_info->graph_ref = graph->sub_graphs->rnum;
@@ -52,18 +65,36 @@ int ccv_nnc_graph_while_run(const ccv_nnc_graph_t* const graph, ccv_nnc_tensor_t
 	} while (0)
 	if (graph->while_func)
 	{
+		int i, j;
+		ccv_array_t* follows = ccv_array_new(sizeof(ccv_nnc_graph_exec_t), graph->conditional_size, 0);
+		for (i = 0; i < graph->conditional_size; i++)
+		{
+			const ccv_nnc_graph_exec_info_t* const exec_info = (const ccv_nnc_graph_exec_info_t*)ccv_array_get(graph->exec_info, graph->conditionals->d);
+			if (exec_info->outgoings)
+				for (j = 0; j < exec_info->outgoings->rnum; j++)
+				{
+					const ccv_nnc_graph_exec_t exec = {
+						.d = *(int*)ccv_array_get(exec_info->outgoings, j),
+						.graph = graph,
+					};
+					ccv_array_push(follows, &exec);
+				}
+		}
 		uint64_t count = 0;
 		ccv_nnc_tensor_t count_tensor = ccv_nnc_tensor(&count, ONE_CPU_TENSOR(1, 1, 1), 0);
 		ccv_nnc_tensor_t* special_tensors[] = { &count_tensor };
 		for (;;)
 		{
+			++count;
 			CCV_NNC_GRAPH_VISIT(graph, (ccv_nnc_graph_exec_info_t*)ccv_array_get(graph->exec_info, 0), graph->exec_info->rnum, sources, source_size, graph->conditionals, graph->conditional_size, visitor);
-			// Reached conditionals, now check the conditional, if met, break out.
-			if (graph->while_func(special_tensors, 1, 0, 0, 0, 0, graph->while_data))
+			// Reached conditionals, now check the conditional, if not met, break out.
+			if (!graph->while_func(special_tensors, 1, 0, 0, 0, 0, graph->while_data))
 				break;
-			CCV_NNC_GRAPH_VISIT(graph, (ccv_nnc_graph_exec_info_t*)ccv_array_get(graph->exec_info, 0), graph->exec_info->rnum, graph->conditionals, graph->conditional_size, destinations, destination_size, visitor);
+			if (follows->rnum > 0)
+				CCV_NNC_GRAPH_VISIT(graph, (ccv_nnc_graph_exec_info_t*)ccv_array_get(graph->exec_info, 0), graph->exec_info->rnum, (ccv_nnc_graph_exec_t*)ccv_array_get(follows, 0), follows->rnum, destinations, destination_size, visitor);
 			// TODO: Change versions on the tensor, so it point to a different part of the data.
 		}
+		ccv_array_free(follows);
 	} else {
 		CCV_NNC_GRAPH_VISIT(graph, (ccv_nnc_graph_exec_info_t*)ccv_array_get(graph->exec_info, 0), graph->exec_info->rnum, sources, source_size, destinations, destination_size, visitor);
 	}
--- test/unit/nnc/while.tests.c
+++ test/unit/nnc/while.tests.c
@@ -15,7 +15,29 @@ int while_5(ccv_nnc_tensor_t* const* const specials, const int special_size, ccv
 	return specials[0]->data.i64[0] < 5;
 }
 
-TEST_CASE("a while loop to compute x * y ^ 5")
+TEST_CASE("graph for a while loop to compute 0.34 * 1.11 ^ 5")
+{
+	ccv_nnc_graph_t* graph = ccv_nnc_graph_new();
+	ccv_nnc_tensor_t* x = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(1), 0);
+	ccv_nnc_tensor_t* y = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(1), 0);
+	ccv_nnc_tensor_t* z = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(1), 0);
+	ccv_nnc_graph_t* while_graph = ccv_nnc_graph_new();
+	ccv_nnc_graph_exec_t prod0 = ccv_nnc_graph_exec_new(while_graph, ccv_nnc_cmd(CCV_NNC_EWPROD_FORWARD, 0, CMD_GENERIC(), 0), ccv_nnc_no_hint, TENSOR_LIST(y, z), TENSOR_LIST(z));
+	ccv_nnc_graph_exec_t loop = ccv_nnc_graph_while(graph, CCV_NNC_GRAPH_FORWARD, while_graph, GRAPH_EXEC_LIST(prod0), GRAPH_EXEC_LIST(prod0), GRAPH_EXEC_LIST(prod0), TENSOR_LIST(z), TENSOR_LIST(z), while_5, 0);
+	ccv_nnc_graph_exec_t prod1 = ccv_nnc_graph_exec_new(graph, ccv_nnc_cmd(CCV_NNC_EWPROD_FORWARD, 0, CMD_GENERIC(), 0), ccv_nnc_no_hint, TENSOR_LIST(x, z), TENSOR_LIST(z));
+	ccv_nnc_graph_exec_concat(graph, loop, prod1);
+	x->data.f32[0] = 0.34;
+	y->data.f32[0] = 1.11;
+	z->data.f32[0] = 1;
+	ccv_nnc_graph_while_run(graph, 0, 0, GRAPH_EXEC_LIST(loop), GRAPH_EXEC_LIST(prod1));
+	ccv_nnc_graph_free(graph);
+	REQUIRE_EQ_WITH_TOLERANCE(z->data.f32[0], 0.34 * 1.11 * 1.11 * 1.11 * 1.11 * 1.11, 1e-6, "computed result of 0.34 * 1.11 ^ 5 should be the same");
+	ccv_nnc_tensor_free(x);
+	ccv_nnc_tensor_free(y);
+	ccv_nnc_tensor_free(z);
+}
+
+TEST_CASE("symbolic graph for a while loop to compute x * y ^ 5")
 {
 	ccv_nnc_symbolic_graph_t* symbolic_graph = ccv_nnc_symbolic_graph_new();
 	ccv_nnc_tensor_symbol_t x = ccv_nnc_tensor_symbol_new(symbolic_graph, ONE_CPU_TENSOR(1), "x");
