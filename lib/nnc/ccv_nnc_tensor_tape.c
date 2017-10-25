a7667c1083e6bb1aeb95523c18c3ff9997eb4b26
Liu Liu <i@liuliu.me>
2017-10-24 22:59:10 -0400

    Validate tape for more complicated case.

--- lib/nnc/ccv_nnc_tensor_tape.c
+++ lib/nnc/ccv_nnc_tensor_tape.c
@@ -65,12 +65,12 @@ static ccv_nnc_tape_tensor_data_array_t* _ccv_nnc_tape_tensor_data_array_get(con
 
 static void _ccv_nnc_tape_tensor_data_move(ccv_nnc_tape_tensor_data_t* const old_data, ccv_nnc_tape_tensor_data_t* const new_data, const int offset, const ccv_nnc_graph_t* const* const graphs, const int graph_size, const int* const dim, const int dim_count)
 {
-		int i;
+	int i;
 	if (offset == ccv_max(dim_count, graph_size) - 1)
 	{
 		const int data_dim = offset < dim_count ? dim[offset] - 1 : 0;
 		const int graph_dim = offset < graph_size ? graphs[offset]->while_count : 0;
-		assert(data_dim <= graph_dim);
+		assert(old_data <= new_data);
 		// Do the actual copy or set.
 		if (!old_data)
 			for (i = ccv_max(data_dim, graph_dim); i >= 0; i--)
@@ -119,18 +119,7 @@ static void _ccv_nnc_tape_tensor_data_array_resize(ccv_nnc_tape_tensor_data_arra
 	// overwrite issues.
 	assert(graph_size > 0);
 	assert(data_array->dim_count > 0);
-	int old_data_step = 1;
-	for (i = 1; i < data_array->dim_count; i++)
-		old_data_step *= data_array->dim[i];
-	int new_data_step = 1;
-	for (i = 1; i < new_dim_count; i++)
-	{
-		int old_dim = (i < data_array->dim_count) ? data_array->dim[i] : 1;
-		int graph_dim = (i < graph_size) ? (int)(graphs[i]->while_count + 1) : 1;
-		new_data_step *= ccv_max(old_dim, graph_dim);
-	}
-	for (i = ccv_max(data_array->dim[0] - 1, graphs[0]->while_count); i >= 0; i--)
-		_ccv_nnc_tape_tensor_data_move(i < data_array->dim[0] ? old_data + i * old_data_step : 0, new_data + i * new_data_step, 0, graphs, graph_size, data_array->dim, data_array->dim_count);
+	_ccv_nnc_tape_tensor_data_move(old_data, new_data, 0, graphs, graph_size, data_array->dim, data_array->dim_count);
 	data_array->data = new_data;
 	// We are done, update the dim.
 	for (i = 0; i < new_dim_count; i++)
