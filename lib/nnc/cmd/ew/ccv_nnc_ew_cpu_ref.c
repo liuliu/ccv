4ccfde876f37e7fd6bac98948018f1e8190eb0ac
Liu Liu <i@liuliu.me>
2018-04-04 00:02:43 -0400

    Replace some methods with shared ones.

--- lib/nnc/cmd/ew/ccv_nnc_ew_cpu_ref.c
+++ lib/nnc/cmd/ew/ccv_nnc_ew_cpu_ref.c
@@ -399,16 +399,13 @@ static int _ccv_nnc_ewprod_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hi
 	return CCV_NNC_EXEC_SUCCESS;
 }
 
-static int _ccv_nnc_ewdiv_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, const ccv_nnc_stream_context_t* const stream_context)
+static void _ccv_nnc_ewdiv_forw_cpu_ref(const float p, ccv_nnc_tensor_view_t* const a, ccv_nnc_tensor_view_t* const b, ccv_nnc_tensor_view_t* const c)
 {
 	// Assuming this is float 32.
 	int dim[CCV_NNC_MAX_DIM + 2];
 	int ainc[CCV_NNC_MAX_DIM + 2];
 	int binc[CCV_NNC_MAX_DIM + 2];
 	int cinc[CCV_NNC_MAX_DIM + 2];
-	ccv_nnc_tensor_view_t* a = (ccv_nnc_tensor_view_t*)inputs[0];
-	ccv_nnc_tensor_view_t* b = (ccv_nnc_tensor_view_t*)inputs[1];
-	ccv_nnc_tensor_view_t* c = (ccv_nnc_tensor_view_t*)outputs[0];
 	if (a == 0) // Take 0 as all ones tensor.
 	{
 		assert(b->info.dim[CCV_NNC_MAX_DIM + 2] == 0);
@@ -421,8 +418,8 @@ static int _ccv_nnc_ewdiv_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hin
 			// Super optimal case, just do one for-loop for sum.
 			const int tensor_count = ccv_nnc_tensor_count(b->info);
 			for (x = 0; x < tensor_count; x++)
-				c->data.f32[x] = 1 / b->data.f32[x];
-			return CCV_NNC_EXEC_SUCCESS;
+				c->data.f32[x] = p / b->data.f32[x];
+			return;
 		}
 		assert(CCV_NNC_MAX_DIM == 2); // Need to change this logic for CCV_NNC_MAX_DIM == other number.
 		ccv_nnc_tensor_view_get_inc(b, binc);
@@ -439,14 +436,14 @@ static int _ccv_nnc_ewdiv_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hin
 				for (i[1] = 0; i[1] < dim[1]; i[1]++)
 				{
 					for (x = 0; x < count; x++)
-						cp[x] = 1 / bp[x];
+						cp[x] = p / bp[x];
 					bp += binc[2] * binc[3];
 					cp += cinc[2] * cinc[3];
 				}
 				bp += (binc[1] - dim[1]) * binc[2] * binc[3];
 				cp += (cinc[1] - dim[1]) * cinc[2] * cinc[3];
 			}
-			return CCV_NNC_EXEC_SUCCESS;
+			return;
 		}
 		// Non-optimal case, need to do skip copy.
 		for (i[0] = 0; i[0] < dim[0]; i[0]++)
@@ -456,7 +453,7 @@ static int _ccv_nnc_ewdiv_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hin
 				for (i[2] = 0; i[2] < dim[2]; i[2]++)
 				{
 					for (x = 0; x < dim[3]; x++)
-						cp[x] = 1 / bp[x];
+						cp[x] = p / bp[x];
 					bp += binc[3];
 					cp += cinc[3];
 				}
@@ -479,8 +476,8 @@ static int _ccv_nnc_ewdiv_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hin
 			// Super optimal case, just do one for-loop for sum.
 			const int tensor_count = ccv_nnc_tensor_count(a->info);
 			for (x = 0; x < tensor_count; x++)
-				c->data.f32[x] = a->data.f32[x] / b->data.f32[x];
-			return CCV_NNC_EXEC_SUCCESS;
+				c->data.f32[x] = p * a->data.f32[x] / b->data.f32[x];
+			return;
 		}
 		assert(CCV_NNC_MAX_DIM == 2); // Need to change this logic for CCV_NNC_MAX_DIM == other number.
 		ccv_nnc_tensor_view_get_inc(a, ainc);
@@ -499,7 +496,7 @@ static int _ccv_nnc_ewdiv_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hin
 				for (i[1] = 0; i[1] < dim[1]; i[1]++)
 				{
 					for (x = 0; x < count; x++)
-						cp[x] = ap[x] / bp[x];
+						cp[x] = p * ap[x] / bp[x];
 					ap += ainc[2] * ainc[3];
 					bp += binc[2] * binc[3];
 					cp += cinc[2] * cinc[3];
@@ -508,7 +505,7 @@ static int _ccv_nnc_ewdiv_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hin
 				bp += (binc[1] - dim[1]) * binc[2] * binc[3];
 				cp += (cinc[1] - dim[1]) * cinc[2] * cinc[3];
 			}
-			return CCV_NNC_EXEC_SUCCESS;
+			return;
 		}
 		// Non-optimal case, need to do skip copy.
 		for (i[0] = 0; i[0] < dim[0]; i[0]++)
@@ -518,7 +515,7 @@ static int _ccv_nnc_ewdiv_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hin
 				for (i[2] = 0; i[2] < dim[2]; i[2]++)
 				{
 					for (x = 0; x < dim[3]; x++)
-						cp[x] = ap[x] / bp[x];
+						cp[x] = p * ap[x] / bp[x];
 					ap += ainc[3];
 					bp += binc[3];
 					cp += cinc[3];
@@ -532,6 +529,11 @@ static int _ccv_nnc_ewdiv_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hin
 			cp += (cinc[1] - dim[1]) * cinc[2] * cinc[3];
 		}
 	}
+}
+
+static int _ccv_nnc_ewdiv_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, const ccv_nnc_stream_context_t* const stream_context)
+{
+	_ccv_nnc_ewdiv_forw_cpu_ref(1, (ccv_nnc_tensor_view_t*)inputs[0], (ccv_nnc_tensor_view_t*)inputs[1], (ccv_nnc_tensor_view_t*)outputs[0]);
 	return CCV_NNC_EXEC_SUCCESS;
 }
 
@@ -541,9 +543,8 @@ static int _ccv_nnc_ewdiv_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hin
 	if (output_size == 1 || outputs[1] == 0)
 	{
 		// When we only need D[x / y, y]
-		ccv_nnc_cmd_t forw_cmd = cmd;
-		forw_cmd.cmd = CCV_NNC_EWDIV_FORWARD;
-		return _ccv_nnc_ewdiv_forw(cmd, ccv_nnc_no_hint, flags, TENSOR_LIST(inputs[0], inputs[2]), &outputs[0], 1, stream_context);
+		_ccv_nnc_ewdiv_forw_cpu_ref(1, (ccv_nnc_tensor_view_t*)inputs[0], (ccv_nnc_tensor_view_t*)inputs[2], (ccv_nnc_tensor_view_t*)outputs[0]);
+		return CCV_NNC_EXEC_SUCCESS;
 	}
 	int dim[CCV_NNC_MAX_DIM + 2];
 	int ginc[CCV_NNC_MAX_DIM + 2];
@@ -1017,10 +1018,9 @@ static int _ccv_nnc_ewlog_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hin
 
 static int _ccv_nnc_ewlog_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, const ccv_nnc_stream_context_t* const stream_context)
 {
-	ccv_nnc_cmd_t forw_cmd = cmd;
-	forw_cmd.cmd = CCV_NNC_EWDIV_FORWARD;
 	// D[Log[x], x] = 1 / x
-	return _ccv_nnc_ewdiv_forw(forw_cmd, ccv_nnc_no_hint, flags, TENSOR_LIST(inputs[0], inputs[1]), outputs, output_size, stream_context);
+	_ccv_nnc_ewdiv_forw_cpu_ref(1, (ccv_nnc_tensor_view_t*)inputs[0], (ccv_nnc_tensor_view_t*)inputs[1], (ccv_nnc_tensor_view_t*)outputs[0]);
+	return CCV_NNC_EXEC_SUCCESS;
 }
 
 static int _ccv_nnc_ewsqrt_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, const ccv_nnc_stream_context_t* const stream_context)
@@ -1092,137 +1092,7 @@ static int _ccv_nnc_ewsqrt_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hi
 static int _ccv_nnc_ewsqrt_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, const ccv_nnc_stream_context_t* const stream_context)
 {
 	// D[Sqrt[x], x] = 0.5 / Sqrt[x]
-	// Assuming this is float 32.
-	int dim[CCV_NNC_MAX_DIM + 2];
-	int ainc[CCV_NNC_MAX_DIM + 2];
-	int binc[CCV_NNC_MAX_DIM + 2];
-	int cinc[CCV_NNC_MAX_DIM + 2];
-	ccv_nnc_tensor_view_t* a = (ccv_nnc_tensor_view_t*)inputs[0];
-	ccv_nnc_tensor_view_t* b = (ccv_nnc_tensor_view_t*)inputs[2];
-	ccv_nnc_tensor_view_t* c = (ccv_nnc_tensor_view_t*)outputs[0];
-	if (a == 0) // Take 0 as all ones tensor.
-	{
-		assert(b->info.dim[CCV_NNC_MAX_DIM + 2] == 0);
-		assert(c->info.dim[CCV_NNC_MAX_DIM + 2] == 0);
-		ccv_nnc_tensor_view_get_dim(b, dim);
-		assert(ccv_nnc_tensor_view_check_dim(c, dim));
-		int x;
-		if (!CCV_IS_TENSOR_VIEW(b) && !CCV_IS_TENSOR_VIEW(c))
-		{
-			// Super optimal case, just do one for-loop for sum.
-			const int tensor_count = ccv_nnc_tensor_count(b->info);
-			for (x = 0; x < tensor_count; x++)
-				c->data.f32[x] = 0.5 / b->data.f32[x];
-			return CCV_NNC_EXEC_SUCCESS;
-		}
-		assert(CCV_NNC_MAX_DIM == 2); // Need to change this logic for CCV_NNC_MAX_DIM == other number.
-		ccv_nnc_tensor_view_get_inc(b, binc);
-		ccv_nnc_tensor_view_get_inc(c, cinc);
-		int i[CCV_NNC_MAX_DIM + 2];
-		float* bp = b->data.f32;
-		float* cp = c->data.f32;
-		const int count = dim[2] * dim[3];
-		if (binc[3] == dim[3] && cinc[3] == dim[3])
-		{
-			// Special casing if the ainc[3] is the same as dim[3]
-			for (i[0] = 0; i[0] < dim[0]; i[0]++)
-			{
-				for (i[1] = 0; i[1] < dim[1]; i[1]++)
-				{
-					for (x = 0; x < count; x++)
-						cp[x] = 0.5 / bp[x];
-					bp += binc[2] * binc[3];
-					cp += cinc[2] * cinc[3];
-				}
-				bp += (binc[1] - dim[1]) * binc[2] * binc[3];
-				cp += (cinc[1] - dim[1]) * cinc[2] * cinc[3];
-			}
-			return CCV_NNC_EXEC_SUCCESS;
-		}
-		// Non-optimal case, need to do skip copy.
-		for (i[0] = 0; i[0] < dim[0]; i[0]++)
-		{
-			for (i[1] = 0; i[1] < dim[1]; i[1]++)
-			{
-				for (i[2] = 0; i[2] < dim[2]; i[2]++)
-				{
-					for (x = 0; x < dim[3]; x++)
-						cp[x] = 0.5 / bp[x];
-					bp += binc[3];
-					cp += cinc[3];
-				}
-				bp += (binc[2] - dim[2]) * binc[3];
-				cp += (cinc[2] - dim[2]) * cinc[3];
-			}
-			bp += (binc[1] - dim[1]) * binc[2] * binc[3];
-			cp += (cinc[1] - dim[1]) * cinc[2] * cinc[3];
-		}
-	} else {
-		assert(a->info.dim[CCV_NNC_MAX_DIM + 2] == 0);
-		assert(b->info.dim[CCV_NNC_MAX_DIM + 2] == 0);
-		assert(c->info.dim[CCV_NNC_MAX_DIM + 2] == 0);
-		ccv_nnc_tensor_view_get_dim(a, dim);
-		assert(ccv_nnc_tensor_view_check_dim(b, dim));
-		assert(ccv_nnc_tensor_view_check_dim(c, dim));
-		int x;
-		if (!CCV_IS_TENSOR_VIEW(a) && !CCV_IS_TENSOR_VIEW(b) && !CCV_IS_TENSOR_VIEW(c))
-		{
-			// Super optimal case, just do one for-loop for sum.
-			const int tensor_count = ccv_nnc_tensor_count(a->info);
-			for (x = 0; x < tensor_count; x++)
-				c->data.f32[x] = 0.5 * a->data.f32[x] / b->data.f32[x];
-			return CCV_NNC_EXEC_SUCCESS;
-		}
-		assert(CCV_NNC_MAX_DIM == 2); // Need to change this logic for CCV_NNC_MAX_DIM == other number.
-		ccv_nnc_tensor_view_get_inc(a, ainc);
-		ccv_nnc_tensor_view_get_inc(b, binc);
-		ccv_nnc_tensor_view_get_inc(c, cinc);
-		int i[CCV_NNC_MAX_DIM + 2];
-		float* ap = a->data.f32;
-		float* bp = b->data.f32;
-		float* cp = c->data.f32;
-		const int count = dim[2] * dim[3];
-		if (ainc[3] == dim[3] && binc[3] == dim[3] && cinc[3] == dim[3])
-		{
-			// Special casing if the ainc[3] is the same as dim[3]
-			for (i[0] = 0; i[0] < dim[0]; i[0]++)
-			{
-				for (i[1] = 0; i[1] < dim[1]; i[1]++)
-				{
-					for (x = 0; x < count; x++)
-						cp[x] = 0.5 * ap[x] / bp[x];
-					ap += ainc[2] * ainc[3];
-					bp += binc[2] * binc[3];
-					cp += cinc[2] * cinc[3];
-				}
-				ap += (ainc[1] - dim[1]) * ainc[2] * ainc[3];
-				bp += (binc[1] - dim[1]) * binc[2] * binc[3];
-				cp += (cinc[1] - dim[1]) * cinc[2] * cinc[3];
-			}
-			return CCV_NNC_EXEC_SUCCESS;
-		}
-		// Non-optimal case, need to do skip copy.
-		for (i[0] = 0; i[0] < dim[0]; i[0]++)
-		{
-			for (i[1] = 0; i[1] < dim[1]; i[1]++)
-			{
-				for (i[2] = 0; i[2] < dim[2]; i[2]++)
-				{
-					for (x = 0; x < dim[3]; x++)
-						cp[x] = 0.5 * ap[x] / bp[x];
-					ap += ainc[3];
-					bp += binc[3];
-					cp += cinc[3];
-				}
-				ap += (ainc[2] - dim[2]) * ainc[3];
-				bp += (binc[2] - dim[2]) * binc[3];
-				cp += (cinc[2] - dim[2]) * cinc[3];
-			}
-			ap += (ainc[1] - dim[1]) * ainc[2] * ainc[3];
-			bp += (binc[1] - dim[1]) * binc[2] * binc[3];
-			cp += (cinc[1] - dim[1]) * cinc[2] * cinc[3];
-		}
-	}
+	_ccv_nnc_ewdiv_forw_cpu_ref(0.5, (ccv_nnc_tensor_view_t*)inputs[0], (ccv_nnc_tensor_view_t*)inputs[2], (ccv_nnc_tensor_view_t*)outputs[0]);
 	return CCV_NNC_EXEC_SUCCESS;
 }
 
