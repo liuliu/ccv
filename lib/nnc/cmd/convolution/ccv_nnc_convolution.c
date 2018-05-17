84d67b2740d12ce08e2e91f82627923ceb0ae242
Liu Liu <i@liuliu.me>
2018-05-17 04:42:12 -0400

    Make sure the convolution bitmask works.

--- lib/nnc/cmd/convolution/ccv_nnc_convolution.c
+++ lib/nnc/cmd/convolution/ccv_nnc_convolution.c
@@ -13,7 +13,7 @@ static int _ccv_nnc_conv_forw_bitmask(const int input_size, const int output_siz
 static int _ccv_nnc_conv_back_bitmask(const int input_size, const int output_size, const uint64_t* const input_bitmasks, const int input_bitmask_size, const uint64_t* const output_bitmasks, const int output_bitmask_size)
 {
 	// Output the propagated error, gradient w.r.t. w and bias.
-	if ((input_bitmasks[0] & 15u) == ((1u << 0) | (1u << 1) | (1u << 2) | (0 << 3)) && output_bitmasks[0] == ((1u << 0) | (1u << 1) | (1u << 2)))
+	if ((input_bitmasks[0] & 7u) == ((1u << 0) | (1u << 1) | (1u << 2) | (0 << 3)) && output_bitmasks[0] == ((1u << 0) | (1u << 1) | (1u << 2)))
 		return 1;
 	// Don't propagate error, only gradient w.r.t. w and bias.
 	if ((input_bitmasks[0] & 3u) == ((1u << 0) | (1u << 1) | (0 << 2) | (0 << 3)) && output_bitmasks[0] == ((0 << 0) | (1u << 1) | (1u << 2)))
