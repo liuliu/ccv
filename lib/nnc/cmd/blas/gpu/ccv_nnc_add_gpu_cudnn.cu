f630370b48143c7b649880bad5a615856a4ca450
Liu Liu <i@liuliu.me>
2018-05-21 14:11:26 -0400

    Fix missing file.

---
+++ lib/nnc/cmd/blas/gpu/ccv_nnc_add_gpu_cudnn.cu
@@ -0,0 +1,126 @@
+extern "C" {
+#include <ccv.h>
+#include <ccv_internal.h>
+#include <nnc/ccv_nnc.h>
+#include <nnc/ccv_nnc_easy.h>
+#include <nnc/ccv_nnc_internal.h>
+}
+#include <nnc/gpu/ccv_nnc_compat.h>
+
+#ifdef HAVE_CUDNN
+
+static int _ccv_nnc_add_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, const ccv_nnc_stream_context_t* const stream_context)
+{
+	assert(input_size == 2);
+	cudnnHandle_t cudnn = ccv_nnc_stream_context_get_cudnn(stream_context);
+	const int device = ccv_nnc_stream_context_get_device(stream_context);
+	cudaSetDevice(device);
+	const float p = cmd.info.blas.a[0];
+	static const float zero = 0;
+	if (inputs[1] == 0)
+	{
+		const ccv_nnc_cudnn_tensor_view_descriptor_t a = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[0]);
+		const ccv_nnc_cudnn_tensor_view_descriptor_t c = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)outputs[0]);
+		assert_cudnn(cudnnTransformTensor(cudnn, &p, a.descriptor, a.data.u8,  &zero, c.descriptor, c.data.u8));
+		ccv_nnc_cudnn_deinit_tensor_view_descriptor(a);
+		ccv_nnc_cudnn_deinit_tensor_view_descriptor(c);
+		return CCV_NNC_EXEC_SUCCESS;
+	}
+	const float q = cmd.info.blas.a[1];
+	const ccv_nnc_cudnn_tensor_view_descriptor_t a = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[0]);
+	const ccv_nnc_cudnn_tensor_view_descriptor_t b = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[1]);
+	const ccv_nnc_cudnn_tensor_view_descriptor_t c = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)outputs[0]);
+	cudnnOpTensorDescriptor_t add = ccv_nnc_stream_context_get_op_tensor_descriptor(stream_context);
+	cudnnSetOpTensorDescriptor(add, CUDNN_OP_TENSOR_ADD, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN);
+	assert_cudnn(cudnnOpTensor(cudnn, add, &p, a.descriptor, a.data.u8, &q, b.descriptor, b.data.u8, &zero, c.descriptor, c.data.u8));
+	ccv_nnc_stream_context_return_op_tensor_descriptor(stream_context, add);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(a);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(b);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(c);
+	return CCV_NNC_EXEC_SUCCESS;
+}
+
+static int _ccv_nnc_add_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, const ccv_nnc_stream_context_t* const stream_context)
+{
+	cudnnHandle_t cudnn = ccv_nnc_stream_context_get_cudnn(stream_context);
+	const int device = ccv_nnc_stream_context_get_device(stream_context);
+	cudaSetDevice(device);
+	const float p = cmd.info.blas.a[0];
+	const float q = cmd.info.blas.a[1];
+	if (inputs[0] == 0)
+	{
+		if (outputs[0])
+		{
+			const ccv_nnc_cudnn_tensor_view_descriptor_t a = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)outputs[0]);
+			assert_cudnn(cudnnSetTensor(cudnn, a.descriptor, a.data.u8, &p));
+			ccv_nnc_cudnn_deinit_tensor_view_descriptor(a);
+		}
+		if (output_size > 1 && outputs[1])
+		{
+			const ccv_nnc_cudnn_tensor_view_descriptor_t b = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)outputs[1]);
+			assert_cudnn(cudnnSetTensor(cudnn, b.descriptor, b.data.u8, &q));
+			ccv_nnc_cudnn_deinit_tensor_view_descriptor(b);
+		}
+		return CCV_NNC_EXEC_SUCCESS;
+	}
+	int gdim[CCV_NNC_MAX_DIM + 2];
+	ccv_nnc_tensor_view_t* const g = (ccv_nnc_tensor_view_t*)inputs[0];
+	ccv_nnc_tensor_view_get_dim(g, gdim);
+	const ccv_nnc_cudnn_tensor_view_descriptor_t gcu = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, g);
+	static const float zero = 0;
+	if (outputs[0])
+	{
+		ccv_nnc_tensor_view_t* const a = (ccv_nnc_tensor_view_t*)outputs[0];
+		const ccv_nnc_cudnn_tensor_view_descriptor_t acu = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, a);
+		if (ccv_nnc_tensor_view_check_dim(a, gdim))
+		{
+			assert_cudnn(cudnnTransformTensor(cudnn, &p, gcu.descriptor, gcu.data.u8,  &zero, acu.descriptor, acu.data.u8));
+		} else {
+			cudnnReduceTensorDescriptor_t reduce_sum = ccv_nnc_stream_context_get_reduce_tensor_descriptor(stream_context);
+			cudnnSetReduceTensorDescriptor(reduce_sum, CUDNN_REDUCE_TENSOR_ADD, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN, CUDNN_REDUCE_TENSOR_NO_INDICES, CUDNN_32BIT_INDICES);
+			assert_cudnn(cudnnReduceTensor(cudnn, reduce_sum, 0, 0, 0, 0, &p, gcu.descriptor, gcu.data.u8, &zero, acu.descriptor, acu.data.u8));
+		}
+		ccv_nnc_cudnn_deinit_tensor_view_descriptor(acu);
+	}
+	if (output_size > 1 && outputs[1])
+	{
+		ccv_nnc_tensor_view_t* const a = (ccv_nnc_tensor_view_t*)outputs[1];
+		const ccv_nnc_cudnn_tensor_view_descriptor_t acu = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, a);
+		if (ccv_nnc_tensor_view_check_dim(a, gdim))
+		{
+			assert_cudnn(cudnnTransformTensor(cudnn, &q, gcu.descriptor, gcu.data.u8,  &zero, acu.descriptor, acu.data.u8));
+		} else {
+			cudnnReduceTensorDescriptor_t reduce_sum = ccv_nnc_stream_context_get_reduce_tensor_descriptor(stream_context);
+			cudnnSetReduceTensorDescriptor(reduce_sum, CUDNN_REDUCE_TENSOR_ADD, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN, CUDNN_REDUCE_TENSOR_NO_INDICES, CUDNN_32BIT_INDICES);
+			assert_cudnn(cudnnReduceTensor(cudnn, reduce_sum, 0, 0, 0, 0, &q, gcu.descriptor, gcu.data.u8, &zero, acu.descriptor, acu.data.u8));
+		}
+		ccv_nnc_cudnn_deinit_tensor_view_descriptor(acu);
+	}
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(gcu);
+	return CCV_NNC_EXEC_SUCCESS;
+}
+
+#endif
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_ADD_FORWARD, CCV_NNC_BACKEND_GPU_CUDNN)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+#ifdef HAVE_CUDNN
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NHWC | CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_CHWN;
+	registry->tensor_datatypes = CCV_32F;
+	registry->tensor_memory = CCV_TENSOR_GPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_add_forw;
+#endif
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_ADD_BACKWARD, CCV_NNC_BACKEND_GPU_CUDNN)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+#ifdef HAVE_CUDNN
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NHWC | CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_CHWN;
+	registry->tensor_datatypes = CCV_32F;
+	registry->tensor_memory = CCV_TENSOR_GPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_add_back;
+#endif
+}
+
