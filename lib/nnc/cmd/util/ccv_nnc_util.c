4f078bd0c1e7c790b85dc11b6ae3ffb6fd06b113
Liu Liu <i@liuliu.me>
2018-07-15 00:14:15 -0400

    Fix the issue with gradient.

--- lib/nnc/cmd/util/ccv_nnc_util.c
+++ lib/nnc/cmd/util/ccv_nnc_util.c
@@ -4,9 +4,28 @@
 
 static int _ccv_nnc_set_bitmask(const int input_size, const int output_size, const uint64_t* const input_bitmasks, const int input_bitmask_size, const uint64_t* const output_bitmasks, const int output_bitmask_size)
 {
-	if (output_bitmasks[0] == 1u)
-		return 1;
-	return 0;
+	int i, j, flag = 0;
+	int output_bitcount = 0;
+	for (i = 0; i < output_bitmask_size; i++)
+	{
+		for (j = 0; j < 64; j++)
+			if (output_bitmasks[i] & (uint64_t)1 << j)
+			{
+				if (flag)
+					return 0;
+			} else
+				break;
+		output_bitcount += j;
+		// Trailing zero even if it is not the end of input_bitmask_size, mark flag,
+		// if we encounter additional 1, return invalid.
+		if (j < 64)
+			flag = 1;
+		// Always like 1111100000, no 1110010101
+		for (; j < 64; j++)
+			if (output_bitmasks[i] & (uint64_t)1 << j)
+				return 0;
+	}
+	return output_size == output_bitcount;
 }
 
 REGISTER_COMMAND(CCV_NNC_SET_FORWARD)(ccv_nnc_cmd_registry_t* const registry)
