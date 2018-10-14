4ca735ddf010557d9450be80d928c863aaa2f44e
Liu Liu <i@liuliu.me>
2018-10-13 20:00:13 -0400

    Tuned for the resnet56 to run
    Need to write some more code for learning rate decay and run on test
    dataset.

--- bin/nnc/cifar-10.c
+++ bin/nnc/cifar-10.c
@@ -18,7 +18,7 @@ static ccv_cnnp_model_t* _building_block_new(const int filters, const int stride
 	{
 		ccv_cnnp_model_t* const conv0 = ccv_cnnp_convolution(1, filters, DIM_ALLOC(1, 1), (ccv_cnnp_param_t){
 			.no_bias = 1,
-			.hint = HINT((1, 1), (0, 0)),
+			.hint = HINT((strides, strides), (0, 0)),
 		});
 		shortcut = ccv_cnnp_model_apply(conv0, MODEL_IO_LIST(output));
 	}
@@ -61,8 +61,8 @@ inline static ccv_cnnp_model_t* _cifar_10_resnet56(void)
 	});
 	ccv_cnnp_model_io_t output = ccv_cnnp_model_apply(init_conv, MODEL_IO_LIST(input));
 	output = ccv_cnnp_model_apply(_block_layer_new(16, 1, 1, 9), MODEL_IO_LIST(output));
-	output = ccv_cnnp_model_apply(_block_layer_new(32, 2, 0, 9), MODEL_IO_LIST(output));
-	output = ccv_cnnp_model_apply(_block_layer_new(64, 2, 0, 9), MODEL_IO_LIST(output));
+	output = ccv_cnnp_model_apply(_block_layer_new(32, 2, 1, 9), MODEL_IO_LIST(output));
+	output = ccv_cnnp_model_apply(_block_layer_new(64, 2, 1, 9), MODEL_IO_LIST(output));
 	ccv_cnnp_model_t* identity = ccv_cnnp_identity((ccv_cnnp_param_t){
 		.norm = CCV_CNNP_BATCH_NORM,
 		.activation = CCV_CNNP_ACTIVATION_RELU,
@@ -117,7 +117,7 @@ inline static ccv_cnnp_model_t* _cifar_10_alexnet(void)
 static void train_cifar_10(ccv_array_t* const training_set, const float mean[3], ccv_array_t* const test_set)
 {
 	ccv_cnnp_model_t* const cifar_10 = _cifar_10_resnet56();
-	const ccv_nnc_tensor_param_t input = GPU_TENSOR_NCHW(000, 128, 3, 31, 31);
+	const ccv_nnc_tensor_param_t input = GPU_TENSOR_NCHW(000, 128, 3, 32, 32);
 	ccv_cnnp_model_compile(cifar_10, &input, 1, CMD_SGD_FORWARD(0.0001, 0.99, 0.9, 0.9), CMD_CATEGORICAL_CROSSENTROPY_FORWARD());
 	FILE *w = fopen("cifar-10.dot", "w+");
 	ccv_cnnp_model_dot(cifar_10, CCV_NNC_LONG_DOT_GRAPH, w);
@@ -125,7 +125,7 @@ static void train_cifar_10(ccv_array_t* const training_set, const float mean[3],
 	ccv_nnc_tensor_t* const input_tensor = ccv_nnc_tensor_new(0, input, 0);
 	ccv_nnc_tensor_t* const output_tensor = ccv_nnc_tensor_new(0, GPU_TENSOR_NCHW(000, 128, 10), 0);
 	ccv_nnc_tensor_t* const fit_tensor = ccv_nnc_tensor_new(0, GPU_TENSOR_NCHW(000, 128, 1), 0);
-	ccv_nnc_tensor_t* const cpu_input = ccv_nnc_tensor_new(0, CPU_TENSOR_NCHW(128, 3, 31, 31), 0);
+	ccv_nnc_tensor_t* const cpu_input = ccv_nnc_tensor_new(0, CPU_TENSOR_NCHW(128, 3, 32, 32), 0);
 	ccv_nnc_tensor_t* const cpu_output = ccv_nnc_tensor_new(0, CPU_TENSOR_NCHW(128, 10), 0);
 	ccv_nnc_tensor_t* const cpu_fit = ccv_nnc_tensor_new(0, CPU_TENSOR_NCHW(128, 1), 0);
 	int i, j, k;
@@ -140,13 +140,13 @@ static void train_cifar_10(ccv_array_t* const training_set, const float mean[3],
 			k = (int)(dsfmt_genrand_close_open(&dsfmt) * training_set->rnum);
 			assert(k < training_set->rnum);
 			ccv_categorized_t* const categorized = (ccv_categorized_t*)ccv_array_get(training_set, k);
-			float* const ip = cpu_input->data.f32 + j * 31 * 31 * 3;
+			float* const ip = cpu_input->data.f32 + j * 32 * 32 * 3;
 			float* const cp = categorized->matrix->data.f32;
 			int fi, fj, fk;
-			for (fi = 0; fi < 31; fi++)
-				for (fj = 0; fj < 31; fj++)
+			for (fi = 0; fi < 32; fi++)
+				for (fj = 0; fj < 32; fj++)
 					for (fk = 0; fk < 3; fk++)
-						ip[fi * 31 + fj + fk * 31 * 31] = cp[fi * 31 * 3 + fj * 3 + fk] - mean[fk];
+						ip[fi * 32 + fj + fk * 32 * 32] = cp[fi * 32 * 3 + fj * 3 + fk] - mean[fk];
 			assert(categorized->c >= 0 && categorized->c < 10);
 			cpu_fit->data.f32[j] = c[j] = categorized->c;
 		}
@@ -196,41 +196,41 @@ int main(int argc, char** argv)
 			fread(bytes, 32 * 32 + 1, 1, r1);
 			double per_mean[3] = {};
 			int c = bytes[0];
-			ccv_dense_matrix_t* a = ccv_dense_matrix_new(31, 31, CCV_32F | CCV_C3, 0, 0);
-			for (i = 0; i < 31; i++)
-				for (j = 0; j < 31; j++)
-					per_mean[0] += (a->data.f32[(j + i * 31) * 3] = bytes[j + i * 32 + 1] * 2. / 255.);
+			ccv_dense_matrix_t* a = ccv_dense_matrix_new(32, 32, CCV_32F | CCV_C3, 0, 0);
+			for (i = 0; i < 32; i++)
+				for (j = 0; j < 32; j++)
+					per_mean[0] += (a->data.f32[(j + i * 32) * 3] = bytes[j + i * 32 + 1] * 2. / 255.);
 			fread(bytes, 32 * 32, 1, r1);
-			for (i = 0; i < 31; i++)
-				for (j = 0; j < 31; j++)
-					per_mean[1] += (a->data.f32[(j + i * 31) * 3 + 1] = bytes[j + i * 32] * 2. / 255.);
+			for (i = 0; i < 32; i++)
+				for (j = 0; j < 32; j++)
+					per_mean[1] += (a->data.f32[(j + i * 32) * 3 + 1] = bytes[j + i * 32] * 2. / 255.);
 			fread(bytes, 32 * 32, 1, r1);
-			for (i = 0; i < 31; i++)
-				for (j = 0; j < 31; j++)
-					per_mean[2] += (a->data.f32[(j + i * 31) * 3 + 2] = bytes[j + i * 32] * 2. / 255.);
+			for (i = 0; i < 32; i++)
+				for (j = 0; j < 32; j++)
+					per_mean[2] += (a->data.f32[(j + i * 32) * 3 + 2] = bytes[j + i * 32] * 2. / 255.);
 			ccv_categorized_t categorized = ccv_categorized(c, a, 0);
 			ccv_array_push(categorizeds, &categorized);
-			mean[0] += per_mean[0] / (31 * 31);
-			mean[1] += per_mean[1] / (31 * 31);
-			mean[2] += per_mean[2] / (31 * 31);
+			mean[0] += per_mean[0] / (32 * 32);
+			mean[1] += per_mean[1] / (32 * 32);
+			mean[2] += per_mean[2] / (32 * 32);
 		}
 		ccv_array_t* tests = ccv_array_new(sizeof(ccv_categorized_t), num2, 0);
 		for (k = 0; k < num2; k++)
 		{
 			fread(bytes, 32 * 32 + 1, 1, r2);
 			int c = bytes[0];
-			ccv_dense_matrix_t* a = ccv_dense_matrix_new(31, 31, CCV_32F | CCV_C3, 0, 0);
-			for (i = 0; i < 31; i++)
-				for (j = 0; j < 31; j++)
-					a->data.f32[(j + i * 31) * 3] = bytes[j + i * 32 + 1] * 2. / 255.;
+			ccv_dense_matrix_t* a = ccv_dense_matrix_new(32, 32, CCV_32F | CCV_C3, 0, 0);
+			for (i = 0; i < 32; i++)
+				for (j = 0; j < 32; j++)
+					a->data.f32[(j + i * 32) * 3] = bytes[j + i * 32 + 1] * 2. / 255.;
 			fread(bytes, 32 * 32, 1, r2);
-			for (i = 0; i < 31; i++)
-				for (j = 0; j < 31; j++)
-					a->data.f32[(j + i * 31) * 3 + 1] = bytes[j + i * 32] * 2. / 255.;
+			for (i = 0; i < 32; i++)
+				for (j = 0; j < 32; j++)
+					a->data.f32[(j + i * 32) * 3 + 1] = bytes[j + i * 32] * 2. / 255.;
 			fread(bytes, 32 * 32, 1, r2);
-			for (i = 0; i < 31; i++)
-				for (j = 0; j < 31; j++)
-					a->data.f32[(j + i * 31) * 3 + 2] = bytes[j + i * 32] * 2. / 255.;
+			for (i = 0; i < 32; i++)
+				for (j = 0; j < 32; j++)
+					a->data.f32[(j + i * 32) * 3 + 2] = bytes[j + i * 32] * 2. / 255.;
 			ccv_categorized_t categorized = ccv_categorized(c, a, 0);
 			ccv_array_push(tests, &categorized);
 		}
