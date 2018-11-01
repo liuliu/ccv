67d119e8fa2cdfec590264a94c9e7fc139ad85e6
Liu Liu <i@liuliu.me>
2018-11-01 16:17:04 -0400

    Optimize the function to only deal with same size proposal first.

--- lib/nnc/ccv_nnc_symbolic_graph_compile.c
+++ lib/nnc/ccv_nnc_symbolic_graph_compile.c
@@ -74,8 +74,8 @@ typedef struct {
 
 // We first sort the same type together (because they won't be reused at all.
 // And then we sort by size, after that, sort by oc.
-#define more_than(i1, i2, aux) (((i1).type < (i2).type) || ((i1).type == (i2).type && ((i1).size > (i2).size) || ((i1).size == (i2).size && (i1).oc >= (i2).oc)))
-static CCV_IMPLEMENT_QSORT(_ccv_nnc_tensor_opt_sort_by_type_size_and_oc, ccv_nnc_tensor_opt_t, more_than)
+#define more_than(i1, i2, aux) ((i1).oc >= (i2).oc)
+static CCV_IMPLEMENT_QSORT(_ccv_nnc_tensor_opt_sort_by_oc, ccv_nnc_tensor_opt_t, more_than)
 #undef more_than
 
 // If b has items overlap with a, a is still after b (inclusive).
@@ -284,11 +284,13 @@ static ccv_nnc_tensor_alloc_prep_t* _ccv_nnc_tensor_alloc_prep_new(const ccv_spa
 		// Go through opt array, find all tensors that doesn't interfere with it, and have tensor size larger than it.
 		// Push them with the "companion" into the opt array as well.
 		const int rnum = opt->rnum;
+		uint64_t max_size = 0;
 		for (i = 0; i < rnum; i++)
 		{
 			// Copy it out, because after insertion, it may hold invalid pointer.
 			ccv_nnc_tensor_opt_t a = *(ccv_nnc_tensor_opt_t*)ccv_array_get(opt, i);
 			assert(a.companion == -1);
+			max_size = ccv_max(max_size, a.size);
 			const int companion_ref = tensor_blocks[a.index].companion_ref - 1;
 			for (k = 0; k < tensor_block_size; k++)
 				// Find non-overlapping tensor that has larger size (of course, is unassigned and is not one with designated companion).
@@ -312,11 +314,28 @@ static ccv_nnc_tensor_alloc_prep_t* _ccv_nnc_tensor_alloc_prep_new(const ccv_spa
 					b.companion = k;
 					b.oc = a.oc + oc[k];
 					b.size = tensor_blocks[k].size;
+					max_size = ccv_max(max_size, b.size);
 					ccv_array_push(opt, &b);
 				}
 		}
-		// Order opt array by the type, size and then oc.
-		_ccv_nnc_tensor_opt_sort_by_type_size_and_oc((ccv_nnc_tensor_opt_t*)opt->data, opt->rnum, 0);
+		// Filter out opt array if the size is smaller than the max size we have. The reason is because
+		// if we cannot accommodate the larger sizes, no matter how many smaller sizes we allocated,
+		// we still cannot accommodate the larger sizes. Thus, allocate the larger sizes only for now.
+		for (i = 0; i < opt->rnum;)
+		{
+			ccv_nnc_tensor_opt_t* a = (ccv_nnc_tensor_opt_t*)ccv_array_get(opt, i);
+			if (a->size == max_size)
+				++i;
+			else {
+				assert(a->size < max_size);
+				// We don't care the order, so just move the one from the end to the current position and move on.
+				if (i < opt->rnum - 1)
+					*a = *(ccv_nnc_tensor_opt_t*)ccv_array_get(opt, opt->rnum - 1);
+				--opt->rnum;
+			}
+		}
+		// Order opt array by the oc because type and size should be equal at this point.
+		_ccv_nnc_tensor_opt_sort_by_oc((ccv_nnc_tensor_opt_t*)opt->data, opt->rnum, 0);
 		// Go through opt array again, this time, it is ordered by size, therefore, if we found a place to insert, we are good.
 		int min_y = 0, min_x = tensor_block_size + 1, min_i = -1, min_hop = exec_dep->rows * 3;
 		uint64_t min_val[2] = {
