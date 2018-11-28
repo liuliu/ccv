e4177576b9e99faa32f312cf34504b2d4075f294
Liu Liu <i@liuliu.me>
2018-11-28 14:20:11 -0500

    Add missing target.

---
+++ test/int/nnc/nccl.tests.c
@@ -0,0 +1,19 @@
+#include "case.h"
+#include "ccv_case.h"
+#include "ccv_nnc_case.h"
+#include <ccv.h>
+#include <nnc/ccv_nnc.h>
+#include <nnc/ccv_nnc_easy.h>
+#include <3rdparty/dsfmt/dSFMT.h>
+
+TEST_SETUP()
+{
+	ccv_nnc_init();
+}
+
+TEST_CASE("nccl with allreduce")
+{
+	GUARD_ELSE_RETURN(1);
+}
+
+#include "case_main.h"
