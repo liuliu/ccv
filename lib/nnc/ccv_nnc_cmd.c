30c4ab1c7208be4e521d4d75d199499337371a77
Liu Liu <i@liuliu.me>
2018-11-28 05:02:42 -0500

    assert if tensor memory is 0.

--- lib/nnc/ccv_nnc_cmd.c
+++ lib/nnc/ccv_nnc_cmd.c
@@ -282,10 +282,12 @@ uint32_t ccv_nnc_cmd_find_backend(const ccv_nnc_cmd_t cmd, const int tensor_memo
 {
 	if (cmd.cmd == CCV_NNC_NOOP ||
 		cmd.cmd == CCV_NNC_GRAPH_FORWARD || cmd.cmd == CCV_NNC_GRAPH_BACKWARD ||
-		cmd.cmd == CCV_NNC_CUSTOM_FORWARD || cmd.cmd == CCV_NNC_CUSTOM_BACKWARD)
+		cmd.cmd == CCV_NNC_CUSTOM_FORWARD || cmd.cmd == CCV_NNC_CUSTOM_BACKWARD ||
+		tensor_memory == 0 || tensor_formats == 0 || tensor_datatypes == 0)
 		return cmd.backend;
 	const int cmd_idx = _ccv_nnc_cmd_ph(cmd.cmd);
 	assert(cmd_idx >= 0 && cmd_idx < sizeof(init_map) / sizeof(init_map[0]));
+	assert(tensor_memory != 0 && tensor_formats != 0 && tensor_datatypes != 0);
 	int i;
 	for (i = 0; i < CCV_NNC_BACKEND_COUNT; i++)
 	{
