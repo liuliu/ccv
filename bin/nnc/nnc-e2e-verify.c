ae4969934bff00f7938c06d428195ab48f2853b3
Liu Liu <i@liuliu.me>
2019-01-23 01:05:15 -0500

    Fix more compile errors regarding the API changes.

--- bin/nnc/nnc-e2e-verify.c
+++ bin/nnc/nnc-e2e-verify.c
@@ -27,17 +27,17 @@ static ccv_nnc_graph_t* ccv_nnc_simple_graph(ccv_convnet_t* convnet, ccv_nnc_ten
 		if (i < convnet->count - 1)
 		{
 			if (layer->type == CCV_CONVNET_FULL_CONNECT)
-				tensor = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(rows * cols * partition), 0);
+				tensor = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, rows * cols * partition), 0);
 			else
-				tensor = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(rows, cols, (layer->type == CCV_CONVNET_CONVOLUTIONAL ? layer->net.convolutional.count : layer->input.matrix.channels)), 0);
+				tensor = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, rows, cols, (layer->type == CCV_CONVNET_CONVOLUTIONAL ? layer->net.convolutional.count : layer->input.matrix.channels)), 0);
 			ccv_array_push(tensors, &tensor);
 		}
 		ccv_nnc_graph_exec_t exec = {0};
 		if (layer->type == CCV_CONVNET_CONVOLUTIONAL)
 		{
-			ccv_nnc_tensor_t* w = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(layer->net.convolutional.count, layer->net.convolutional.rows, layer->net.convolutional.cols, layer->net.convolutional.channels), 0);
+			ccv_nnc_tensor_t* w = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, layer->net.convolutional.count, layer->net.convolutional.rows, layer->net.convolutional.cols, layer->net.convolutional.channels), 0);
 			memcpy(w->data.f32, layer->w, layer->wnum * sizeof(float));
-			ccv_nnc_tensor_t* bias = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(layer->net.convolutional.count), 0);
+			ccv_nnc_tensor_t* bias = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, layer->net.convolutional.count), 0);
 			memcpy(bias->data.f32, layer->bias, layer->net.convolutional.count * sizeof(float));
 			ccv_array_push(tensors, &w);
 			ccv_array_push(tensors, &bias);
@@ -50,9 +50,9 @@ static ccv_nnc_graph_t* ccv_nnc_simple_graph(ccv_convnet_t* convnet, ccv_nnc_ten
 			ccv_nnc_hint_t hint = ccv_nnc_hint_auto(cmd.info, input->info, tensor->info);
 			exec = ccv_nnc_graph_exec_new(vgg, cmd, hint, TENSOR_LIST(input), TENSOR_LIST(tensor));
 		} else if (layer->type == CCV_CONVNET_FULL_CONNECT) {
-			ccv_nnc_tensor_t* w = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(layer->net.full_connect.count, layer->input.node.count), 0);
+			ccv_nnc_tensor_t* w = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, layer->net.full_connect.count, layer->input.node.count), 0);
 			memcpy(w->data.f32, layer->w, layer->wnum * sizeof(float));
-			ccv_nnc_tensor_t* bias = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(layer->net.full_connect.count), 0);
+			ccv_nnc_tensor_t* bias = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, layer->net.full_connect.count), 0);
 			memcpy(bias->data.f32, layer->bias, layer->net.full_connect.count * sizeof(float));
 			ccv_array_push(tensors, &w);
 			ccv_array_push(tensors, &bias);
@@ -60,7 +60,7 @@ static ccv_nnc_graph_t* ccv_nnc_simple_graph(ccv_convnet_t* convnet, ccv_nnc_ten
 			// If the input is not what I expected (array), reshape it.
 			if (input->info.dim[0] != ccv_nnc_tensor_count(input->info))
 			{
-				input = ccv_nnc_tensor_new(input->data.u8, ONE_CPU_TENSOR(ccv_nnc_tensor_count(input->info)), 0);
+				input = ccv_nnc_tensor_new(input->data.u8, CPU_TENSOR_NHWC(32F, ccv_nnc_tensor_count(input->info)), 0);
 				ccv_array_push(tensors, &input);
 			}
 			cmd = ccv_nnc_cmd_autotune(cmd, 0, ccv_nnc_no_hint, 0, TENSOR_LIST(input, w, bias), TENSOR_LIST(tensor), 0);
@@ -109,7 +109,7 @@ int main(int argc, char** argv)
 		unsigned int elapsed_time = get_current_time();
 		ccv_convnet_encode(convnet, &sliced, &b, 1);
 		printf("ccv_convnet_encode %u ms\n", get_current_time() - elapsed_time);
-		ccv_nnc_tensor_t* c = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(1000), 0);
+		ccv_nnc_tensor_t* c = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 1000), 0);
 		ccv_nnc_graph_exec_t source, dest;
 		ccv_array_t* tensors = ccv_array_new(sizeof(ccv_nnc_tensor_t*), 1, 0);
 		ccv_nnc_graph_t* graph = ccv_nnc_simple_graph(convnet, (ccv_nnc_tensor_t*)sliced, c, &source, &dest, tensors);
--- bin/nnc/nnc-sym.c
+++ bin/nnc/nnc-sym.c
@@ -9,11 +9,11 @@ int main(int argc, char** argv)
 	ccv_nnc_init();
 	ccv_nnc_symbolic_graph_t* graph = ccv_nnc_symbolic_graph_new();
 	// Input tensor
-	ccv_nnc_tensor_symbol_t a = ccv_nnc_tensor_symbol_new(graph, ONE_CPU_TENSOR(223, 223, 3), "a");
-	ccv_nnc_tensor_t* tensor_a = ccv_nnc_tensor_new(0, ONE_CPU_TENSOR(223, 223, 3), 0);
+	ccv_nnc_tensor_symbol_t a = ccv_nnc_tensor_symbol_new(graph, CPU_TENSOR_NHWC(32F, 223, 223, 3), "a");
+	ccv_nnc_tensor_t* tensor_a = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 223, 223, 3), 0);
 	// conv1
-	ccv_nnc_tensor_symbol_t conv1w = ccv_nnc_tensor_symbol_new(graph, ONE_CPU_TENSOR(64, 7, 7, 3), "conv1w");
-	ccv_nnc_tensor_symbol_t conv1b = ccv_nnc_tensor_symbol_new(graph, ONE_CPU_TENSOR(64), "conv1b");
+	ccv_nnc_tensor_symbol_t conv1w = ccv_nnc_tensor_symbol_new(graph, CPU_TENSOR_NHWC(32F, 64, 7, 7, 3), "conv1w");
+	ccv_nnc_tensor_symbol_t conv1b = ccv_nnc_tensor_symbol_new(graph, CPU_TENSOR_NHWC(32F, 64), "conv1b");
 	ccv_nnc_tensor_symbol_t b[9];
 	b[0] = ccv_nnc_tensor_symbol_new(graph, ccv_nnc_tensor_auto, "b0");
 	ccv_nnc_graph_exec_symbol_t conv1 = ccv_nnc_graph_exec_symbol_new(graph, CMD_CONVOLUTION_FORWARD(1, 64, 7, 7, 3), TENSOR_SYMBOL_LIST(a, conv1w, conv1b), TENSOR_SYMBOL_LIST(b[0]), "conv1");
@@ -29,9 +29,9 @@ int main(int argc, char** argv)
 	// conv2(x3)
 	b[3] = ccv_nnc_tensor_symbol_new(graph, ccv_nnc_tensor_auto, "b3");
 	ccv_nnc_tensor_symbol_t conv2w[3];
-	conv2w[0] = ccv_nnc_tensor_symbol_new(graph, ONE_CPU_TENSOR(64, 3, 3, 64), "conv2w");
+	conv2w[0] = ccv_nnc_tensor_symbol_new(graph, CPU_TENSOR_NHWC(32F, 64, 3, 3, 64), "conv2w");
 	ccv_nnc_tensor_symbol_t conv2b[3];
-	conv2b[0] = ccv_nnc_tensor_symbol_new(graph, ONE_CPU_TENSOR(64), "conv2b");
+	conv2b[0] = ccv_nnc_tensor_symbol_new(graph, CPU_TENSOR_NHWC(32F, 64), "conv2b");
 	ccv_nnc_graph_exec_symbol_t conv2[3];
 	conv2[0] = ccv_nnc_graph_exec_symbol_new(graph, CMD_CONVOLUTION_FORWARD(1, 64, 3, 3, 64), TENSOR_SYMBOL_LIST(b[2], conv2w[0], conv2b[0]), TENSOR_SYMBOL_LIST(b[3]), "conv2");
 	ccv_nnc_graph_exec_symbol_concat(graph, relu1, conv2[0]);
@@ -43,8 +43,8 @@ int main(int argc, char** argv)
 	for (i = 1; i < 3; i++)
 	{
 		b[3 + i * 2] = ccv_nnc_tensor_symbol_new(graph, ccv_nnc_tensor_auto, "bn");
-		conv2w[i] = ccv_nnc_tensor_symbol_new(graph, ONE_CPU_TENSOR(64, 3, 3, 64), "conv2wn");
-		conv2b[i] = ccv_nnc_tensor_symbol_new(graph, ONE_CPU_TENSOR(64), "conv2bn");
+		conv2w[i] = ccv_nnc_tensor_symbol_new(graph, CPU_TENSOR_NHWC(32F, 64, 3, 3, 64), "conv2wn");
+		conv2b[i] = ccv_nnc_tensor_symbol_new(graph, CPU_TENSOR_NHWC(32F, 64), "conv2bn");
 		conv2[i] = ccv_nnc_graph_exec_symbol_new(graph, CMD_CONVOLUTION_FORWARD(1, 64, 3, 3, 64), TENSOR_SYMBOL_LIST(b[2 + i * 2], conv2w[i], conv2b[i]), TENSOR_SYMBOL_LIST(b[3 + i * 2]), "conv2n");
 		ccv_nnc_graph_exec_symbol_concat(graph, relu2[i - 1], conv2[i]);
 		b[4 + i * 2] = ccv_nnc_tensor_symbol_new(graph, ccv_nnc_tensor_auto, "bn");
