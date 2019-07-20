6063e9afbe1ee16c62cb0c2ec1c7c6662c464183
Liu Liu <i@liuliu.me>
2019-07-20 01:00:44 -0400

    Add no padding variant.

--- lib/nnc/cmd/compression/gpu/ccv_nnc_lssc_gpu_ref.cu
+++ lib/nnc/cmd/compression/gpu/ccv_nnc_lssc_gpu_ref.cu
@@ -7,6 +7,66 @@ extern "C" {
 }
 #include <nnc/gpu/ccv_nnc_compat.h>
 
+__global__ void _ccv_nnc_lssc_nchw_forw_kernel_no_padding(const int X_H, const int X_W, const int Y_H, const int Y_W, const __half* const X, __half* const Y)
+{
+	const int X_HxW = X_H * X_W;
+	const int Y_HxW = Y_H * Y_W;
+	const int Y_W4 = Y_W / 4;
+	const int Y_HxW4 = Y_H * Y_W4;
+	const __half* const Xp = X + X_HxW * blockIdx.x;
+	__half* const Yp = Y + Y_HxW * blockIdx.x;
+	for (size_t idx = threadIdx.x; idx < Y_HxW4; idx += blockDim.x)
+	{
+		const int y = idx / Y_W4;
+		const int x = idx % Y_W4;
+		const __half* const Xpz = Xp + (x + y * X_W) * 4;
+		__half X16_0 = 0;
+#if __CUDA_ARCH__ >= 350
+		X16_0 = __ldg(Xpz);
+#else
+		X16_0 = Xpz[0];
+#endif
+		float X16[16];
+		float Xmax = (float)X16_0;
+		float Xmin = Xmax;
+#pragma unroll
+		for (int i = 0; i < 16; i++)
+			X16[i] = Xmax;
+#pragma unroll
+		for (int i = 0; i < 4; i++)
+#pragma unroll
+			for (int j = 0; j < 4; j++)
+#if __CUDA_ARCH__ >= 350
+				X16[i * 4 + j] = (float)__ldg(Xpz + i * X_W + j);
+#else
+				X16[i * 4 + j] = (float)Xpz[i * X_W + j];
+#endif
+#pragma unroll
+		for (int i = 1; i < 16; i++)
+			Xmax = max(Xmax, X16[i]), Xmin = min(Xmin, X16[i]);
+		const float Xbottom = Xmin * 7 / 6 - Xmax / 6;
+		const float Xscale = 3 / max(Xmax - Xmin, 1e-6);
+		uint16_t m0 = 0, m1 = 0;
+#pragma unroll
+		for (int i = 0; i < 8; i++)
+		{
+			const int v = (int)((X16[i] - Xbottom) * Xscale);
+			m0 |= (min(max(v, 0), 3) << (i << 1));
+		}
+#pragma unroll
+		for (int i = 0; i < 8; i++)
+		{
+			const int v = (int)((X16[8 + i] - Xbottom) * Xscale);
+			m1 |= (min(max(v, 0), 3) << (i << 1));
+		}
+		__half* const Ypz = Yp + y * Y_W + x * 4;
+		Ypz[0] = Xmin;
+		Ypz[1] = Xmax;
+		((uint16_t*)Ypz)[2] = m0;
+		((uint16_t*)Ypz)[3] = m1;
+	}
+}
+
 __global__ void _ccv_nnc_lssc_nchw_forw_kernel(const int X_H, const int X_W, const int Y_H, const int Y_W, const __half* const X, __half* const Y)
 {
 	const int X_HxW = X_H * X_W;
@@ -34,7 +94,9 @@ __global__ void _ccv_nnc_lssc_nchw_forw_kernel(const int X_H, const int X_W, con
 			X16[i] = Xmax;
 		const int xh = min(y * 4 + 4, X_H) - y * 4;
 		const int xw = min(x * 4 + 4, X_W) - x * 4;
+#pragma unroll
 		for (int i = 0; i < xh; i++)
+#pragma unroll
 			for (int j = 0; j < xw; j++)
 #if __CUDA_ARCH__ >= 350
 				X16[i * 4 + j] = (float)__ldg(Xpz + i * X_W + j);
@@ -88,9 +150,59 @@ static int _ccv_nnc_lssc_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint
 		assert(n == ccv_nnc_tensor_get_n(b->info));
 		const int c = ccv_nnc_tensor_get_c(a->info);
 		assert(c == ccv_nnc_tensor_get_c(b->info));
-		_ccv_nnc_lssc_nchw_forw_kernel<<<n * c, CUDA_NUM_THREADS, 0, stream>>>(adim[1], adim[CCV_NNC_MAX_DIM], bdim[1], bdim[CCV_NNC_MAX_DIM], (__half*)a->data.f16, (__half*)b->data.f16);
+		if (adim[1] % 4 == 0 && adim[CCV_NNC_MAX_DIM] % 4 == 0)
+			_ccv_nnc_lssc_nchw_forw_kernel_no_padding<<<n * c, CUDA_NUM_THREADS, 0, stream>>>(adim[1], adim[CCV_NNC_MAX_DIM], bdim[1], bdim[CCV_NNC_MAX_DIM], (__half*)a->data.f16, (__half*)b->data.f16);
+		else
+			_ccv_nnc_lssc_nchw_forw_kernel<<<n * c, CUDA_NUM_THREADS, 0, stream>>>(adim[1], adim[CCV_NNC_MAX_DIM], bdim[1], bdim[CCV_NNC_MAX_DIM], (__half*)a->data.f16, (__half*)b->data.f16);
 	}
 	return CCV_NNC_EXEC_SUCCESS;
+}
+
+__global__ void _ccv_nnc_lssc_nchw_back_kernel_no_padding(const int Y_H, const int Y_W, const int X_H, const int X_W, const __half* const Y, __half* const X)
+{
+	const int X_HxW = X_H * X_W;
+	const int Y_HxW = Y_H * Y_W;
+	const int Y_W4 = Y_W / 4;
+	const int Y_HxW4 = Y_H * Y_W4;
+	__half* const Xp = X + X_HxW * blockIdx.x;
+	const __half* const Yp = Y + Y_HxW * blockIdx.x;
+	for (size_t idx = threadIdx.x; idx < Y_HxW4; idx += blockDim.x)
+	{
+		const int y = idx / Y_W4;
+		const int x = idx % Y_W4;
+		const __half* const Ypz = Yp + y * Y_W + x * 4;
+		__half X4[4];
+		uint16_t m0;
+		uint16_t m1;
+#if __CUDA_ARCH__ >= 350
+		X4[0] = __ldg(Ypz);
+		X4[3] = __ldg(Ypz + 1);
+		m0 = __ldg((uint16_t*)Ypz + 2);
+		m1 = __ldg((uint16_t*)Ypz + 3);
+#else
+		X4[0] = Ypz[0];
+		X4[3] = Ypz[1];
+		m0 = ((uint16_t*)Ypz)[2];
+		m1 = ((uint16_t*)Ypz)[3];
+#endif
+		const float Xmin = (float)X4[0];
+		const float Xmax = (float)X4[3];
+		X4[1] = (__half)(Xmax / 3 + Xmin * 2 / 3);
+		X4[2] = (__half)(Xmax * 2 / 3 + Xmin / 3);
+		__half X16[16];
+#pragma unroll
+		for (int i = 0; i < 8; i++)
+			X16[i] = X4[((m0 >> (i << 1)) & 3)];
+#pragma unroll
+		for (int i = 0; i < 8; i++)
+			X16[8 + i] = X4[((m1 >> (i << 1)) & 3)];
+		__half* const Xpz = Xp + (x + y * X_W) * 4;
+#pragma unroll
+		for (int i = 0; i < 4; i++)
+#pragma unroll
+			for (int j = 0; j < 4; j++)
+				Xpz[i * X_W + j] = X16[i * 4 + j];
+	}
 }
 
 __global__ void _ccv_nnc_lssc_nchw_back_kernel(const int Y_H, const int Y_W, const int X_H, const int X_W, const __half* const Y, __half* const X)
@@ -134,7 +246,9 @@ __global__ void _ccv_nnc_lssc_nchw_back_kernel(const int Y_H, const int Y_W, con
 		__half* const Xpz = Xp + (x + y * X_W) * 4;
 		const int xh = min(y * 4 + 4, X_H) - y * 4;
 		const int xw = min(x * 4 + 4, X_W) - x * 4;
+#pragma unroll
 		for (int i = 0; i < xh; i++)
+#pragma unroll
 			for (int j = 0; j < xw; j++)
 				Xpz[i * X_W + j] = X16[i * 4 + j];
 	}
@@ -161,7 +275,10 @@ static int _ccv_nnc_lssc_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint
 		assert(n == ccv_nnc_tensor_get_n(b->info));
 		const int c = ccv_nnc_tensor_get_c(a->info);
 		assert(c == ccv_nnc_tensor_get_c(b->info));
-		_ccv_nnc_lssc_nchw_back_kernel<<<n * c, CUDA_NUM_THREADS, 0, stream>>>(adim[1], adim[CCV_NNC_MAX_DIM], bdim[1], bdim[CCV_NNC_MAX_DIM], (__half*)a->data.f16, (__half*)b->data.f16);
+		if (bdim[1] % 4 == 0 && bdim[CCV_NNC_MAX_DIM] % 4 == 0)
+			_ccv_nnc_lssc_nchw_back_kernel_no_padding<<<n * c, CUDA_NUM_THREADS, 0, stream>>>(adim[1], adim[CCV_NNC_MAX_DIM], bdim[1], bdim[CCV_NNC_MAX_DIM], (__half*)a->data.f16, (__half*)b->data.f16);
+		else
+			_ccv_nnc_lssc_nchw_back_kernel<<<n * c, CUDA_NUM_THREADS, 0, stream>>>(adim[1], adim[CCV_NNC_MAX_DIM], bdim[1], bdim[CCV_NNC_MAX_DIM], (__half*)a->data.f16, (__half*)b->data.f16);
 	}
 	return CCV_NNC_EXEC_SUCCESS;
 }
--- test/int/nnc/compression.tests.c
+++ test/int/nnc/compression.tests.c
@@ -11,47 +11,43 @@ TEST_SETUP()
 	ccv_nnc_init();
 }
 
-TEST_CASE("LSSC should give exact result from GPU")
+static void prepare_nchw(const int N, const int C, const int H, const int W, ccv_nnc_tensor_t** const c_ptr, ccv_nnc_tensor_t** const cgc_ptr)
 {
-	const int N = 128;
-	const int C = 64;
-	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_COMPRESSION_LSSC_FORWARD, CCV_NNC_BACKEND_GPU_REF) &&
-		ccv_nnc_cmd_ok(CCV_NNC_COMPRESSION_LSSC_BACKWARD, CCV_NNC_BACKEND_GPU_REF));
-	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(0, CPU_TENSOR_NCHW(32F, N, C, 113, 114), 0);
-	ccv_nnc_tensor_param_t a_params = CPU_TENSOR_NCHW(16F, N, C, 113, 114);
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(0, CPU_TENSOR_NCHW(32F, N, C, H, W), 0);
+	ccv_nnc_tensor_param_t a_params = CPU_TENSOR_NCHW(16F, N, C, H, W);
 	ccv_nnc_tensor_t* const a16 = ccv_nnc_tensor_new(0, a_params, 0);
 	ccv_nnc_tensor_param_t b_params;
 	ccv_nnc_hint_tensor_auto(CMD_COMPRESSION_LSSC_FORWARD(), &a_params, 1, ccv_nnc_no_hint, &b_params, 1);
 	ccv_nnc_tensor_t* const b16 = ccv_nnc_tensor_new(0, b_params, 0);
 	ccv_nnc_tensor_t* const c16 = ccv_nnc_tensor_new(0, a_params, 0);
-	ccv_nnc_tensor_t* const c = ccv_nnc_tensor_new(0, CPU_TENSOR_NCHW(32F, N, C, 113, 114), 0);
+	ccv_nnc_tensor_t* const c = ccv_nnc_tensor_new(0, CPU_TENSOR_NCHW(32F, N, C, H, W), 0);
 	dsfmt_t dsfmt;
 	int i;
 	dsfmt_init_gen_rand(&dsfmt, 1);
-	for (i = 0; i < N * C * 113 * 114; i++)
+	for (i = 0; i < N * C * H * W; i++)
 		a->data.f32[i] = dsfmt_genrand_open_close(&dsfmt) * 2 - 1;
-	ccv_float_to_half_precision(a->data.f32, (uint16_t*)a16->data.f16, N * C * 113 * 114);
-	ccv_half_precision_to_float((uint16_t*)a16->data.f16, a->data.f32, N * C * 113 * 114);
+	ccv_float_to_half_precision(a->data.f32, (uint16_t*)a16->data.f16, N * C * H * W);
+	ccv_half_precision_to_float((uint16_t*)a16->data.f16, a->data.f32, N * C * H * W);
 	int x, y, ix, iy;
 	for (i = 0; i < N * C; i++)
-		for (y = 0; y < 113; y += 4)
-			for (x = 0; x < 114; x += 4)
+		for (y = 0; y < H; y += 4)
+			for (x = 0; x < W; x += 4)
 			{
-				float* const ap = a->data.f32 + x + y * 114 + i * 113 * 114;
+				float* const ap = a->data.f32 + x + y * W + i * H * W;
 				float v[4] = { ap[0], ap[1], ap[0] * 2 / 3 + ap[1] / 3, ap[0] / 3 + ap[1] * 2 / 3 };
-				for (iy = 0; iy < ccv_min(y + 4, 113) - y; iy++)
-					for (ix = 0; ix < ccv_min(x + 4, 114) - x; ix++)
-						ap[iy * 114 + ix] = v[dsfmt_genrand_uint32(&dsfmt) % 4];
+				for (iy = 0; iy < ccv_min(y + 4, H) - y; iy++)
+					for (ix = 0; ix < ccv_min(x + 4, W) - x; ix++)
+						ap[iy * W + ix] = v[dsfmt_genrand_uint32(&dsfmt) % 4];
 				ap[0] = v[0];
 				ap[1] = v[1]; // Make sure we still have max min.
 			}
-	ccv_float_to_half_precision(a->data.f32, (uint16_t*)a16->data.f16, N * C * 113 * 114);
-	ccv_half_precision_to_float((uint16_t*)a16->data.f16, a->data.f32, N * C * 113 * 114);
+	ccv_float_to_half_precision(a->data.f32, (uint16_t*)a16->data.f16, N * C * H * W);
+	ccv_half_precision_to_float((uint16_t*)a16->data.f16, a->data.f32, N * C * H * W);
 	ccv_nnc_cmd_exec(CMD_COMPRESSION_LSSC_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a16), TENSOR_LIST(b16), 0);
 	ccv_nnc_cmd_exec(CMD_COMPRESSION_LSSC_BACKWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(b16), TENSOR_LIST(c16), 0);
-	ccv_half_precision_to_float((uint16_t*)c16->data.f16, c->data.f32, N * C * 113 * 114);
+	ccv_half_precision_to_float((uint16_t*)c16->data.f16, c->data.f32, N * C * H * W);
 	// Compare against GPU computation
-	ccv_nnc_tensor_param_t ag_params = GPU_TENSOR_NCHW(000, 16F, N, C, 113, 114);
+	ccv_nnc_tensor_param_t ag_params = GPU_TENSOR_NCHW(000, 16F, N, C, H, W);
 	ccv_nnc_tensor_t* const a16g = ccv_nnc_tensor_new(0, ag_params, 0);
 	ccv_nnc_tensor_param_t bg_params;
 	ccv_nnc_hint_tensor_auto(CMD_COMPRESSION_LSSC_FORWARD(), &ag_params, 1, ccv_nnc_no_hint, &bg_params, 1);
@@ -60,20 +56,49 @@ TEST_CASE("LSSC should give exact result from GPU")
 	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a16), TENSOR_LIST(a16g), 0);
 	ccv_nnc_cmd_exec(CMD_COMPRESSION_LSSC_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a16g), TENSOR_LIST(b16g), 0);
 	ccv_nnc_cmd_exec(CMD_COMPRESSION_LSSC_BACKWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(b16g), TENSOR_LIST(c16g), 0);
-	memset(c16->data.f16, 0, sizeof(ccv_float16_t) * N * C * 113 * 114);
+	memset(c16->data.f16, 0, sizeof(ccv_float16_t) * N * C * H * W);
 	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(c16g), TENSOR_LIST(c16), 0);
-	ccv_nnc_tensor_t* const cgc = ccv_nnc_tensor_new(0, CPU_TENSOR_NCHW(32F, N, C, 113, 114), 0);
-	ccv_half_precision_to_float((uint16_t*)c16->data.f16, cgc->data.f32, N * C * 113 * 114);
-	REQUIRE_ARRAY_EQ_WITH_TOLERANCE(float, c->data.f32, cgc->data.f32, N * C * 113 * 114, 1e-3, "GPU and CPU computed result should match");
+	ccv_nnc_tensor_t* const cgc = ccv_nnc_tensor_new(0, CPU_TENSOR_NCHW(32F, N, C, H, W), 0);
+	ccv_half_precision_to_float((uint16_t*)c16->data.f16, cgc->data.f32, N * C * H * W);
+	*c_ptr = c;
+	*cgc_ptr = cgc;
 	ccv_nnc_tensor_free(a);
-	ccv_nnc_tensor_free(c);
-	ccv_nnc_tensor_free(cgc);
 	ccv_nnc_tensor_free(a16);
 	ccv_nnc_tensor_free(b16);
 	ccv_nnc_tensor_free(c16);
 	ccv_nnc_tensor_free(a16g);
 	ccv_nnc_tensor_free(b16g);
 	ccv_nnc_tensor_free(c16g);
+}
+
+TEST_CASE("LSSC should give exact result from GPU for 128x512x14x14")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_COMPRESSION_LSSC_FORWARD, CCV_NNC_BACKEND_GPU_REF) &&
+		ccv_nnc_cmd_ok(CCV_NNC_COMPRESSION_LSSC_BACKWARD, CCV_NNC_BACKEND_GPU_REF));
+	ccv_nnc_tensor_t* c;
+	ccv_nnc_tensor_t* cgc;
+	prepare_nchw(128, 512, 14, 14, &c, &cgc);
+	REQUIRE_ARRAY_EQ_WITH_TOLERANCE(float, c->data.f32, cgc->data.f32, 128 * 512 * 14 * 14, 1e-3, "GPU and CPU computed result should match");
+}
+
+TEST_CASE("LSSC should give exact result from GPU for 128x64x113x114")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_COMPRESSION_LSSC_FORWARD, CCV_NNC_BACKEND_GPU_REF) &&
+		ccv_nnc_cmd_ok(CCV_NNC_COMPRESSION_LSSC_BACKWARD, CCV_NNC_BACKEND_GPU_REF));
+	ccv_nnc_tensor_t* c;
+	ccv_nnc_tensor_t* cgc;
+	prepare_nchw(128, 64, 113, 114, &c, &cgc);
+	REQUIRE_ARRAY_EQ_WITH_TOLERANCE(float, c->data.f32, cgc->data.f32, 128 * 64 * 113 * 114, 1e-3, "GPU and CPU computed result should match");
+}
+
+TEST_CASE("LSSC should give exact result from GPU for 128x256x28x28")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_COMPRESSION_LSSC_FORWARD, CCV_NNC_BACKEND_GPU_REF) &&
+		ccv_nnc_cmd_ok(CCV_NNC_COMPRESSION_LSSC_BACKWARD, CCV_NNC_BACKEND_GPU_REF));
+	ccv_nnc_tensor_t* c;
+	ccv_nnc_tensor_t* cgc;
+	prepare_nchw(128, 256, 28, 28, &c, &cgc);
+	REQUIRE_ARRAY_EQ_WITH_TOLERANCE(float, c->data.f32, cgc->data.f32, 128 * 256 * 28 * 28, 1e-3, "GPU and CPU computed result should match");
 }
 
 #include "case_main.h"
