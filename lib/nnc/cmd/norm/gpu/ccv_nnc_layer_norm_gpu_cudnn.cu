c57e6bbc80d044699b2b73305a022ffc997eb872
Liu Liu <i@liuliu.me>
2019-10-09 18:09:33 -0400

    Add gradient in cudnn.

--- lib/nnc/cmd/norm/gpu/ccv_nnc_layer_norm_gpu_cudnn.cu
+++ lib/nnc/cmd/norm/gpu/ccv_nnc_layer_norm_gpu_cudnn.cu
@@ -26,13 +26,17 @@ static int _ccv_nnc_layer_norm_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_
 	assert(output_size == 3);
 	const ccv_nnc_cudnn_tensor_view_descriptor_t a = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[0]);
 	assert(!CCV_IS_TENSOR_VIEW(inputs[1]));
+	assert(inputs[1]->info.datatype == CCV_32F);
 	const ccv_nnc_cudnn_tensor_view_descriptor_t scale = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[1]);
 	assert(!CCV_IS_TENSOR_VIEW(inputs[2]));
+	assert(inputs[2]->info.datatype == CCV_32F);
 	const ccv_nnc_cudnn_tensor_view_descriptor_t bias = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[2]);
 	const ccv_nnc_cudnn_tensor_view_descriptor_t b = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)outputs[0]);
 	assert(!CCV_IS_TENSOR_VIEW(outputs[1]));
+	assert(outputs[1]->info.datatype == CCV_32F);
 	const ccv_nnc_cudnn_tensor_view_descriptor_t saved_mean = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)outputs[1]);
 	assert(!CCV_IS_TENSOR_VIEW(outputs[2]));
+	assert(outputs[2]->info.datatype == CCV_32F);
 	const ccv_nnc_cudnn_tensor_view_descriptor_t saved_inv_std = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)outputs[2]);
 	int adim[CCV_NNC_MAX_DIM + 2];
 	int rdim[CCV_NNC_MAX_DIM + 2];
@@ -59,10 +63,9 @@ static int _ccv_nnc_layer_norm_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_
 	CUDNN_ENFORCE(cudnnGetReductionWorkspaceSize(cudnn, reduce, a.descriptor, saved_mean.descriptor, &saved_mean_workspace_size));
 	cudnnSetReduceTensorDescriptor(reduce, CUDNN_REDUCE_TENSOR_NORM2, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN, CUDNN_REDUCE_TENSOR_NO_INDICES, CUDNN_32BIT_INDICES);
 	CUDNN_ENFORCE(cudnnGetReductionWorkspaceSize(cudnn, reduce, b.descriptor, saved_inv_std.descriptor, &saved_inv_std_workspace_size));
-	const size_t workspace_size = ccv_max(saved_mean_workspace_size, saved_inv_std_workspace_size);
-	void* workspace = 0;
-	if (workspace_size)
-		workspace = ccv_nnc_stream_context_get_workspace(stream_context, workspace_size, CCV_TENSOR_GPU_MEMORY);
+	const size_t workspace_size = ccv_max(saved_mean_workspace_size, saved_inv_std_workspace_size) + sizeof(float) * rcount;
+	uint8_t* const workspace = (uint8_t*)ccv_nnc_stream_context_get_workspace(stream_context, workspace_size, CCV_TENSOR_GPU_MEMORY);
+	float* const sisbp = (float*)(workspace + ccv_max(saved_mean_workspace_size, saved_inv_std_workspace_size));
 	cudnnSetReduceTensorDescriptor(reduce, CUDNN_REDUCE_TENSOR_ADD, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN, CUDNN_REDUCE_TENSOR_NO_INDICES, CUDNN_32BIT_INDICES);
 	CUDNN_ENFORCE(cudnnReduceTensor(cudnn, reduce, 0, 0, workspace, workspace_size, &inv_n, a.descriptor, a.data.u8, &zero, saved_mean.descriptor, saved_mean.data.u8));
 	cudnnOpTensorDescriptor_t op = ccv_nnc_stream_context_get_op_tensor_descriptor(stream_context);
@@ -73,14 +76,12 @@ static int _ccv_nnc_layer_norm_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_
 	CUDNN_ENFORCE(cudnnReduceTensor(cudnn, reduce, 0, 0, workspace, workspace_size, &inv_n_sqrt, b.descriptor, b.data.u8, &zero, saved_inv_std.descriptor, saved_inv_std.data.u8));
 	// The epsilon is used a little bit differently from batch norm, it is outside of the sqrt in this case.
 	const float epsilon = cmd.info.lnorm.epsilon;
-	assert(outputs[2]->info.datatype == CCV_16F || outputs[2]->info.datatype == CCV_32F);
-	if (outputs[2]->info.datatype == CCV_16F)
-		_ccv_nnc_inv_std_kernel<<<CUDA_GET_BLOCKS(rcount), CUDA_NUM_THREADS, 0, stream>>>(rcount, epsilon, (__half*)saved_inv_std.data.f16, (__half*)saved_inv_std.data.f16);
-	else
-		_ccv_nnc_inv_std_kernel<<<CUDA_GET_BLOCKS(rcount), CUDA_NUM_THREADS, 0, stream>>>(rcount, epsilon, saved_inv_std.data.f32, saved_inv_std.data.f32);
+	_ccv_nnc_inv_std_kernel<<<CUDA_GET_BLOCKS(rcount), CUDA_NUM_THREADS, 0, stream>>>(rcount, epsilon, saved_inv_std.data.f32, saved_inv_std.data.f32);
+	const ccv_nnc_tensor_t sisbt = ccv_nnc_tensor(sisbp, inputs[1]->info, 0);
+	const ccv_nnc_cudnn_tensor_view_descriptor_t sisb = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)&sisbt);
 	cudnnSetOpTensorDescriptor(op, CUDNN_OP_TENSOR_MUL, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN);
-	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, b.descriptor, b.data.u8, &one, saved_inv_std.descriptor, saved_inv_std.data.u8, &zero, b.descriptor, b.data.u8));
-	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, b.descriptor, b.data.u8, &one, scale.descriptor, scale.data.u8, &zero, b.descriptor, b.data.u8));
+	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, saved_inv_std.descriptor, saved_inv_std.data.u8, &one, scale.descriptor, scale.data.u8, &zero, sisb.descriptor, sisb.data.u8));
+	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, b.descriptor, b.data.u8, &one, sisb.descriptor, sisb.data.u8, &zero, b.descriptor, b.data.u8));
 	cudnnSetOpTensorDescriptor(op, CUDNN_OP_TENSOR_ADD, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN);
 	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, b.descriptor, b.data.u8, &one, bias.descriptor, bias.data.u8, &zero, b.descriptor, b.data.u8));
 	ccv_nnc_stream_context_return_reduce_tensor_descriptor(stream_context, reduce);
@@ -88,6 +89,10 @@ static int _ccv_nnc_layer_norm_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_
 	ccv_nnc_cudnn_deinit_tensor_view_descriptor(a);
 	ccv_nnc_cudnn_deinit_tensor_view_descriptor(b);
 	ccv_nnc_cudnn_deinit_tensor_view_descriptor(scale);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(bias);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(saved_mean);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(saved_inv_std);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(sisb);
 	return CCV_NNC_EXEC_SUCCESS;
 }
 
@@ -95,20 +100,84 @@ static int _ccv_nnc_layer_norm_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_
 {
 	assert(input_size == 9);
 	assert(output_size == 3);
-	// cudnnHandle_t cudnn = ccv_nnc_stream_context_get_cudnn(stream_context);
-	// static const float one = 1, zero = 0;
+	cudnnHandle_t cudnn = ccv_nnc_stream_context_get_cudnn(stream_context);
 	const ccv_nnc_cudnn_tensor_view_descriptor_t g = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[0]);
-	const ccv_nnc_cudnn_tensor_view_descriptor_t a = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[5]);
+	const ccv_nnc_cudnn_tensor_view_descriptor_t a = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[3]);
 	const ccv_nnc_cudnn_tensor_view_descriptor_t h = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)outputs[0]);
-	const ccv_nnc_cudnn_tensor_view_descriptor_t scale = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[6]);
-	// ccv_nnc_tensor_t* const saved_mean = inputs[13];
-	// ccv_nnc_tensor_t* const saved_inv_std = inputs[14];
-	// ccv_nnc_tensor_t* const dscale = outputs[1];
-	// ccv_nnc_tensor_t* const dbias = outputs[2];
+	assert(!CCV_IS_TENSOR_VIEW(inputs[4]));
+	const ccv_nnc_cudnn_tensor_view_descriptor_t scale = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[4]);
+	assert(!CCV_IS_TENSOR_VIEW(inputs[7]));
+	const ccv_nnc_cudnn_tensor_view_descriptor_t saved_mean = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[7]);
+	assert(!CCV_IS_TENSOR_VIEW(inputs[8]));
+	const ccv_nnc_cudnn_tensor_view_descriptor_t saved_inv_std = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)inputs[8]);
+	assert(!CCV_IS_TENSOR_VIEW(outputs[1]));
+	const ccv_nnc_cudnn_tensor_view_descriptor_t dscale = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)outputs[1]);
+	assert(!CCV_IS_TENSOR_VIEW(outputs[2]));
+	const ccv_nnc_cudnn_tensor_view_descriptor_t dbias = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)outputs[2]);
+	int gdim[CCV_NNC_MAX_DIM + 2];
+	int rdim[CCV_NNC_MAX_DIM + 2];
+	ccv_nnc_tensor_view_get_dim((ccv_nnc_tensor_view_t*)inputs[0], gdim);
+	ccv_nnc_tensor_view_get_dim((ccv_nnc_tensor_view_t*)inputs[4], rdim);
+	assert(ccv_nnc_tensor_view_check_dim((ccv_nnc_tensor_view_t*)inputs[7], rdim));
+	assert(ccv_nnc_tensor_view_check_dim((ccv_nnc_tensor_view_t*)inputs[8], rdim));
+	assert(ccv_nnc_tensor_view_check_dim((ccv_nnc_tensor_view_t*)outputs[1], rdim));
+	assert(ccv_nnc_tensor_view_check_dim((ccv_nnc_tensor_view_t*)outputs[2], rdim));
+	assert(ccv_nnc_tensor_view_check_dim((ccv_nnc_tensor_view_t*)inputs[3], gdim));
+	assert(ccv_nnc_tensor_view_check_dim((ccv_nnc_tensor_view_t*)outputs[0], gdim));
+	static const float one = 1, zero = 0, neg_one = -1;
+	int x;
+	int n = 1;
+	for (x = 0; x < CCV_NNC_MAX_DIM + 2; x++)
+		n *= gdim[x];
+	for (x = 0; x < CCV_NNC_MAX_DIM + 2; x++)
+		n /= rdim[x];
+	int gcount = 1, rcount = 1;
+	for (x = 0; x < CCV_NNC_MAX_DIM + 2; x++)
+		gcount *= gdim[x], rcount *= rdim[x];
+	const float inv_n = 1. / n;
+	const float fn = n;
+	cudnnReduceTensorDescriptor_t reduce = ccv_nnc_stream_context_get_reduce_tensor_descriptor(stream_context);
+	cudnnSetReduceTensorDescriptor(reduce, CUDNN_REDUCE_TENSOR_ADD, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN, CUDNN_REDUCE_TENSOR_NO_INDICES, CUDNN_32BIT_INDICES);
+	size_t workspace_size = 0;
+	CUDNN_ENFORCE(cudnnGetReductionWorkspaceSize(cudnn, reduce, g.descriptor, dscale.descriptor, &workspace_size));
+	uint8_t* const workspace = (uint8_t*)ccv_nnc_stream_context_get_workspace(stream_context, workspace_size + sizeof(float) * gcount * 2 + sizeof(float) * rcount, CCV_TENSOR_GPU_MEMORY);
+	CUDNN_ENFORCE(cudnnReduceTensor(cudnn, reduce, 0, 0, workspace, workspace_size, &one, g.descriptor, g.data.u8, &zero, dbias.descriptor, dbias.data.u8));
+	float* const ahp = (float*)(workspace + workspace_size);
+	const ccv_nnc_tensor_t aht = ccv_nnc_tensor(ahp, inputs[0]->info, 0);
+	const ccv_nnc_cudnn_tensor_view_descriptor_t ah = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)&aht);
+	float* const ahgp = ahp + gcount;
+	const ccv_nnc_tensor_t ahgt = ccv_nnc_tensor(ahgp, inputs[0]->info, 0);
+	const ccv_nnc_cudnn_tensor_view_descriptor_t ahg = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)&ahgt);
+	float* const sisbp = ahp + gcount * 2;
+	const ccv_nnc_tensor_t sisbt = ccv_nnc_tensor(sisbp, inputs[4]->info, 0);
+	const ccv_nnc_cudnn_tensor_view_descriptor_t sisb = ccv_nnc_cudnn_get_tensor_view_descriptor(stream_context, (const ccv_nnc_tensor_view_t*)&sisbt);
+	cudnnOpTensorDescriptor_t op = ccv_nnc_stream_context_get_op_tensor_descriptor(stream_context);
+	cudnnSetOpTensorDescriptor(op, CUDNN_OP_TENSOR_ADD, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN);
+	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, a.descriptor, a.data.u8, &neg_one, saved_mean.descriptor, saved_mean.data.u8, &zero, ah.descriptor, ah.data.u8));
+	cudnnSetOpTensorDescriptor(op, CUDNN_OP_TENSOR_MUL, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN);
+	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, ah.descriptor, ah.data.u8, &one, saved_inv_std.descriptor, saved_inv_std.data.u8, &zero, ah.descriptor, ah.data.u8));
+	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, ah.descriptor, ah.data.u8, &one, g.descriptor, g.data.u8, &zero, ahg.descriptor, ahg.data.u8));
+	CUDNN_ENFORCE(cudnnReduceTensor(cudnn, reduce, 0, 0, workspace, workspace_size, &one, ahg.descriptor, ahg.data.u8, &zero, dscale.descriptor, dscale.data.u8));
+	ccv_nnc_stream_context_return_reduce_tensor_descriptor(stream_context, reduce);
+	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &inv_n, scale.descriptor, scale.data.u8, &one, saved_inv_std.descriptor, saved_inv_std.data.u8, &zero, sisb.descriptor, sisb.data.u8));
+	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, ah.descriptor, ah.data.u8, &one, dscale.descriptor, dscale.data.u8, &zero, ah.descriptor, ah.data.u8));
+	cudnnSetOpTensorDescriptor(op, CUDNN_OP_TENSOR_ADD, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN);
+	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &fn, g.descriptor, g.data.u8, &neg_one, dbias.descriptor, dbias.data.u8, &zero, ahg.descriptor, ahg.data.u8));
+	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, ahg.descriptor, ahg.data.u8, &neg_one, ah.descriptor, ah.data.u8, &zero, ahg.descriptor, ahg.data.u8));
+	cudnnSetOpTensorDescriptor(op, CUDNN_OP_TENSOR_MUL, CUDNN_DATA_FLOAT, CUDNN_PROPAGATE_NAN);
+	CUDNN_ENFORCE(cudnnOpTensor(cudnn, op, &one, ahg.descriptor, ahg.data.u8, &one, sisb.descriptor, sisb.data.u8, &zero, h.descriptor, h.data.u8));
+	ccv_nnc_stream_context_return_op_tensor_descriptor(stream_context, op);
 	ccv_nnc_cudnn_deinit_tensor_view_descriptor(g);
 	ccv_nnc_cudnn_deinit_tensor_view_descriptor(a);
 	ccv_nnc_cudnn_deinit_tensor_view_descriptor(h);
 	ccv_nnc_cudnn_deinit_tensor_view_descriptor(scale);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(saved_mean);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(saved_inv_std);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(dscale);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(dbias);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(ah);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(ahg);
+	ccv_nnc_cudnn_deinit_tensor_view_descriptor(sisb);
 	return CCV_NNC_EXEC_SUCCESS;
 }
 
