f8c3254bf247ea15c2943ff54a1542e726b554dc
Liu Liu <i@liuliu.me>
2020-08-30 23:26:02 -0400

    Add backprop, and removed suppressed ones.

--- lib/nnc/cmd/nms/ccv_nnc_nms_cpu_ref.c
+++ lib/nnc/cmd/nms/ccv_nnc_nms_cpu_ref.c
@@ -89,6 +89,21 @@ static int _ccv_nnc_nms_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint,
 						bp[y * 5] = -FLT_MAX;
 				}
 			}
+			// Move these values up and move suppressed to the end.
+			for (x = 0, y = 0; x < m; x++)
+				if (bp[x * 5] != -FLT_MAX)
+				{
+					int j;
+					if (x != y)
+					{
+						for (j = 0; j < 5; j++)
+							bp[y * 5 + j] = bp[x * 5 + j];
+						cp[y] = cp[x];
+					}
+					++y;
+				}
+			for (x = y; x < m; x++)
+				cp[x] = -1, bp[x * 5] = -FLT_MAX;
 		} parallel_endfor
 	} else {
 		// Otherwise, fall to use selection sort.
@@ -143,6 +158,20 @@ static int _ccv_nnc_nms_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint,
 						bp[y * bminc] = -FLT_MAX;
 				}
 			}
+			for (x = 0, y = 0; x < m; x++)
+				if (bp[x * bminc] != -FLT_MAX)
+				{
+					int j;
+					if (x != y)
+					{
+						for (j = 0; j < 5; j++)
+							bp[y * bminc + j] = bp[x * bminc + j];
+						cp[y] = cp[x];
+					}
+					++y;
+				}
+			for (x = y; x < m; x++)
+				cp[x] = -1, bp[x * bminc] = -FLT_MAX;
 		} parallel_endfor
 	}
 	return CCV_NNC_EXEC_SUCCESS;
@@ -150,6 +179,51 @@ static int _ccv_nnc_nms_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint,
 
 static int _ccv_nnc_nms_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
 {
+	assert(input_size >= 5);
+	const ccv_nnc_tensor_view_t* a = (ccv_nnc_tensor_view_t*)inputs[0];
+	const ccv_nnc_tensor_view_t* c = (ccv_nnc_tensor_view_t*)inputs[4];
+	assert(output_size == 1);
+	ccv_nnc_tensor_view_t* b = (ccv_nnc_tensor_view_t*)outputs[0];
+	const int a_nd = ccv_nnc_tensor_nd(a->info.dim);
+	const int b_nd = ccv_nnc_tensor_nd(b->info.dim);
+	const int c_nd = ccv_nnc_tensor_nd(c->info.dim);
+	assert(a_nd == b_nd);
+	int i;
+	for (i = 0; i < a_nd; i++)
+		{ assert(a->info.dim[i] == b->info.dim[i]); }
+	const int* ainc = CCV_IS_TENSOR_VIEW(a) ? a->inc : a->info.dim;
+	const int* binc = CCV_IS_TENSOR_VIEW(b) ? b->inc : b->info.dim;
+	const int* cinc = CCV_IS_TENSOR_VIEW(c) ? c->inc : c->info.dim;
+	const int n = a_nd >= 3 ? a->info.dim[0] : 1;
+	const int aninc = a_nd >= 3 ? ainc[1] * ainc[2] : 0;
+	const int bninc = b_nd >= 3 ? binc[1] * binc[2] : 0;
+	const int cninc = c_nd >= 2 ? cinc[1] : 0;
+	const int m = a_nd >= 3 ? a->info.dim[1] : a->info.dim[0];
+	if (c_nd == 1)
+		{ assert(m == c->info.dim[0]); }
+	else
+		{ assert(c_nd == 2 && n == c->info.dim[0] && m == c->info.dim[1]); }
+	const int aminc = ainc[a_nd - 1];
+	const int bminc = binc[b_nd - 1];
+	const int d = a_nd <= 1 ? 1 : a->info.dim[a_nd - 1];
+	parallel_for(i, n)
+	{
+		int x, y;
+		const float* const ap = a->data.f32 + n * aninc;
+		const int* const cp = c->data.i32 + n * cninc;
+		float* const bp = b->data.f32 + n * bninc;
+		for (x = 0; x < m; x++)
+			for (y = 0; y < d; y++)
+				bp[x * bminc + y] = 0;
+		for (x = 0; x < m; x++)
+		{
+			const int k = cp[x];
+			if (k < 0)
+				break;
+			for (y = 0; y < d; y++)
+				bp[k * bminc + y] = ap[x * aminc + y];
+		}
+	} parallel_endfor
 	return CCV_NNC_EXEC_SUCCESS;
 }
 
--- test/unit/nnc/nms.tests.c
+++ test/unit/nnc/nms.tests.c
@@ -86,4 +86,87 @@ TEST_CASE("compare non-maximal suppression forward with tensor views")
 	ccv_nnc_tensor_view_free(bv);
 }
 
+TEST_CASE("non-maximal suppression forward with some value suppressed")
+{
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 5), 0);
+	ccv_nnc_tensor_t* const b = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 5), 0);
+	ccv_nnc_tensor_t* const c = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32S, 10), 0);
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		a->data.f32[i * 5] = i;
+		a->data.f32[i * 5 + 1] = i;
+		a->data.f32[i * 5 + 2] = 0;
+		a->data.f32[i * 5 + 3] = 2;
+		a->data.f32[i * 5 + 4] = 1;
+	}
+	ccv_nnc_cmd_exec(CMD_NMS_FORWARD(0.3), ccv_nnc_no_hint, 0, TENSOR_LIST(a), TENSOR_LIST(b, c), 0);
+	ccv_nnc_tensor_t* const bt = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 5), 0);
+	for (i = 0; i < 5; i++)
+	{
+		bt->data.f32[i * 5] = 9 - i * 2;
+		bt->data.f32[i * 5 + 1] = 9 - i * 2;
+		bt->data.f32[i * 5 + 2] = 0;
+		bt->data.f32[i * 5 + 3] = 2;
+		bt->data.f32[i * 5 + 4] = 1;
+	}
+	REQUIRE_ARRAY_EQ(float, b->data.f32, bt->data.f32, 5 * 5, "should be equal");
+	int ct[10] = {};
+	for (i = 0; i < 5; i++)
+		ct[i] = 9 - i * 2;
+	for (i = 5; i < 10; i++)
+		ct[i] = -1;
+	REQUIRE_ARRAY_EQ(int, c->data.i32, ct, 10, "should be equal");
+	ccv_nnc_tensor_free(a);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(c);
+	ccv_nnc_tensor_free(bt);
+}
+
+TEST_CASE("non-maximal suppression backward with some value suppressed")
+{
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 5), 0);
+	ccv_nnc_tensor_t* const b = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 5), 0);
+	ccv_nnc_tensor_t* const c = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32S, 10), 0);
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		a->data.f32[i * 5] = i;
+		a->data.f32[i * 5 + 1] = i;
+		a->data.f32[i * 5 + 2] = 0;
+		a->data.f32[i * 5 + 3] = 2;
+		a->data.f32[i * 5 + 4] = 1;
+	}
+	ccv_nnc_cmd_exec(CMD_NMS_FORWARD(0.3), ccv_nnc_no_hint, 0, TENSOR_LIST(a), TENSOR_LIST(b, c), 0);
+	ccv_nnc_tensor_t* const db = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 5), 0);
+	for (i = 0; i < 10; i++)
+	{
+		db->data.f32[i * 5] = i;
+		db->data.f32[i * 5 + 1] = i;
+		db->data.f32[i * 5 + 2] = 0;
+		db->data.f32[i * 5 + 3] = 2;
+		db->data.f32[i * 5 + 4] = 1;
+	}
+	ccv_nnc_tensor_t* const da = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 5), 0);
+	ccv_nnc_cmd_exec(CMD_NMS_BACKWARD(0.3), ccv_nnc_no_hint, 0, TENSOR_LIST(db, 0, 0, 0, c), TENSOR_LIST(da), 0);
+	ccv_nnc_tensor_t* const dat = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 5), 0);
+	memset(dat->data.f32, 0, sizeof(float) * 10 * 5);
+	for (i = 0; i < 5; i++)
+	{
+		const int k = 9 - i * 2;
+		dat->data.f32[k * 5] = i;
+		dat->data.f32[k * 5 + 1] = i;
+		dat->data.f32[k * 5 + 2] = 0;
+		dat->data.f32[k * 5 + 3] = 2;
+		dat->data.f32[k * 5 + 4] = 1;
+	}
+	REQUIRE_TENSOR_EQ(da, dat, "should be equal");
+	ccv_nnc_tensor_free(a);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(c);
+	ccv_nnc_tensor_free(db);
+	ccv_nnc_tensor_free(da);
+	ccv_nnc_tensor_free(dat);
+}
+
 #include "case_main.h"
