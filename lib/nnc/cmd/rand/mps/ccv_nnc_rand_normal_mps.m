b333a2126641dbe945eefe2bb657dddb424e2943
Liu Liu <i@liuliu.me>
2022-10-08 19:27:17 -0400

    Fix random normal in MPS can generate nan.

--- lib/nnc/cmd/rand/mps/ccv_nnc_rand_normal_mps.m
+++ lib/nnc/cmd/rand/mps/ccv_nnc_rand_normal_mps.m
@@ -8,7 +8,7 @@
 static int _ccv_nnc_random_normal(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
 {
 	int i, j;
-	const uint32_t seed = ccv_nnc_stream_context_genrand_uint32(stream_context);
+	uint32_t seed = ccv_nnc_stream_context_genrand_uint32(stream_context);
 	const float std = cmd.info.blas.a[0];
 	const float mean = cmd.info.blas.a[1];
 	@autoreleasepool {
@@ -19,14 +19,30 @@ static int _ccv_nnc_random_normal(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t 
 			MPSGraph *graph = [MPSGraph new];
 			NSMutableArray<NSNumber*>* shape = [NSMutableArray new];
 			const int nd = ccv_nnc_tensor_nd(a->info.dim);
+			NSMutableArray<NSNumber*>* axes = [NSMutableArray new];
 			for (j = 0; j < nd; j++)
+			{
 				[shape addObject:@(a->info.dim[j])];
+				[axes addObject:@(j)];
+			}
+			MPSGraphTensor* mps_state = [graph randomPhiloxStateTensorWithSeed:(NSUInteger)seed name:nil];
 			MPSGraphRandomOpDescriptor* descriptor = [MPSGraphRandomOpDescriptor descriptorWithDistribution:MPSGraphRandomDistributionNormal dataType:ccv_nnc_mps_datatype(a->info.datatype)];
 			descriptor.mean = mean;
 			descriptor.standardDeviation = std;
-			MPSGraphTensor* mps_a = [graph randomTensorWithShape:shape descriptor:descriptor seed:(NSUInteger)seed name:nil];
+			// Using a while loop until found a tensor without nan. Somehow MPSGraph returns random tensor with some nans. This can be reproduced when you set the seed to 2069102477
+			// with philox state as: {counter_high = -5820494845676086657 : i64, counter_low = 149709389795223126 : i64, key = 5952859558996586250 : i64}
+			NSArray<MPSGraphTensor*>* mps_r = [graph whileWithInitialInputs:@[mps_state] before:^MPSGraphTensor*(NSArray<MPSGraphTensor*>* inputTensors, NSMutableArray<MPSGraphTensor*>* resultTensors) {
+				NSArray<MPSGraphTensor*>* mps_results = [graph randomTensorWithShape:shape descriptor:descriptor stateTensor:inputTensors[0] name:nil];
+				[resultTensors addObject:mps_results[0]];
+				[resultTensors addObject:mps_results[1]];
+				MPSGraphTensor* mps_nan = [graph isNaNWithTensor:mps_results[0] name:nil];
+				return [graph reductionOrWithTensor:mps_nan axes:axes name:nil];
+			} after:^NSArray<MPSGraphTensor*>*(NSArray<MPSGraphTensor*>* bodyBlockArguments) {
+				return @[bodyBlockArguments[1]];
+			} name:nil];
 			[shape release];
-			ccv_nnc_mps_graph_result(graph, command_buffer, @{}, mps_a, a, a->info.dim, a->stride);
+			[axes release];
+			ccv_nnc_mps_graph_result(graph, command_buffer, @{}, mps_r[0], a, a->info.dim, a->stride);
 			[graph release];
 		}
 		[command_buffer commit];
