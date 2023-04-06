1d960e48b746e52b6ba37008d0ab2653b405e33b
Liu Liu <i@liuliu.me>
2023-04-06 19:02:33 -0400

    Add missing files.

---
+++ lib/nnc/cmd/reduce/ccv_nnc_reduce_min_cpu_ref.c
@@ -0,0 +1,212 @@
+#include "ccv.h"
+#include "ccv_internal.h"
+#include "nnc/ccv_nnc.h"
+#include "nnc/ccv_nnc_easy.h"
+#include "nnc/ccv_nnc_internal.h"
+#ifdef USE_OPENMP
+#include <omp.h>
+#endif
+#ifdef USE_DISPATCH
+#include <dispatch/dispatch.h>
+#endif
+
+// Shared methods.
+#include "../_ccv_nnc_cpu_ref.h"
+
+static int _ccv_nnc_reduce_min_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	assert(input_size == 1);
+	ccv_nnc_tensor_view_t* const a = (ccv_nnc_tensor_view_t*)inputs[0];
+	ccv_nnc_tensor_view_t* const b = (ccv_nnc_tensor_view_t*)outputs[0];
+	assert(ccv_nnc_tensor_nd(a->info.dim) <= CCV_NNC_MAX_DIM + 2);
+	assert(ccv_nnc_tensor_nd(b->info.dim) <= CCV_NNC_MAX_DIM + 2);
+	// Assuming this is float 32.
+	int adim[CCV_NNC_MAX_DIM_ALLOC];
+	int bdim[CCV_NNC_MAX_DIM_ALLOC];
+	ccv_nnc_tensor_view_get_dim(a, adim);
+	ccv_nnc_tensor_view_get_dim(b, bdim);
+	assert(ccv_nnc_tensor_view_check_broadcast_dim(b, adim));
+	int astride[CCV_NNC_MAX_DIM_ALLOC];
+	int bstride[CCV_NNC_MAX_DIM_ALLOC];
+	assert(CCV_NNC_MAX_DIM == 2); // Need to change this logic for CCV_NNC_MAX_DIM == other number.
+	ccv_nnc_tensor_view_get_stride(a, astride);
+	ccv_nnc_tensor_view_get_stride(b, bstride);
+	int i[CCV_NNC_MAX_DIM + 2];
+	int x;
+	_ccv_nnc_tensor_set_cpu_ref_f32(b, FLT_MAX);
+	float* const ap = a->data.f32;
+	float* const bp = b->data.f32;
+	// Non-optimal case, need to do skip if needed.
+	for (i[0] = 0; i[0] < adim[0]; i[0]++)
+	{
+		float* const ap0 = ap + i[0] * astride[0];
+		float* const bp0 = bdim[0] == 1 ? bp : bp + i[0] * bstride[0];
+		for (i[1] = 0; i[1] < adim[1]; i[1]++)
+		{
+			float* ap1 = ap0 + i[1] * astride[1];
+			float* const bp1 = bdim[1] == 1 ? bp0 : bp0 + i[1] * bstride[1];
+			for (i[2] = 0; i[2] < adim[2]; i[2]++)
+			{
+				float* const bp2 = bdim[2] == 1 ? bp1 : bp1 + i[2] * bstride[2];
+				if (bdim[3] == 1)
+				{
+					for (x = 0; x < adim[3]; x++)
+						if (ap1[x] < bp2[0])
+							bp2[0] = ap1[x];
+				} else {
+					for (x = 0; x < adim[3]; x++)
+						if (ap1[x] < bp2[x])
+							bp2[x] = ap1[x];
+				}
+				ap1 += astride[2];
+			}
+		}
+	}
+	return CCV_NNC_EXEC_SUCCESS;
+}
+
+static int _ccv_nnc_reduce_min_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	if (inputs[0] == 0)
+	{
+		ccv_nnc_tensor_view_t* const h = (ccv_nnc_tensor_view_t*)outputs[0];
+		ccv_nnc_tensor_view_t* const a = (ccv_nnc_tensor_view_t*)inputs[1];
+		ccv_nnc_tensor_view_t* const b = (ccv_nnc_tensor_view_t*)inputs[2];
+		assert(ccv_nnc_tensor_nd(h->info.dim) <= CCV_NNC_MAX_DIM + 2);
+		assert(ccv_nnc_tensor_nd(a->info.dim) <= CCV_NNC_MAX_DIM + 2);
+		assert(ccv_nnc_tensor_nd(b->info.dim) <= CCV_NNC_MAX_DIM + 2);
+		// Assuming this is float 32.
+		int hdim[CCV_NNC_MAX_DIM_ALLOC];
+		int bdim[CCV_NNC_MAX_DIM_ALLOC];
+		ccv_nnc_tensor_view_get_dim(h, hdim);
+		ccv_nnc_tensor_view_get_dim(b, bdim);
+		assert(ccv_nnc_tensor_view_check_broadcast_dim(b, hdim));
+		assert(ccv_nnc_tensor_view_check_dim(a, hdim));
+		int hstride[CCV_NNC_MAX_DIM_ALLOC];
+		int astride[CCV_NNC_MAX_DIM_ALLOC];
+		int bstride[CCV_NNC_MAX_DIM_ALLOC];
+		assert(CCV_NNC_MAX_DIM == 2); // Need to change this logic for CCV_NNC_MAX_DIM == other number.
+		ccv_nnc_tensor_view_get_stride(h, hstride);
+		ccv_nnc_tensor_view_get_stride(a, astride);
+		ccv_nnc_tensor_view_get_stride(b, bstride);
+		int i[CCV_NNC_MAX_DIM + 2];
+		int x;
+		float* const hp = h->data.f32;
+		float* const ap = a->data.f32;
+		float* const bp = b->data.f32;
+		ccv_nnc_tensor_zero(h);
+		// Non-optimal case, need to do skip if needed.
+		for (i[0] = 0; i[0] < hdim[0]; i[0]++)
+		{
+			float* const ap0 = ap + i[0] * astride[0];
+			float* const hp0 = hp + i[0] * hstride[0];
+			float* const bp0 = bdim[0] == 1 ? bp : bp + i[0] * bstride[0];
+			for (i[1] = 0; i[1] < hdim[1]; i[1]++)
+			{
+				float* ap1 = ap0 + i[1] * astride[1];
+				float* hp1 = hp0 + i[1] * hstride[1];
+				float* const bp1 = bdim[1] == 1 ? bp0 : bp0 + i[1] * bstride[1];
+				for (i[2] = 0; i[2] < hdim[2]; i[2]++)
+				{
+					float* const bp2 = bdim[2] == 1 ? bp1 : bp1 + i[2] * bstride[2];
+					if (bdim[3] == 1)
+					{
+						for (x = 0; x < hdim[3]; x++)
+							if (ap1[x] == bp2[0])
+								hp1[x] = 1;
+					} else {
+						for (x = 0; x < hdim[3]; x++)
+							if (ap1[x] == bp2[x])
+								hp1[x] = 1;
+					}
+					hp1 += hstride[2];
+					ap1 += astride[2];
+				}
+			}
+		}
+		return CCV_NNC_EXEC_SUCCESS;
+	}
+	ccv_nnc_tensor_view_t* const h = (ccv_nnc_tensor_view_t*)outputs[0];
+	ccv_nnc_tensor_view_t* const g = (ccv_nnc_tensor_view_t*)inputs[0];
+	ccv_nnc_tensor_view_t* const a = (ccv_nnc_tensor_view_t*)inputs[1];
+	ccv_nnc_tensor_view_t* const b = (ccv_nnc_tensor_view_t*)inputs[2];
+	assert(ccv_nnc_tensor_nd(h->info.dim) <= CCV_NNC_MAX_DIM + 2);
+	assert(ccv_nnc_tensor_nd(g->info.dim) <= CCV_NNC_MAX_DIM + 2);
+	assert(ccv_nnc_tensor_nd(a->info.dim) <= CCV_NNC_MAX_DIM + 2);
+	assert(ccv_nnc_tensor_nd(b->info.dim) <= CCV_NNC_MAX_DIM + 2);
+	// Assuming this is float 32.
+	int hdim[CCV_NNC_MAX_DIM_ALLOC];
+	int gdim[CCV_NNC_MAX_DIM_ALLOC];
+	ccv_nnc_tensor_view_get_dim(h, hdim);
+	ccv_nnc_tensor_view_get_dim(g, gdim);
+	assert(ccv_nnc_tensor_view_check_broadcast_dim(g, hdim));
+	assert(ccv_nnc_tensor_view_check_dim(a, hdim));
+	assert(ccv_nnc_tensor_view_check_dim(b, gdim));
+	int hstride[CCV_NNC_MAX_DIM_ALLOC];
+	int gstride[CCV_NNC_MAX_DIM_ALLOC];
+	int astride[CCV_NNC_MAX_DIM_ALLOC];
+	int bstride[CCV_NNC_MAX_DIM_ALLOC];
+	assert(CCV_NNC_MAX_DIM == 2); // Need to change this logic for CCV_NNC_MAX_DIM == other number.
+	ccv_nnc_tensor_view_get_stride(h, hstride);
+	ccv_nnc_tensor_view_get_stride(g, gstride);
+	ccv_nnc_tensor_view_get_stride(a, astride);
+	ccv_nnc_tensor_view_get_stride(b, bstride);
+	int i[CCV_NNC_MAX_DIM + 2];
+	int x;
+	float* const hp = h->data.f32;
+	float* const gp = g->data.f32;
+	float* const ap = a->data.f32;
+	float* const bp = b->data.f32;
+	ccv_nnc_tensor_zero(h);
+	// Non-optimal case, need to do skip if needed.
+	for (i[0] = 0; i[0] < hdim[0]; i[0]++)
+	{
+		float* const ap0 = ap + i[0] * astride[0];
+		float* const hp0 = hp + i[0] * hstride[0];
+		float* const gp0 = gdim[0] == 1 ? gp : gp + i[0] * gstride[0];
+		float* const bp0 = gdim[0] == 1 ? bp : bp + i[0] * bstride[0];
+		for (i[1] = 0; i[1] < hdim[1]; i[1]++)
+		{
+			float* ap1 = ap0 + i[1] * astride[1];
+			float* hp1 = hp0 + i[1] * hstride[1];
+			float* const gp1 = gdim[1] == 1 ? gp0 : gp0 + i[1] * gstride[1];
+			float* const bp1 = gdim[1] == 1 ? bp0 : bp0 + i[1] * bstride[1];
+			for (i[2] = 0; i[2] < hdim[2]; i[2]++)
+			{
+				float* const gp2 = gdim[2] == 1 ? gp1 : gp1 + i[2] * gstride[2];
+				float* const bp2 = gdim[2] == 1 ? bp1 : bp1 + i[2] * bstride[2];
+				if (gdim[3] == 1)
+				{
+					for (x = 0; x < hdim[3]; x++)
+						if (ap1[x] == bp2[0])
+							hp1[x] = gp2[0];
+				} else {
+					for (x = 0; x < hdim[3]; x++)
+						if (ap1[x] == bp2[x])
+							hp1[x] = gp2[x];
+				}
+				hp1 += hstride[2];
+				ap1 += astride[2];
+			}
+		}
+	}
+	return CCV_NNC_EXEC_SUCCESS;
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_REDUCE_MIN_FORWARD, CCV_NNC_BACKEND_CPU_REF)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NHWC | CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_CHWN;
+	registry->tensor_datatypes = CCV_32F;
+	registry->tensor_memory = CCV_TENSOR_CPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_reduce_min_forw;
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_REDUCE_MIN_BACKWARD, CCV_NNC_BACKEND_CPU_REF)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NHWC | CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_CHWN;
+	registry->tensor_datatypes = CCV_32F;
+	registry->tensor_memory = CCV_TENSOR_CPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_reduce_min_back;
+}
---
+++ lib/nnc/cmd/reduce/gpu/ccv_nnc_argmin_gpu_ref.cu
@@ -0,0 +1,97 @@
+extern "C" {
+#include <ccv.h>
+#include <ccv_internal.h>
+#include <nnc/ccv_nnc.h>
+#include <nnc/ccv_nnc_easy.h>
+#include <nnc/ccv_nnc_internal.h>
+}
+#include <nnc/gpu/ccv_nnc_compat.h>
+
+template<typename NUM1, typename NUM2>
+__global__ void _ccv_nnc_argmin_forw_kernel(const int dim_without_axis, const int axis_dim, const int dim_after_axis, const NUM1* const a, NUM2* const b)
+{
+	CUDA_1D_KERNEL_LOOP(i, dim_without_axis) {
+		const int x = i / dim_after_axis;
+		const int y = i % dim_after_axis;
+		const NUM1* const ap = a + x * dim_after_axis * axis_dim + y;
+		NUM1 min = ap[0];
+		int idx = 0;
+		for (int j = 1; j < axis_dim; j++)
+			if (ap[j * dim_after_axis] < min)
+				min = ap[j * dim_after_axis], idx = j;
+		b[x * dim_after_axis + y] = (NUM2)idx;
+	}
+}
+
+static int _ccv_nnc_argmin_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	assert(input_size == 1);
+	ccv_nnc_tensor_t* const a = inputs[0];
+	assert(CCV_IS_TENSOR_CONTIGUOUS(a));
+	assert(output_size == 1);
+	ccv_nnc_tensor_t* const b = outputs[0];
+	assert(CCV_IS_TENSOR_CONTIGUOUS(b));
+	const int axis = cmd.info.reduce.axis[0];
+	assert(cmd.info.reduce.count == 1);
+	assert(axis >= 0);
+	const int a_nd = ccv_nnc_tensor_nd(a->info.dim);
+	assert(axis < a_nd);
+	const int b_nd = ccv_nnc_tensor_nd(b->info.dim);
+	int i;
+	for (i = axis; i < a_nd; i++)
+		{ assert(a->info.dim[i] == (b_nd - (a_nd - i) >= 0) ? b->info.dim[b_nd - (a_nd - i)] : 1); }
+	assert(1 == (b_nd - (a_nd - axis) >= 0) ? b->info.dim[b_nd - (a_nd - axis)] : 1);
+	for (i = 0; i < axis; i++)
+		{ assert(a->info.dim[i] == (b_nd - (a_nd - i) >= 0) ? b->info.dim[b_nd - (a_nd - i)] : 1); }
+	const int tensor_count = ccv_nnc_tensor_count(a->info);
+	const int axis_dim = a->info.dim[axis];
+	int dim_after_axis = 1;
+	for (i = axis + 1; i < a_nd; i++)
+		dim_after_axis *= a->info.dim[i];
+	assert(ccv_nnc_tensor_count(b->info) == tensor_count / axis_dim);
+	assert(a->info.datatype == CCV_32F || a->info.datatype == CCV_16F);
+	const int dim_without_axis = tensor_count / axis_dim;
+	cudaStream_t stream = ccv_nnc_stream_context_get_stream(stream_context);
+	if (b->info.datatype == CCV_32F)
+	{
+		if (a->info.datatype == CCV_32F)
+			_ccv_nnc_argmin_forw_kernel<<<CUDA_GET_BLOCKS(dim_without_axis), CUDA_NUM_THREADS, 0, stream>>>(dim_without_axis, axis_dim, dim_after_axis, a->data.f32, b->data.f32);
+		else
+			_ccv_nnc_argmin_forw_kernel<<<CUDA_GET_BLOCKS(dim_without_axis), CUDA_NUM_THREADS, 0, stream>>>(dim_without_axis, axis_dim, dim_after_axis, (__half*)a->data.f16, b->data.f32);
+	} else if (b->info.datatype == CCV_32S) {
+		if (a->info.datatype == CCV_32F)
+			_ccv_nnc_argmin_forw_kernel<<<CUDA_GET_BLOCKS(dim_without_axis), CUDA_NUM_THREADS, 0, stream>>>(dim_without_axis, axis_dim, dim_after_axis, a->data.f32, b->data.i32);
+		else
+			_ccv_nnc_argmin_forw_kernel<<<CUDA_GET_BLOCKS(dim_without_axis), CUDA_NUM_THREADS, 0, stream>>>(dim_without_axis, axis_dim, dim_after_axis, (__half*)a->data.f16, b->data.i32);
+	} else {
+		assert(b->info.datatype == CCV_16F);
+		if (a->info.datatype == CCV_32F)
+			_ccv_nnc_argmin_forw_kernel<<<CUDA_GET_BLOCKS(dim_without_axis), CUDA_NUM_THREADS, 0, stream>>>(dim_without_axis, axis_dim, dim_after_axis, a->data.f32, (__half*)b->data.f16);
+		else
+			_ccv_nnc_argmin_forw_kernel<<<CUDA_GET_BLOCKS(dim_without_axis), CUDA_NUM_THREADS, 0, stream>>>(dim_without_axis, axis_dim, dim_after_axis, (__half*)a->data.f16, (__half*)b->data.f16);
+	}
+	return CCV_NNC_EXEC_SUCCESS;
+}
+
+static int _ccv_nnc_argmin_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	return CCV_NNC_EXEC_INVALID;
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_ARGMIN_FORWARD, CCV_NNC_BACKEND_GPU_REF)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_NHWC | CCV_TENSOR_FORMAT_CHWN;
+	registry->tensor_datatypes = CCV_32F | CCV_16F | CCV_32S;
+	registry->tensor_memory = CCV_TENSOR_GPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_argmin_forw;
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_ARGMIN_BACKWARD, CCV_NNC_BACKEND_GPU_REF)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_NHWC | CCV_TENSOR_FORMAT_CHWN;
+	registry->tensor_datatypes = CCV_32F | CCV_16F | CCV_32S;
+	registry->tensor_memory = CCV_TENSOR_GPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_argmin_back;
+}
