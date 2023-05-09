4a20635253e1d73cea25a2019ca8fa99850dd3b6
Liu Liu <i@liuliu.me>
2023-05-09 15:53:41 -0400

    Fix MPS GroupNorm where when you have strided input that cannot be divided when breakdown their dimensions, you get assertion.

--- lib/nnc/cmd/norm/mps/ccv_nnc_group_norm_mps.m
+++ lib/nnc/cmd/norm/mps/ccv_nnc_group_norm_mps.m
@@ -33,19 +33,20 @@ static int _ccv_nnc_group_norm_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_
 	assert(input_size == 3);
 	assert(output_size == 3);
 	ccv_nnc_tensor_view_t at = ccv_nnc_get_tensor_view(inputs[0]);
-	_ccv_break_axis_to_groups(&at, cmd.info.gnorm.group_axis, cmd.info.gnorm.groups, at.info.dim[cmd.info.gnorm.group_axis] / cmd.info.gnorm.groups);
+	const int group_axis = cmd.info.gnorm.group_axis;
+	_ccv_break_axis_to_groups(&at, group_axis, cmd.info.gnorm.groups, at.info.dim[group_axis] / cmd.info.gnorm.groups);
 	assert(CCV_IS_TENSOR_CONTIGUOUS(inputs[1]));
 	ccv_nnc_tensor_view_t scalet = ccv_nnc_get_tensor_view(inputs[1]);
-	_ccv_break_axis_to_groups(&scalet, cmd.info.gnorm.group_axis, cmd.info.gnorm.groups, scalet.info.dim[cmd.info.gnorm.group_axis] / cmd.info.gnorm.groups);
+	_ccv_break_axis_to_groups(&scalet, group_axis, cmd.info.gnorm.groups, scalet.info.dim[group_axis] / cmd.info.gnorm.groups);
 	assert(CCV_IS_TENSOR_CONTIGUOUS(inputs[2]));
 	ccv_nnc_tensor_view_t biast = ccv_nnc_get_tensor_view(inputs[2]);
-	_ccv_break_axis_to_groups(&biast, cmd.info.gnorm.group_axis, cmd.info.gnorm.groups, biast.info.dim[cmd.info.gnorm.group_axis] / cmd.info.gnorm.groups);
+	_ccv_break_axis_to_groups(&biast, group_axis, cmd.info.gnorm.groups, biast.info.dim[group_axis] / cmd.info.gnorm.groups);
 	ccv_nnc_tensor_view_t bt = ccv_nnc_get_tensor_view(outputs[0]);
-	_ccv_break_axis_to_groups(&bt, cmd.info.gnorm.group_axis, cmd.info.gnorm.groups, bt.info.dim[cmd.info.gnorm.group_axis] / cmd.info.gnorm.groups);
+	_ccv_break_axis_to_groups(&bt, group_axis, cmd.info.gnorm.groups, bt.info.dim[group_axis] / cmd.info.gnorm.groups);
 	ccv_nnc_tensor_view_t saved_meant = ccv_nnc_get_tensor_view(outputs[1]);
-	_ccv_break_axis_to_groups(&saved_meant, cmd.info.gnorm.group_axis, cmd.info.gnorm.groups, 1);
+	_ccv_break_axis_to_groups(&saved_meant, group_axis, cmd.info.gnorm.groups, 1);
 	ccv_nnc_tensor_view_t saved_inv_stdt = ccv_nnc_get_tensor_view(outputs[2]);
-	_ccv_break_axis_to_groups(&saved_inv_stdt, cmd.info.gnorm.group_axis, cmd.info.gnorm.groups, 1);
+	_ccv_break_axis_to_groups(&saved_inv_stdt, group_axis, cmd.info.gnorm.groups, 1);
 	ccv_nnc_tensor_view_alignment((ccv_nnc_tensor_view_t*[]){
 		&at,
 		&saved_meant,
@@ -58,9 +59,24 @@ static int _ccv_nnc_group_norm_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_
 		int indices[3];
 		MPSGraphExecutable* executable = ccv_nnc_mps_graph_executable_cache(key, indices, ^void (MPSGraph* graph, NSMutableArray<MPSGraphTensor*>* inputTensors, NSMutableArray<MPSGraphShapedType*>* inputShapedTypes, NSMutableArray<MPSGraphTensor*>* resultTensors) {
 			MPSGraphTensor* mps_input_a;
-			MPSGraphTensor* mps_a = ccv_nnc_mps_graph_tensor_input(graph, &at, at.info.dim, at.stride, &mps_input_a);
+			MPSGraphTensor* mps_a;
+			MPSGraphShapedType* mps_a_shape;
+			int i;
+			// If we cannot break input as expected, it only means: 1. the input is a tensor view. 2. We need to get mps_a first and then reshape into the break dimensions.
+			if (group_axis > 0 && CCV_IS_TENSOR_VIEW(inputs[0]) && (at.stride[group_axis - 1] % at.stride[group_axis]) != 0)
+			{
+				mps_a = ccv_nnc_mps_graph_tensor_input(graph, (ccv_nnc_tensor_view_t*)inputs[0], inputs[0]->info.dim, ((ccv_nnc_tensor_view_t*)inputs[0])->stride, &mps_input_a);
+				NSMutableArray<NSNumber*>* shape = [NSMutableArray new];
+				int nd = ccv_nnc_tensor_nd(at.info.dim);
+				for (i = 0; i < nd; i++)
+					[shape addObject:@(at.info.dim[i])];
+				mps_a = [graph reshapeTensor:mps_a withShape:shape name:nil];
+				mps_a_shape = ccv_nnc_mps_graph_tensor_input_shape((ccv_nnc_tensor_view_t*)inputs[0], inputs[0]->info.dim, ((ccv_nnc_tensor_view_t*)inputs[0])->stride);
+			} else {
+				mps_a = ccv_nnc_mps_graph_tensor_input(graph, &at, at.info.dim, at.stride, &mps_input_a);
+				mps_a_shape = ccv_nnc_mps_graph_tensor_input_shape(&at, at.info.dim, at.stride);
+			}
 			[inputTensors addObject:mps_input_a];
-			MPSGraphShapedType* mps_a_shape = ccv_nnc_mps_graph_tensor_input_shape(&at, at.info.dim, at.stride);
 			[inputShapedTypes addObject:mps_a_shape];
 			MPSGraphTensor* mps_input_scale;
 			MPSGraphTensor* mps_scale = ccv_nnc_mps_graph_tensor_input(graph, &scalet, scalet.info.dim, scalet.stride, &mps_input_scale);
@@ -72,7 +88,6 @@ static int _ccv_nnc_group_norm_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_
 			[inputTensors addObject:mps_input_bias];
 			MPSGraphShapedType* mps_bias_shape = ccv_nnc_mps_graph_tensor_input_shape(&biast, biast.info.dim, biast.stride);
 			[inputShapedTypes addObject:mps_bias_shape];
-			int i;
 			NSMutableArray<NSNumber*>* axes = [NSMutableArray new];
 			const int rnd = ccv_nnc_tensor_nd(saved_meant.info.dim);
 			for (i = 0; i < rnd; i++)
@@ -100,16 +115,31 @@ static int _ccv_nnc_group_norm_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_
 				mps_saved_inv_std = [graph castTensor:mps_inv_std_f32 toType:MPSDataTypeFloat16 name:@"inv_std"];
 			}
 			MPSGraphTensor* mps_b = [graph additionWithPrimaryTensor:[graph multiplicationWithPrimaryTensor:[graph multiplicationWithPrimaryTensor:mps_a_subtract_mean secondaryTensor:mps_saved_inv_std name:nil] secondaryTensor:mps_scale name:nil] secondaryTensor:mps_bias name:nil];
+			if (group_axis > 0 && CCV_IS_TENSOR_VIEW(outputs[0]) && (bt.stride[group_axis - 1] % bt.stride[group_axis]) != 0)
+			{
+				NSMutableArray<NSNumber*>* shape = [NSMutableArray new];
+				const int nd = ccv_nnc_tensor_nd(outputs[0]->info.dim);
+				for (i = 0; i < nd; i++)
+					[shape addObject:@(outputs[0]->info.dim[i])];
+				mps_b = [graph reshapeTensor:mps_b withShape:shape name:nil];
+			}
 			[resultTensors addObject:mps_b];
 			[resultTensors addObject:mps_saved_mean];
 			[resultTensors addObject:mps_saved_inv_std];
 		});
 		// I don't think that I want to implement saved_mean / saved_inv_std properly just yet.
-		MPSGraphTensorData* data_a = ccv_nnc_mps_graph_tensor_data(&at, at.info.dim, at.stride);
+		MPSGraphTensorData* data_a;
+		if (group_axis > 0 && CCV_IS_TENSOR_VIEW(inputs[0]) && (at.stride[group_axis - 1] % at.stride[group_axis]) != 0)
+			data_a = ccv_nnc_mps_graph_tensor_data((ccv_nnc_tensor_view_t*)inputs[0], inputs[0]->info.dim, ((ccv_nnc_tensor_view_t*)inputs[0])->stride);
+		else
+			data_a = ccv_nnc_mps_graph_tensor_data(&at, at.info.dim, at.stride);
 		MPSGraphTensorData* data_scale = ccv_nnc_mps_graph_tensor_data(&scalet, scalet.info.dim, scalet.stride);
 		MPSGraphTensorData* data_bias = ccv_nnc_mps_graph_tensor_data(&biast, biast.info.dim, biast.stride);
 		MPSGraphTensorData* data[] = {data_a, data_scale, data_bias};
-		ccv_nnc_mps_graph_executable_result(executable, command_buffer, @[data[indices[0]], data[indices[1]], data[indices[2]]], (ccv_nnc_tensor_view_t* []){ &bt, &saved_meant, &saved_inv_stdt }, (int*[]){ bt.info.dim, saved_meant.info.dim, saved_inv_stdt.info.dim }, (int*[]){ bt.stride, saved_meant.stride, saved_inv_stdt.stride }, 3);
+		if (group_axis > 0 && CCV_IS_TENSOR_VIEW(outputs[0]) && (bt.stride[group_axis - 1] % bt.stride[group_axis]) != 0)
+			ccv_nnc_mps_graph_executable_result(executable, command_buffer, @[data[indices[0]], data[indices[1]], data[indices[2]]], (ccv_nnc_tensor_view_t* []){ (ccv_nnc_tensor_view_t*)outputs[0], &saved_meant, &saved_inv_stdt }, (int*[]){ outputs[0]->info.dim, saved_meant.info.dim, saved_inv_stdt.info.dim }, (int*[]){ ((ccv_nnc_tensor_view_t*)outputs[0])->stride, saved_meant.stride, saved_inv_stdt.stride }, 3);
+		else
+			ccv_nnc_mps_graph_executable_result(executable, command_buffer, @[data[indices[0]], data[indices[1]], data[indices[2]]], (ccv_nnc_tensor_view_t* []){ &bt, &saved_meant, &saved_inv_stdt }, (int*[]){ bt.info.dim, saved_meant.info.dim, saved_inv_stdt.info.dim }, (int*[]){ bt.stride, saved_meant.stride, saved_inv_stdt.stride }, 3);
 		ccv_nnc_stream_context_commit_command_buffer(stream_context, command_buffer);
 	}
 	return CCV_NNC_EXEC_SUCCESS;
