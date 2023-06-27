a1a74f6814e216fc930b1419e596c533f7ef5935
Liu Liu <i@liuliu.me>
2023-06-27 13:34:43 -0400

    Add adamw optimizer.

---
+++ lib/nnc/cmd/adam/ccv_nnc_adamw_cpu_ref.c
@@ -0,0 +1,133 @@
+#include "ccv.h"
+#include "ccv_internal.h"
+#include "nnc/ccv_nnc.h"
+#include "nnc/ccv_nnc_easy.h"
+#include "nnc/ccv_nnc_internal.h"
+#ifdef USE_OPENMP
+#include <omp.h>
+#endif
+#ifdef USE_DISPATCH
+#include <dispatch/dispatch.h>
+#endif
+
+// Shared methods.
+#include "../_ccv_nnc_cpu_ref.h"
+
+static int _ccv_nnc_adamw_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	assert(input_size == 4);
+	assert(output_size == 3);
+	ccv_nnc_tensor_view_t* const g = (ccv_nnc_tensor_view_t*)inputs[0];
+	ccv_nnc_tensor_view_t* const a = (ccv_nnc_tensor_view_t*)inputs[1];
+	ccv_nnc_tensor_view_t* const m = (ccv_nnc_tensor_view_t*)inputs[2];
+	ccv_nnc_tensor_view_t* const v = (ccv_nnc_tensor_view_t*)inputs[3];
+	ccv_nnc_tensor_view_t* const b = (ccv_nnc_tensor_view_t*)outputs[0];
+	ccv_nnc_tensor_view_t* const n = (ccv_nnc_tensor_view_t*)outputs[1];
+	ccv_nnc_tensor_view_t* const u = (ccv_nnc_tensor_view_t*)outputs[2];
+	assert(ccv_nnc_tensor_nd(a->info.dim) <= CCV_NNC_MAX_DIM + 2);
+	assert(ccv_nnc_tensor_nd(b->info.dim) <= CCV_NNC_MAX_DIM + 2);
+	// Assuming this is float 32.
+	int adim[CCV_NNC_MAX_DIM_ALLOC];
+	ccv_nnc_tensor_view_get_dim(a, adim);
+	assert(ccv_nnc_tensor_view_check_dim(g, adim));
+	assert(ccv_nnc_tensor_view_check_dim(m, adim));
+	assert(ccv_nnc_tensor_view_check_dim(v, adim));
+	assert(ccv_nnc_tensor_view_check_dim(b, adim));
+	assert(ccv_nnc_tensor_view_check_dim(n, adim));
+	assert(ccv_nnc_tensor_view_check_dim(u, adim));
+	assert(CCV_NNC_MAX_DIM == 2); // Need to change this logic for CCV_NNC_MAX_DIM == other number.
+	int gstride[CCV_NNC_MAX_DIM_ALLOC];
+	int astride[CCV_NNC_MAX_DIM_ALLOC];
+	int mstride[CCV_NNC_MAX_DIM_ALLOC];
+	int vstride[CCV_NNC_MAX_DIM_ALLOC];
+	int bstride[CCV_NNC_MAX_DIM_ALLOC];
+	int nstride[CCV_NNC_MAX_DIM_ALLOC];
+	int ustride[CCV_NNC_MAX_DIM_ALLOC];
+	ccv_nnc_tensor_view_get_stride(g, gstride);
+	ccv_nnc_tensor_view_get_stride(a, astride);
+	ccv_nnc_tensor_view_get_stride(m, mstride);
+	ccv_nnc_tensor_view_get_stride(v, vstride);
+	ccv_nnc_tensor_view_get_stride(b, bstride);
+	ccv_nnc_tensor_view_get_stride(n, nstride);
+	ccv_nnc_tensor_view_get_stride(u, ustride);
+	const int step = cmd.info.adam.step;
+	const float rate = cmd.info.adam.rate;
+	const float beta1 = cmd.info.adam.beta1;
+	const float beta2 = cmd.info.adam.beta2;
+	const float decay = cmd.info.adam.decay;
+	const float epsilon = cmd.info.adam.epsilon;
+	assert(step >= 1);
+	const float rate_inv_bias_correction1 = rate / (1 - powf(beta1, step));
+	const float inv_bias_correction2 = 1. / (1 - powf(beta2, step));
+	const float rate_decay = rate * decay;
+	int i[CCV_NNC_MAX_DIM + 1];
+	int x;
+	float* const gp = g->data.f32;
+	float* const ap = a->data.f32;
+	float* const mp = m->data.f32;
+	float* const vp = v->data.f32;
+	float* const bp = b->data.f32;
+	float* const np = n->data.f32;
+	float* const up = u->data.f32;
+	for (i[0] = 0; i[0] < adim[0]; i[0]++)
+	{
+		float* const gp0 = gp + i[0] * gstride[0];
+		float* const ap0 = ap + i[0] * astride[0];
+		float* const mp0 = mp + i[0] * mstride[0];
+		float* const vp0 = vp + i[0] * vstride[0];
+		float* const bp0 = bp + i[0] * bstride[0];
+		float* const np0 = np + i[0] * nstride[0];
+		float* const up0 = up + i[0] * ustride[0];
+		for (i[1] = 0; i[1] < adim[1]; i[1]++)
+		{
+			float* gp1 = gp0 + i[1] * gstride[1];
+			float* ap1 = ap0 + i[1] * astride[1];
+			float* mp1 = mp0 + i[1] * mstride[1];
+			float* vp1 = vp0 + i[1] * vstride[1];
+			float* bp1 = bp0 + i[1] * bstride[1];
+			float* np1 = np0 + i[1] * nstride[1];
+			float* up1 = up0 + i[1] * ustride[1];
+			for (i[2] = 0; i[2] < adim[2]; i[2]++)
+			{
+				for (x = 0; x < adim[3]; x++)
+				{
+					const float grad = gp1[x];
+					const float mom = np1[x] = beta1 * mp1[x] + (1 - beta1) * grad;
+					const float vel = up1[x] = beta2 * vp1[x] + (1 - beta2) * grad * grad;
+					bp1[x] = ap1[x] - rate_decay * ap1[x] - (mom * rate_inv_bias_correction1) / (sqrtf(vel * inv_bias_correction2) + epsilon);
+				}
+				gp1 += gstride[2];
+				ap1 += astride[2];
+				mp1 += mstride[2];
+				vp1 += vstride[2];
+				bp1 += bstride[2];
+				np1 += nstride[2];
+				up1 += ustride[2];
+			}
+		}
+	}
+	return CCV_NNC_EXEC_SUCCESS;
+}
+
+static int _ccv_nnc_adamw_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	return CCV_NNC_EXEC_INVALID;
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_ADAMW_FORWARD, CCV_NNC_BACKEND_CPU_REF)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NHWC | CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_CHWN;
+	registry->tensor_datatypes = CCV_32F;
+	registry->tensor_memory = CCV_TENSOR_CPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_adamw_forw;
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_ADAMW_BACKWARD, CCV_NNC_BACKEND_CPU_REF)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NHWC | CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_CHWN;
+	registry->tensor_datatypes = CCV_32F;
+	registry->tensor_memory = CCV_TENSOR_CPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_adamw_back;
+}
---
+++ lib/nnc/cmd/adam/gpu/ccv_nnc_adamw_gpu_ref.cu
@@ -0,0 +1,108 @@
+extern "C" {
+#include <ccv.h>
+#include <ccv_internal.h>
+#include <nnc/ccv_nnc.h>
+#include <nnc/ccv_nnc_easy.h>
+#include <nnc/ccv_nnc_internal.h>
+}
+#include <nnc/gpu/ccv_nnc_compat.h>
+
+#ifdef HAVE_CUDA
+
+template<typename NUM1, typename NUM2>
+__global__ void _ccv_nnc_adamw_kernel(const size_t tensor_count, const float beta1, const float beta2, const float rate_decay, const float rate_inv_bias_correction1, const float inv_bias_correction2, const float epsilon, const NUM1* const g, const NUM2* const a, const NUM2* const mom, const NUM2* const vel, NUM2* const b, NUM2* const new_mom, NUM2* const new_vel)
+{
+	CUDA_1D_KERNEL_LOOP(i, tensor_count) {
+		const float grad = (float)g[i];
+		const float m = beta1 * (float)mom[i] + (1 - beta1) * grad;
+		const float v = beta2 * (float)vel[i] + (1 - beta2) * grad * grad;
+		b[i] = (NUM2)((float)a[i] - rate_decay * (float)a[i] - (m * rate_inv_bias_correction1) / (sqrtf(v * inv_bias_correction2) + epsilon));
+		new_mom[i] = (NUM2)m;
+		new_vel[i] = (NUM2)v;
+	}
+}
+
+static int _ccv_nnc_adamw_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	assert(input_size == 4);
+	assert(output_size == 3);
+	cudaStream_t stream = ccv_nnc_stream_context_get_stream(stream_context);
+	const int step = cmd.info.adam.step;
+	const float rate = cmd.info.adam.rate;
+	const float beta1 = cmd.info.adam.beta1;
+	const float beta2 = cmd.info.adam.beta2;
+	const float decay = cmd.info.adam.decay;
+	const float epsilon = cmd.info.adam.epsilon;
+	assert(step >= 1);
+	const float rate_inv_bias_correction1 = rate / (1 - powf(beta1, step));
+	const float inv_bias_correction2 = 1. / (1 - powf(beta2, step));
+	const float rate_decay = rate * decay;
+	assert(inputs[1]->info.datatype == inputs[2]->info.datatype &&
+		inputs[2]->info.datatype == inputs[3]->info.datatype &&
+		inputs[3]->info.datatype == outputs[0]->info.datatype &&
+		outputs[0]->info.datatype == outputs[1]->info.datatype &&
+		outputs[1]->info.datatype == outputs[2]->info.datatype);
+	assert(CCV_IS_TENSOR_CONTIGUOUS(inputs[0]));
+	assert(CCV_IS_TENSOR_CONTIGUOUS(inputs[1]));
+	assert(CCV_IS_TENSOR_CONTIGUOUS(inputs[2]));
+	assert(CCV_IS_TENSOR_CONTIGUOUS(inputs[3]));
+	assert(CCV_IS_TENSOR_CONTIGUOUS(outputs[0]));
+	assert(CCV_IS_TENSOR_CONTIGUOUS(outputs[1]));
+	assert(CCV_IS_TENSOR_CONTIGUOUS(outputs[2]));
+	const ccv_nnc_tensor_view_t* const g = (ccv_nnc_tensor_view_t*)inputs[0];
+	const ccv_nnc_tensor_view_t* const a = (ccv_nnc_tensor_view_t*)inputs[1];
+	const ccv_nnc_tensor_view_t* const m = (ccv_nnc_tensor_view_t*)inputs[2];
+	const ccv_nnc_tensor_view_t* const v = (ccv_nnc_tensor_view_t*)inputs[3];
+	ccv_nnc_tensor_view_t* const b = (ccv_nnc_tensor_view_t*)outputs[0];
+	ccv_nnc_tensor_view_t* const n = (ccv_nnc_tensor_view_t*)outputs[1];
+	ccv_nnc_tensor_view_t* const u = (ccv_nnc_tensor_view_t*)outputs[2];
+	const size_t tensor_count = ccv_nnc_tensor_count(g->info);
+	assert(tensor_count ==  ccv_nnc_tensor_count(a->info));
+	assert(tensor_count ==  ccv_nnc_tensor_count(m->info));
+	assert(tensor_count ==  ccv_nnc_tensor_count(v->info));
+	assert(tensor_count ==  ccv_nnc_tensor_count(b->info));
+	assert(tensor_count ==  ccv_nnc_tensor_count(n->info));
+	assert(tensor_count ==  ccv_nnc_tensor_count(u->info));
+	if (g->info.datatype == CCV_16F)
+	{
+		if (b->info.datatype == CCV_16F)
+			_ccv_nnc_adamw_kernel<<<CUDA_GET_BLOCKS(tensor_count), CUDA_NUM_THREADS, 0, stream>>>(tensor_count, beta1, beta2, rate_decay, rate_inv_bias_correction1, inv_bias_correction2, epsilon, (__half*)g->data.f16, (__half*)a->data.f16, (__half*)m->data.f16, (__half*)v->data.f16, (__half*)b->data.f16, (__half*)n->data.f16, (__half*)u->data.f16);
+		else if (b->info.datatype == CCV_32F)
+			_ccv_nnc_adamw_kernel<<<CUDA_GET_BLOCKS(tensor_count), CUDA_NUM_THREADS, 0, stream>>>(tensor_count, beta1, beta2, rate_decay, rate_inv_bias_correction1, inv_bias_correction2, epsilon, (__half*)g->data.f16, a->data.f32, m->data.f32, v->data.f32, b->data.f32, n->data.f32, u->data.f32);
+	} else if (g->info.datatype == CCV_32F) {
+		if (b->info.datatype == CCV_16F)
+			_ccv_nnc_adamw_kernel<<<CUDA_GET_BLOCKS(tensor_count), CUDA_NUM_THREADS, 0, stream>>>(tensor_count, beta1, beta2, rate_decay, rate_inv_bias_correction1, inv_bias_correction2, epsilon, g->data.f32, (__half*)a->data.f16, (__half*)m->data.f16, (__half*)v->data.f16, (__half*)b->data.f16, (__half*)n->data.f16, (__half*)u->data.f16);
+		else if (b->info.datatype == CCV_32F)
+			_ccv_nnc_adamw_kernel<<<CUDA_GET_BLOCKS(tensor_count), CUDA_NUM_THREADS, 0, stream>>>(tensor_count, beta1, beta2, rate_decay, rate_inv_bias_correction1, inv_bias_correction2, epsilon, g->data.f32, a->data.f32, m->data.f32, v->data.f32, b->data.f32, n->data.f32, u->data.f32);
+	}
+	return CCV_NNC_EXEC_SUCCESS;
+}
+
+static int _ccv_nnc_adamw_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	return CCV_NNC_EXEC_INVALID;
+}
+
+#endif
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_ADAMW_FORWARD, CCV_NNC_BACKEND_GPU_REF)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+#ifdef HAVE_CUDA
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_NHWC;
+	registry->tensor_datatypes = CCV_32F | CCV_16F;
+	registry->tensor_memory = CCV_TENSOR_GPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_adamw_forw;
+#endif
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_ADAMW_BACKWARD, CCV_NNC_BACKEND_GPU_REF)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+#ifdef HAVE_CUDA
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_NHWC;
+	registry->tensor_datatypes = CCV_32F | CCV_16F;
+	registry->tensor_memory = CCV_TENSOR_GPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_adamw_back;
+#endif
+}
