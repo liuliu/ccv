f2d5be42b756448754c9691276b5f7fd11fe0263
weiyan <weiyanlin117@gmail.com>
2023-07-22 17:56:05 -0700

    clean up and make it work

--- lib/nnc/cmd/loss/mps/ccv_nnc_mse_mps.m
+++ lib/nnc/cmd/loss/mps/ccv_nnc_mse_mps.m
@@ -7,7 +7,6 @@
 
 static int _ccv_nnc_mse_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
 {
-    printf(_ccv_nnc_mse_forw);
 	assert(input_size == 2);
 	const ccv_nnc_tensor_view_t* a = (ccv_nnc_tensor_view_t*)inputs[0];
 	assert(ccv_nnc_tensor_nd(a->info.dim) <= 2);
@@ -15,22 +14,12 @@ static int _ccv_nnc_mse_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint,
 	assert(output_size == 1);
 	ccv_nnc_tensor_view_t* c = (ccv_nnc_tensor_view_t*)outputs[0];
 	int dim[CCV_NNC_MAX_DIM_ALLOC];
-	int astride[CCV_NNC_MAX_DIM_ALLOC];
-	int bstride[CCV_NNC_MAX_DIM_ALLOC];
-	int cstride[CCV_NNC_MAX_DIM_ALLOC];
     ccv_nnc_tensor_view_get_dim(a, dim);
 	assert(ccv_nnc_tensor_view_check_dim(b, dim));
-	ccv_nnc_tensor_view_get_stride(a, astride);
-	ccv_nnc_tensor_view_get_stride(b, bstride);
-	ccv_nnc_tensor_view_get_stride(c, cstride);
 	assert(ccv_nnc_tensor_nd(a->info.dim) <= 2);
 	const int batch_size = dim[CCV_NNC_MAX_DIM];
 	assert(ccv_nnc_tensor_count(c->info) == batch_size);
-	const int count = dim[CCV_NNC_MAX_DIM + 1];
-	const int astep = astride[CCV_NNC_MAX_DIM];
-	const int bstep = bstride[CCV_NNC_MAX_DIM];
-	const int cstep = ccv_nnc_tensor_nd(c->info.dim) == 1 ? 1 : cstride[CCV_NNC_MAX_DIM];
-
+	const int a_nd = ccv_nnc_tensor_nd(a->info.dim);
 	@autoreleasepool {
 		MPSCommandBuffer* command_buffer = ccv_nnc_stream_context_get_command_buffer(stream_context);
 		ccv_nnc_mps_graph_key_t key = ccv_nnc_mps_graph_key_new(cmd, hint, flags, inputs, input_size, outputs, output_size);
@@ -52,9 +41,23 @@ static int _ccv_nnc_mse_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint,
                                                           secondaryTensor:mps_b
                                                                      name:nil];
             MPSGraphTensor* diffSquareTensor = [graph squareWithTensor:diffTensor name:nil];
-            MPSGraphTensor* mps_c = ccv_nnc_mps_reduce_tensor(diffSquareTensor,cmd.info.mse.reduce_op, graph, a->info.dim);
+            NSUInteger axesCount = ccv_nnc_tensor_nd(a->info.dim);
+            NSMutableArray<NSNumber*>* axes = [NSMutableArray new];
+			int i;
+			for (i = 0; i < a_nd; i++) {
+				if (a->info.dim[i] != c->info.dim[i])
+					[axes addObject:@(i)];
+			}
+            MPSGraphTensor* mps_c;
+            if (cmd.info.mse.reduce_op == CCV_NNC_MSE_REDUCE_MEAN) {
+                mps_c = [graph meanOfTensor:diffSquareTensor axes:axes name:nil];
+            } else {
+                assert(cmd.info.mse.reduce_op == CCV_NNC_MSE_REDUCE_SUM);
+                mps_c = [graph reductionSumWithTensor:diffSquareTensor axes:axes name:nil];
+            }
 			[resultTensors addObject:mps_c];
 		});
+
 		MPSGraphTensorData* data_a = ccv_nnc_mps_graph_tensor_data(a, a->info.dim, a->stride);
 		MPSGraphTensorData* data_b = ccv_nnc_mps_graph_tensor_data(b, b->info.dim, b->stride);
 		MPSGraphTensorData* data[] = {data_a, data_b};
