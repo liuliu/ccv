d9c56e214a284a696ec66be1f3da1e72f2cc1b53
Liu Liu <i@liuliu.me>
2023-08-15 23:55:55 -0400

    Implement cpu ref for sdp back pass
    Fixed GEMM back pass (cpu ref) doesn't handle multi-batch dimensions.

--- lib/nnc/cmd/scaled_dot_product_attention/ccv_nnc_scaled_dot_product_attention_cpu_ref.c
+++ lib/nnc/cmd/scaled_dot_product_attention/ccv_nnc_scaled_dot_product_attention_cpu_ref.c
@@ -73,9 +73,9 @@ static int _ccv_nnc_scaled_dot_product_attention_forw(const ccv_nnc_cmd_t cmd, c
 	ccv_nnc_tensor_view_get_stride(c, cstride);
 	if (q_nd == 3)
 	{
-		qstride[0] = qstride[1], qstride[1] = qstride[2], qstride[2] = cstride[3];
-		kstride[0] = kstride[1], kstride[1] = kstride[2], kstride[2] = cstride[3];
-		vstride[0] = vstride[1], vstride[1] = vstride[2], vstride[2] = cstride[3];
+		qstride[0] = qstride[1], qstride[1] = qstride[2], qstride[2] = qstride[3];
+		kstride[0] = kstride[1], kstride[1] = kstride[2], kstride[2] = kstride[3];
+		vstride[0] = vstride[1], vstride[1] = vstride[2], vstride[2] = vstride[3];
 		cstride[0] = cstride[1], cstride[1] = cstride[2], cstride[2] = cstride[3];
 	}
 	if (saved_softmax)
@@ -272,6 +272,198 @@ static int _ccv_nnc_scaled_dot_product_attention_forw(const ccv_nnc_cmd_t cmd, c
 
 static int _ccv_nnc_scaled_dot_product_attention_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
 {
+	// Assuming no saved_softmax, we need to recompute from q, k, v.
+	// We cannot do this with masks (yet).
+	assert(input_size >= 6);
+	assert(!cmd.info.scaled_dot_product_attention.is_causal);
+	ccv_nnc_tensor_view_t* const g = (ccv_nnc_tensor_view_t*)inputs[0];
+	ccv_nnc_tensor_view_t* const q = (ccv_nnc_tensor_view_t*)inputs[3];
+	ccv_nnc_tensor_view_t* const k = (ccv_nnc_tensor_view_t*)inputs[4];
+	ccv_nnc_tensor_view_t* const v = (ccv_nnc_tensor_view_t*)inputs[5];
+	ccv_nnc_tensor_view_t* const dq = (ccv_nnc_tensor_view_t*)outputs[0];
+	ccv_nnc_tensor_view_t* const dk = (ccv_nnc_tensor_view_t*)outputs[1];
+	ccv_nnc_tensor_view_t* const dv = (ccv_nnc_tensor_view_t*)outputs[2];
+	const int q_nd = ccv_nnc_tensor_nd(q->info.dim);
+	assert(q_nd == 3 || q_nd == 4);
+	const int k_nd = ccv_nnc_tensor_nd(k->info.dim);
+	assert(k_nd == 3 || k_nd == 4);
+	const int v_nd = ccv_nnc_tensor_nd(v->info.dim);
+	assert(v_nd == 3 || v_nd == 4);
+	const int g_nd = ccv_nnc_tensor_nd(g->info.dim);
+	assert(g_nd == 3 || g_nd == 4);
+	const int dq_nd = ccv_nnc_tensor_nd(dq->info.dim);
+	assert(dq_nd == 3 || dq_nd == 4);
+	assert(dq_nd == q_nd);
+	const int dk_nd = ccv_nnc_tensor_nd(dk->info.dim);
+	assert(dk_nd == 3 || dk_nd == 4);
+	assert(dk_nd == k_nd);
+	const int dv_nd = ccv_nnc_tensor_nd(dv->info.dim);
+	assert(dv_nd == 3 || dv_nd == 4);
+	assert(dv_nd == v_nd);
+	assert(q_nd == k_nd && k_nd == v_nd && v_nd == g_nd);
+	// Assuming this is float 32.
+	int qdim[CCV_NNC_MAX_DIM_ALLOC];
+	int kdim[CCV_NNC_MAX_DIM_ALLOC];
+	int vdim[CCV_NNC_MAX_DIM_ALLOC];
+	int gdim[CCV_NNC_MAX_DIM_ALLOC];
+	int dqdim[CCV_NNC_MAX_DIM_ALLOC];
+	int dkdim[CCV_NNC_MAX_DIM_ALLOC];
+	int dvdim[CCV_NNC_MAX_DIM_ALLOC];
+	ccv_nnc_tensor_view_get_dim(q, qdim);
+	ccv_nnc_tensor_view_get_dim(k, kdim);
+	ccv_nnc_tensor_view_get_dim(v, vdim);
+	ccv_nnc_tensor_view_get_dim(g, gdim);
+	ccv_nnc_tensor_view_get_dim(dq, dqdim);
+	ccv_nnc_tensor_view_get_dim(dk, dkdim);
+	ccv_nnc_tensor_view_get_dim(dv, dvdim);
+	if (q_nd == 3)
+	{
+		qdim[0] = qdim[1], qdim[1] = qdim[2], qdim[2] = 1;
+		kdim[0] = kdim[1], kdim[1] = kdim[2], kdim[2] = 1;
+		vdim[0] = vdim[1], vdim[1] = vdim[2], vdim[2] = 1;
+		gdim[0] = gdim[1], gdim[1] = gdim[2], gdim[2] = 1;
+		dqdim[0] = dqdim[1], dqdim[1] = dqdim[2], dqdim[2] = 1;
+		dkdim[0] = dkdim[1], dkdim[1] = dkdim[2], dkdim[2] = 1;
+		dvdim[0] = dvdim[1], dvdim[1] = dvdim[2], dvdim[2] = 1;
+	}
+	assert(qdim[0] == kdim[0] && kdim[0] == vdim[0] && vdim[0] == gdim[0]);
+	assert(qdim[2] == kdim[2] && kdim[2] == vdim[2] && vdim[2] == gdim[2]);
+	assert(qdim[3] == kdim[3]);
+	assert(kdim[1] == vdim[1]);
+	assert(gdim[1] == qdim[1]);
+	assert(gdim[3] == vdim[3]);
+	assert(CCV_NNC_MAX_DIM == 2); // Need to change this logic for CCV_NNC_MAX_DIM == other number.
+	int qstride[CCV_NNC_MAX_DIM_ALLOC];
+	int kstride[CCV_NNC_MAX_DIM_ALLOC];
+	int vstride[CCV_NNC_MAX_DIM_ALLOC];
+	int gstride[CCV_NNC_MAX_DIM_ALLOC];
+	int dqstride[CCV_NNC_MAX_DIM_ALLOC];
+	int dkstride[CCV_NNC_MAX_DIM_ALLOC];
+	int dvstride[CCV_NNC_MAX_DIM_ALLOC];
+	ccv_nnc_tensor_view_get_stride(q, qstride);
+	ccv_nnc_tensor_view_get_stride(k, kstride);
+	ccv_nnc_tensor_view_get_stride(v, vstride);
+	ccv_nnc_tensor_view_get_stride(g, gstride);
+	ccv_nnc_tensor_view_get_stride(dq, dqstride);
+	ccv_nnc_tensor_view_get_stride(dk, dkstride);
+	ccv_nnc_tensor_view_get_stride(dv, dvstride);
+	if (q_nd == 3)
+	{
+		qstride[0] = qstride[1], qstride[1] = qstride[2], qstride[2] = qstride[3];
+		kstride[0] = kstride[1], kstride[1] = kstride[2], kstride[2] = kstride[3];
+		vstride[0] = vstride[1], vstride[1] = vstride[2], vstride[2] = vstride[3];
+		gstride[0] = gstride[1], gstride[1] = gstride[2], gstride[2] = gstride[3];
+		dqstride[0] = dqstride[1], dqstride[1] = dqstride[2], dqstride[2] = dqstride[3];
+		dkstride[0] = dkstride[1], dkstride[1] = dkstride[2], dkstride[2] = dkstride[3];
+		dvstride[0] = dvstride[1], dvstride[1] = dvstride[2], dvstride[2] = dvstride[3];
+	}
+	int i[CCV_NNC_MAX_DIM + 2];
+	float* qk = ccv_nnc_stream_context_get_workspace(stream_context, sizeof(float) * 2 * kdim[1], CCV_TENSOR_CPU_MEMORY);
+	const float* const qp = q->data.f32;
+	const float* const kp = k->data.f32;
+	const float* const vp = v->data.f32;
+	const float* const gp = g->data.f32;
+	float* const dqp = dq->data.f32;
+	float* const dkp = dk->data.f32;
+	float* const dvp = dv->data.f32;
+	const float scale = cmd.info.scaled_dot_product_attention.scale;
+	for (i[0] = 0; i[0] < qdim[0]; i[0]++)
+	{
+		const float* const qp0 = qp + i[0] * qstride[0];
+		const float* const kp0 = kp + i[0] * kstride[0];
+		const float* const vp0 = vp + i[0] * vstride[0];
+		const float* const gp0 = gp + i[0] * gstride[0];
+		float* const dqp0 = dqp + i[0] * dqstride[0];
+		float* const dkp0 = dkp + i[0] * dkstride[0];
+		float* const dvp0 = dvp + i[0] * dvstride[0];
+		for (i[1] = 0; i[1] < qdim[2]; i[1]++)
+		{
+			const float* const qp1 = qp0 + i[1] * qstride[2];
+			const float* const kp1 = kp0 + i[1] * kstride[2];
+			const float* const vp1 = vp0 + i[1] * vstride[2];
+			const float* const gp1 = gp0 + i[1] * gstride[2];
+			float* const dqp1 = dqp0 + i[1] * dqstride[2];
+			float* const dkp1 = dkp0 + i[1] * dkstride[2];
+			float* const dvp1 = dvp0 + i[1] * dvstride[2];
+			// Compute Q @ K^T
+			int x, y, k;
+			for (y = 0; y < kdim[1]; y++)
+			{
+				float* const dvp2 = dvp1 + y * dvstride[1];
+				for (k = 0; k < vdim[3]; k++)
+					dvp2[k * dvstride[3]] = 0;
+			}
+			for (x = 0; x < qdim[1]; x++)
+			{
+				float* const dqp2 = dqp1 + x * dqstride[1];
+				for (k = 0; k < qdim[3]; k++)
+					dqp2[k * dqstride[3]] = 0;
+			}
+			for (y = 0; y < kdim[1]; y++)
+			{
+				float* const dkp2 = dkp1 + y * dkstride[1];
+				for (k = 0; k < qdim[3]; k++)
+					dkp2[k * dkstride[3]] = 0;
+			}
+			for (x = 0; x < qdim[1]; x++)
+			{
+				const float* const qp2 = qp1 + x * qstride[1];
+				const float* const gp2 = gp1 + x * gstride[1];
+				float* const qk0 = qk;
+				float* const qks0 = qk + kdim[1];
+				for (y = 0; y < kdim[1]; y++)
+				{
+					const float* const kp2 = kp1 + y * kstride[1];
+					float v = 0;
+					for (k = 0; k < qdim[3]; k++)
+						v += qp2[k * qstride[3]] * kp2[k * kstride[3]];
+					qk0[y] = scale * v;
+				}
+				// Compute softmax on qk.
+				double maxval = qk0[0];
+				for (y = 1; y < kdim[1]; y++)
+					if (qk0[y] > maxval)
+						maxval = qk0[y];
+				double sumval = 0;
+				for (y = 0; y < kdim[1]; y++)
+					sumval += (qk0[y] = expf(qk0[y] - maxval));
+				sumval = 1.0 / sumval;
+				for (y = 0; y < kdim[1]; y++)
+					qk0[y] *= sumval;
+				for (y = 0; y < kdim[1]; y++)
+				{
+					float* const dvp2 = dvp1 + y * dvstride[1];
+					const float v = qk0[y];
+					for (k = 0; k < vdim[3]; k++)
+						dvp2[k * dvstride[3]] += v * gp2[k * gstride[3]];
+				}
+				sumval = 0;
+				for (y = 0; y < kdim[1]; y++)
+				{
+					const float* const vp2 = vp1 + y * vstride[1];
+					float v = 0;
+					for (k = 0; k < vdim[3]; k++)
+						v += gp2[k * gstride[3]] * vp2[k * vstride[3]];
+					qks0[y] = v;
+					sumval += v * qk0[y];
+				}
+				for (y = 0; y < kdim[1]; y++)
+					qk0[y] = (qks0[y] - sumval) * qk0[y];
+				float* const dqp2 = dqp1 + x * dqstride[1];
+				for (y = 0; y < kdim[1]; y++)
+				{
+					const float* const kp2 = kp1 + y * kstride[1];
+					float* const dkp2 = dkp1 + y * dkstride[1];
+					const float v = scale * qk0[y];
+					for (k = 0; k < qdim[3]; k++)
+					{
+						dqp2[k * dqstride[3]] += v * kp2[k * kstride[3]];
+						dkp2[k * dkstride[3]] += v * qp2[k * qstride[3]];
+					}
+				}
+			}
+		}
+	}
 	return CCV_NNC_EXEC_SUCCESS;
 }
 
