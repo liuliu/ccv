a0c00639ff173a839f8285b80a9368893f088562
Liu Liu <i@liuliu.me>
2023-08-17 01:03:47 -0400

    Add reduce mean.

--- lib/nnc/cmd/reduce/mps/ccv_nnc_reduce_mean_mps.m
+++ lib/nnc/cmd/reduce/mps/ccv_nnc_reduce_mean_mps.m
@@ -68,3 +68,60 @@ REGISTER_COMMAND_BACKEND(CCV_NNC_REDUCE_MEAN_FORWARD, CCV_NNC_BACKEND_MPS)(ccv_n
 	registry->algorithms = 1;
 	registry->exec = _ccv_nnc_reduce_mean_forw;
 }
+
+static int _ccv_nnc_reduce_mean_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	assert(input_size >= 1);
+	assert(output_size >= 1);
+	ccv_nnc_tensor_view_t atv = ccv_nnc_get_tensor_view(outputs[0]);
+	ccv_nnc_tensor_view_t btv = ccv_nnc_get_tensor_view(inputs[0]);
+	ccv_nnc_tensor_view_t* tvs[] = {
+		&atv, &btv
+	};
+	ccv_nnc_tensor_view_alignment(tvs, 2);
+	const int a_nd = ccv_nnc_tensor_nd(atv.info.dim);
+	int noop = 1;
+	int i;
+	for (i = 0; noop && i < a_nd; i++)
+		noop = btv.info.dim[i] != atv.info.dim[i];
+	const float scale = (float)ccv_nnc_tensor_count(btv.info) / (float)ccv_nnc_tensor_count(atv.info);
+	@autoreleasepool {
+		MPSCommandBuffer* command_buffer = ccv_nnc_stream_context_start_mps_command_buffer(stream_context);
+		ccv_nnc_mps_graph_key_t key = ccv_nnc_mps_graph_key_new(cmd, hint, flags, inputs, input_size, outputs, output_size);
+		int indices[1];
+		MPSGraphExecutable* executable = ccv_nnc_mps_graph_executable_cache(key, indices, ^void (MPSGraph* graph, NSMutableArray<MPSGraphTensor*>* inputTensors, NSMutableArray<MPSGraphShapedType*>* inputShapedTypes, NSMutableArray<MPSGraphTensor*>* resultTensors) {
+			MPSGraphTensor* mps_input_b;
+			MPSGraphTensor* mps_b = ccv_nnc_mps_graph_tensor_input(graph, &btv, btv.info.dim, btv.stride, &mps_input_b);
+			[inputTensors addObject:mps_input_b];
+			MPSGraphShapedType* mps_b_shape = ccv_nnc_mps_graph_tensor_input_shape(&btv, btv.info.dim, btv.stride);
+			[inputShapedTypes addObject:mps_b_shape];
+			if (noop)
+			{
+				MPSGraphTensor* mps_a = [graph multiplicationWithPrimaryTensor:mps_b secondaryTensor:[graph constantWithScalar:scale dataType:ccv_nnc_mps_datatype(btv.info.datatype)] name:nil];
+				[resultTensors addObject:mps_a];
+			} else {
+				NSMutableArray<NSNumber*>* shape = [NSMutableArray new];
+				int i;
+				for (i = 0; i < a_nd; i++)
+					[shape addObject:@(atv.info.dim[i])];
+				MPSGraphTensor* mps_a = [graph multiplicationWithPrimaryTensor:mps_b secondaryTensor:[graph constantWithScalar:scale dataType:ccv_nnc_mps_datatype(btv.info.datatype)] name:nil];
+				mps_a = [graph broadcastTensor:mps_a toShape:shape name:nil];
+				[shape release];
+				[resultTensors addObject:mps_a];
+			}
+		});
+		MPSGraphTensorData* data_b = ccv_nnc_mps_graph_tensor_data(&btv, btv.info.dim, btv.stride);
+		ccv_nnc_mps_graph_executable_result(executable, command_buffer, @[data_b], &tvs[0], (int*[]){ atv.info.dim }, (int*[]){ atv.stride }, 1);
+		ccv_nnc_stream_context_finish_mps_command_buffer(stream_context, command_buffer);
+	}
+	return CCV_NNC_EXEC_SUCCESS;
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_REDUCE_MEAN_BACKWARD, CCV_NNC_BACKEND_MPS)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_NHWC | CCV_TENSOR_FORMAT_CHWN;
+	registry->tensor_datatypes = CCV_32F | CCV_16F;
+	registry->tensor_memory = CCV_TENSOR_GPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_reduce_mean_back;
+}
