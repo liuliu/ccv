2b4706ffd41a2110a18725775efccb7851f784b0
Philip Turner <philipturner.AR@gmail.com>
2024-07-13 12:53:49 -0400

    Added the description of the shader cache

---
+++ lib/nnc/mfa/GEMM/GEMMShaderCache.hpp
@@ -0,0 +1,9 @@
+#ifndef GEMMShaderCache_hpp
+#define GEMMShaderCache_hpp
+
+/// The design for this cache differs substantially from the Swift version. I
+/// took the time to explore a variation that minimized certain latencies. For
+/// example, the Metal device and core count are determined externally to the
+/// `GEMMKernelDescriptor` constructor.
+
+#endif /* GEMMShaderCache_hpp */
