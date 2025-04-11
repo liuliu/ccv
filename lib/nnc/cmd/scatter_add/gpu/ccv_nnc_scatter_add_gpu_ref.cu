28b1959370e19651d41b6504030a29703a6cc775
Liu Liu <i@liuliu.me>
2025-04-11 18:05:55 -0400

    Add scatter_add GPU
    Missing: segmented_gemm

---
+++ lib/nnc/cmd/scatter_add/gpu/ccv_nnc_scatter_add_gpu_ref.cu
@@ -0,0 +1,136 @@
+extern "C" {
+#include <ccv.h>
+#include <ccv_internal.h>
+#include <nnc/ccv_nnc.h>
+#include <nnc/ccv_nnc_easy.h>
+#include <nnc/ccv_nnc_internal.h>
+}
+#include <nnc/gpu/ccv_nnc_compat.h>
+
+template<typename NUM>
+__global__ void _ccv_nnc_scatter_add_zero_kernel(const int n, const int d, NUM* const a, const int a_inc)
+{
+	CUDA_1D_KERNEL_LOOP(i, n) {
+		const int j = i % d;
+		const int k = i / d;
+		a[k * a_inc + j] = 0;
+	}
+}
+
+template<typename NUM>
+__global__ void _ccv_nnc_scatter_add_forw_kernel(const int n, const int d, const NUM* const a, const int a_inc, const int* const indices, NUM* const b, const int b_inc)
+{
+	CUDA_1D_KERNEL_LOOP(i, d) {
+		for (int dest = 0; dest < n; dest++)
+		{
+			const int src = indices[dest];
+			b[src * b_inc + i] += a[dest * a_inc + i];
+		}
+	}
+}
+
+static int _ccv_nnc_scatter_add_forw(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	assert(input_size == 2);
+	assert(output_size == 1);
+	const ccv_nnc_tensor_view_t* const a = (ccv_nnc_tensor_view_t*)inputs[0];
+	const int a_nd = ccv_nnc_tensor_nd(a->info.dim);
+	assert(a_nd <= 2);
+	const ccv_nnc_tensor_view_t* const indices = (ccv_nnc_tensor_view_t*)inputs[1];
+	assert(ccv_nnc_tensor_nd(indices->info.dim) == 1);
+	const ccv_nnc_tensor_view_t* const b = (ccv_nnc_tensor_view_t*)outputs[0];
+	const int b_nd = ccv_nnc_tensor_nd(b->info.dim);
+	assert(b_nd <= 2);
+	const int a_cols = a_nd < 2 ? 1 : a->info.dim[1];
+	const int a_cols_inc = CCV_IS_TENSOR_VIEW(a) ? (a_nd < 2 ? 1 : a->stride[0]) : a_cols;
+	const int a_rows = a->info.dim[0];
+	const int b_cols = b_nd < 2 ? 1 : b->info.dim[1];
+	const int b_cols_inc = CCV_IS_TENSOR_VIEW(b) ? (b_nd < 2 ? 1 : b->stride[0]) : b_cols;
+	const int b_rows = b->info.dim[0];
+	assert(a_rows == indices->info.dim[0]);
+	assert(a_cols == b_cols);
+	assert(a->info.datatype == b->info.datatype);
+	assert(a->info.datatype == CCV_32F || a->info.datatype == CCV_16F);
+	cudaStream_t stream = ccv_nnc_stream_context_get_stream(stream_context);
+	const int b_count = b_rows * b_cols;
+	if (a->info.datatype == CCV_16F)
+	{
+		_ccv_nnc_scatter_add_zero_kernel<<<CUDA_GET_BLOCKS(b_count), CUDA_NUM_THREADS, 0, stream>>>(b_count, b_cols, (__half*)b->data.f16, b_cols_inc);
+		_ccv_nnc_scatter_add_forw_kernel<<<CUDA_GET_BLOCKS(a_cols), CUDA_NUM_THREADS, 0, stream>>>(a_rows, a_cols, (__half*)a->data.f16, a_cols_inc, indices->data.i32, (__half*)b->data.f16, b_cols_inc);
+	} else {
+		_ccv_nnc_scatter_add_zero_kernel<<<CUDA_GET_BLOCKS(b_count), CUDA_NUM_THREADS, 0, stream>>>(b_count, b_cols, b->data.f32, b_cols_inc);
+		_ccv_nnc_scatter_add_forw_kernel<<<CUDA_GET_BLOCKS(a_cols), CUDA_NUM_THREADS, 0, stream>>>(a_rows, a_cols, a->data.f32, a_cols_inc, indices->data.i32, b->data.f32, b_cols_inc);
+	}
+	return CCV_NNC_EXEC_SUCCESS;
+}
+
+template<typename NUM>
+__global__ void _ccv_nnc_scatter_add_back_kernel(const int n, const int d, const NUM* const a, const int a_inc, const int* const indices, NUM* const b, const int b_inc)
+{
+	CUDA_1D_KERNEL_LOOP(i, n) {
+		const int j = i % d;
+		const int dest = i / d;
+		const int src = indices[dest];
+		b[dest * b_inc + j] = a[src * a_inc + j];
+	}
+}
+
+static int _ccv_nnc_scatter_add_back(const ccv_nnc_cmd_t cmd, const ccv_nnc_hint_t hint, const int flags, ccv_nnc_tensor_t* const* const inputs, const int input_size, ccv_nnc_tensor_t* const* const outputs, const int output_size, ccv_nnc_stream_context_t* const stream_context)
+{
+	assert(input_size >= 3);
+	assert(output_size <= 2);
+	const ccv_nnc_tensor_view_t* const g = (ccv_nnc_tensor_view_t*)inputs[0];
+	const int g_nd = ccv_nnc_tensor_nd(g->info.dim);
+	assert(g_nd <= 2);
+	const ccv_nnc_tensor_view_t* const indices = (ccv_nnc_tensor_view_t*)inputs[2];
+	assert(ccv_nnc_tensor_nd(indices->info.dim) == 1);
+	const ccv_nnc_tensor_view_t* const h = (ccv_nnc_tensor_view_t*)outputs[0];
+	const int h_nd = ccv_nnc_tensor_nd(h->info.dim);
+	assert(h_nd <= 2);
+	cudaStream_t stream = ccv_nnc_stream_context_get_stream(stream_context);
+	if (output_size >= 2 && outputs[1])
+	{
+		const ccv_nnc_tensor_view_t* const output = (ccv_nnc_tensor_view_t*)outputs[1];
+		const int output_nd = ccv_nnc_tensor_nd(output->info.dim);
+		const int output_cols = output_nd < 2 ? 1 : output->info.dim[1];
+		const int output_cols_inc = CCV_IS_TENSOR_VIEW(output) ? (output_nd < 2 ? 1 : output->stride[0]) : output_cols;
+		const int output_rows = output->info.dim[0];
+		const int output_count = output_rows * output_cols;
+		_ccv_nnc_scatter_add_zero_kernel<<<CUDA_GET_BLOCKS(output_count), CUDA_NUM_THREADS, 0, stream>>>(output_count, output_cols, output->data.i32, output_cols_inc);
+	}
+	const int g_cols = g_nd < 2 ? 1 : g->info.dim[1];
+	const int g_cols_inc = CCV_IS_TENSOR_VIEW(g) ? (g_nd < 2 ? 1 : g->stride[0]) : g_cols;
+	const int g_rows = g->info.dim[0];
+	const int h_cols = h_nd < 2 ? 1 : h->info.dim[1];
+	const int h_cols_inc = CCV_IS_TENSOR_VIEW(h) ? (h_nd < 2 ? 1 : h->stride[0]) : h_cols;
+	const int h_rows = h->info.dim[0];
+	assert(h_rows == indices->info.dim[0]);
+	assert(g_cols == h_cols);
+	assert(indices->info.datatype == CCV_32S);
+	assert(g->info.datatype == h->info.datatype);
+	assert(g->info.datatype == CCV_32F || g->info.datatype == CCV_16F);
+	const int count = h_rows * h_cols;
+	if (g->info.datatype == CCV_16F)
+		_ccv_nnc_scatter_add_back_kernel<<<CUDA_GET_BLOCKS(count), CUDA_NUM_THREADS, 0, stream>>>(count, h_cols, (__half*)g->data.f16, g_cols_inc, indices->data.i32, (__half*)h->data.f16, h_cols_inc);
+	else
+		_ccv_nnc_scatter_add_back_kernel<<<CUDA_GET_BLOCKS(count), CUDA_NUM_THREADS, 0, stream>>>(count, h_cols, g->data.f32, g_cols_inc, indices->data.i32, h->data.f32, h_cols_inc);
+	return CCV_NNC_EXEC_SUCCESS;
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_SCATTER_ADD_FORWARD, CCV_NNC_BACKEND_GPU_REF)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_NHWC;
+	registry->tensor_datatypes = CCV_32F | CCV_32S | CCV_16F;
+	registry->tensor_memory = CCV_TENSOR_GPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_scatter_add_forw;
+}
+
+REGISTER_COMMAND_BACKEND(CCV_NNC_SCATTER_ADD_BACKWARD, CCV_NNC_BACKEND_GPU_REF)(ccv_nnc_cmd_backend_registry_t* const registry)
+{
+	registry->tensor_formats = CCV_TENSOR_FORMAT_NCHW | CCV_TENSOR_FORMAT_NHWC;
+	registry->tensor_datatypes = CCV_32F | CCV_32S | CCV_16F;
+	registry->tensor_memory = CCV_TENSOR_GPU_MEMORY;
+	registry->algorithms = 1;
+	registry->exec = _ccv_nnc_scatter_add_back;
+}
---
+++ test/int/nnc/scatter_add.tests.c
@@ -0,0 +1,298 @@
+#include "case.h"
+#include "ccv_case.h"
+#include "ccv_nnc_case.h"
+#include <ccv.h>
+#include <nnc/ccv_nnc.h>
+#include <nnc/ccv_nnc_easy.h>
+#include <3rdparty/dsfmt/dSFMT.h>
+
+TEST_SETUP()
+{
+	ccv_nnc_init();
+}
+
+TEST_CASE("scatter add a tensor")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_FORWARD, CCV_NNC_BACKEND_GPU_REF) || ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_FORWARD, CCV_NNC_BACKEND_MPS));
+	float bp[] = {
+		1, 2,
+		2, 3,
+	};
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 3, 2), 0);
+	int ip[] = {1, 1};
+	ccv_nnc_tensor_t* const indices = ccv_nnc_tensor_new(ip, CPU_TENSOR_NHWC(32S, 2), 0);
+	ccv_nnc_tensor_t* const b = ccv_nnc_tensor_new(bp, CPU_TENSOR_NHWC(32F, 2, 2), 0);
+	ccv_nnc_tensor_t* const ga = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 3, 2), 0);
+	ccv_nnc_tensor_t* const gindices = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32S, 2), 0);
+	ccv_nnc_tensor_t* const gb = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 2, 2), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(b, indices), TENSOR_LIST(gb, gindices), 0);
+	ccv_nnc_cmd_exec(CMD_SCATTER_ADD_FORWARD(3), ccv_nnc_no_hint, 0, TENSOR_LIST(gb, gindices), TENSOR_LIST(ga), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(ga), TENSOR_LIST(a), 0);
+	float atp[] = {
+		0, 0,
+		3, 5,
+		0, 0,
+	};
+	ccv_nnc_tensor_t const at = ccv_nnc_tensor(atp, CPU_TENSOR_NHWC(32F, 3, 2), 0);
+	REQUIRE_TENSOR_EQ(a, &at, "should be equal");
+	ccv_nnc_tensor_free(a);
+	ccv_nnc_tensor_free(indices);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(ga);
+	ccv_nnc_tensor_free(gindices);
+	ccv_nnc_tensor_free(gb);
+}
+
+TEST_CASE("scatter add a 1d tensor")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_FORWARD, CCV_NNC_BACKEND_GPU_REF) || ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_FORWARD, CCV_NNC_BACKEND_MPS));
+	float bp[] = {
+		4, 3, 5,
+	};
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 5), 0);
+	int ip[] = {3, 2, 4};
+	ccv_nnc_tensor_t* const indices = ccv_nnc_tensor_new(ip, CPU_TENSOR_NHWC(32S, 3), 0);
+	ccv_nnc_tensor_t* const b = ccv_nnc_tensor_new(bp, CPU_TENSOR_NHWC(32F, 3), 0);
+	ccv_nnc_tensor_t* const ga = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 5), 0);
+	ccv_nnc_tensor_t* const gindices = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32S, 3), 0);
+	ccv_nnc_tensor_t* const gb = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 3), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(b, indices), TENSOR_LIST(gb, gindices), 0);
+	ccv_nnc_cmd_exec(CMD_SCATTER_ADD_FORWARD(5), ccv_nnc_no_hint, 0, TENSOR_LIST(gb, gindices), TENSOR_LIST(ga), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(ga), TENSOR_LIST(a), 0);
+	float atp[] = {
+		0, 0, 3, 4, 5
+	};
+	ccv_nnc_tensor_t const at = ccv_nnc_tensor(atp, CPU_TENSOR_NHWC(32F, 5), 0);
+	REQUIRE_TENSOR_EQ(a, &at, "should be equal");
+	ccv_nnc_tensor_free(a);
+	ccv_nnc_tensor_free(indices);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(ga);
+	ccv_nnc_tensor_free(gindices);
+	ccv_nnc_tensor_free(gb);
+}
+
+TEST_CASE("scatter add a tensor view")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_FORWARD, CCV_NNC_BACKEND_GPU_REF) || ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_FORWARD, CCV_NNC_BACKEND_MPS));
+	float bp[] = {
+		0, 3, 4, 0,
+		0, 1, 5, 0,
+	};
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 3, 4), 0);
+	int i;
+	for (i = 0; i < 3 * 4; i++)
+		a->data.f32[i] = i;
+	ccv_nnc_tensor_view_t* const av = ccv_nnc_tensor_view_new(a, CPU_TENSOR_NHWC(32F, 3, 2), DIM_ALLOC(0, 1), DIM_ALLOC(4, 1));
+	int ip[] = {1, 1};
+	ccv_nnc_tensor_t* const indices = ccv_nnc_tensor_new(ip, CPU_TENSOR_NHWC(32S, 2), 0);
+	ccv_nnc_tensor_t* const b = ccv_nnc_tensor_new(bp, CPU_TENSOR_NHWC(32F, 2, 4), 0);
+	ccv_nnc_tensor_view_t* const bv = ccv_nnc_tensor_view_new(b, CPU_TENSOR_NHWC(32F, 2, 2), DIM_ALLOC(0, 1), DIM_ALLOC(4, 1));
+	ccv_nnc_tensor_t* const ga = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 3, 4), 0);
+	ccv_nnc_tensor_view_t* const gav = ccv_nnc_tensor_view_new(ga, GPU_TENSOR_NHWC(000, 32F, 3, 2), DIM_ALLOC(0, 1), DIM_ALLOC(4, 1));
+	ccv_nnc_tensor_t* const gindices = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32S, 2), 0);
+	ccv_nnc_tensor_t* const gb = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 2, 4), 0);
+	ccv_nnc_tensor_view_t* const gbv = ccv_nnc_tensor_view_new(gb, GPU_TENSOR_NHWC(000, 32F, 2, 2), DIM_ALLOC(0, 1), DIM_ALLOC(4, 1));
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a, indices, b), TENSOR_LIST(ga, gindices, gb), 0);
+	ccv_nnc_cmd_exec(CMD_SCATTER_ADD_FORWARD(3), ccv_nnc_no_hint, 0, TENSOR_LIST((ccv_nnc_tensor_t*)gbv, gindices), TENSOR_LIST((ccv_nnc_tensor_t*)gav), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(ga), TENSOR_LIST(a), 0);
+	float atp[] = {
+		0, 0, 0, 3,
+		4, 4, 9, 7,
+		8, 0, 0, 11,
+	};
+	ccv_nnc_tensor_t const at = ccv_nnc_tensor(atp, CPU_TENSOR_NHWC(32F, 3, 4), 0);
+	REQUIRE_TENSOR_EQ(a, &at, "should be equal");
+	ccv_nnc_tensor_free(a);
+	ccv_nnc_tensor_view_free(av);
+	ccv_nnc_tensor_free(indices);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_view_free(bv);
+	ccv_nnc_tensor_free(ga);
+	ccv_nnc_tensor_view_free(gav);
+	ccv_nnc_tensor_free(gindices);
+	ccv_nnc_tensor_free(gb);
+	ccv_nnc_tensor_view_free(gbv);
+}
+
+TEST_CASE("backward scatter add a tensor")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_BACKWARD, CCV_NNC_BACKEND_GPU_REF) || ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_BACKWARD, CCV_NNC_BACKEND_MPS));
+	float ap[] = {
+		1, 2,
+		2, 3,
+		3, 4,
+	};
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(ap, CPU_TENSOR_NHWC(32F, 3, 2), 0);
+	int ip[] = {1, 1};
+	ccv_nnc_tensor_t* const indices = ccv_nnc_tensor_new(ip, CPU_TENSOR_NHWC(32S, 2), 0);
+	ccv_nnc_tensor_t* const b = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 2, 2), 0);
+	ccv_nnc_tensor_t* const ga = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 3, 2), 0);
+	ccv_nnc_tensor_t* const gindices = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32S, 2), 0);
+	ccv_nnc_tensor_t* const gb = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 2, 2), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a, indices), TENSOR_LIST(ga, gindices), 0);
+	ccv_nnc_cmd_exec(CMD_SCATTER_ADD_BACKWARD(3), ccv_nnc_no_hint, 0, TENSOR_LIST(ga, 0, gindices), TENSOR_LIST(gb), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(gb), TENSOR_LIST(b), 0);
+	float btp[] = {
+		2, 3,
+		2, 3,
+	};
+	ccv_nnc_tensor_t const bt = ccv_nnc_tensor(btp, CPU_TENSOR_NHWC(32F, 2, 2), 0);
+	REQUIRE_TENSOR_EQ(b, &bt, "should be equal");
+	ccv_nnc_tensor_free(a);
+	ccv_nnc_tensor_free(indices);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(ga);
+	ccv_nnc_tensor_free(gindices);
+	ccv_nnc_tensor_free(gb);
+}
+
+TEST_CASE("backward scatter add a 1d tensor")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_BACKWARD, CCV_NNC_BACKEND_GPU_REF) || ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_BACKWARD, CCV_NNC_BACKEND_MPS));
+	float ap[] = {
+		1, 2, 3, 4, 5
+	};
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(ap, CPU_TENSOR_NHWC(32F, 5), 0);
+	int ip[] = {3, 2, 4};
+	ccv_nnc_tensor_t* const indices = ccv_nnc_tensor_new(ip, CPU_TENSOR_NHWC(32S, 3), 0);
+	ccv_nnc_tensor_t* const b = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 3), 0);
+	ccv_nnc_tensor_t* const ga = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 5), 0);
+	ccv_nnc_tensor_t* const gindices = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32S, 3), 0);
+	ccv_nnc_tensor_t* const gb = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 3), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a, indices), TENSOR_LIST(ga, gindices), 0);
+	ccv_nnc_cmd_exec(CMD_SCATTER_ADD_BACKWARD(5), ccv_nnc_no_hint, 0, TENSOR_LIST(ga, 0, gindices), TENSOR_LIST(gb), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(gb), TENSOR_LIST(b), 0);
+	float btp[] = {
+		4, 3, 5
+	};
+	ccv_nnc_tensor_t const bt = ccv_nnc_tensor(btp, CPU_TENSOR_NHWC(32F, 3), 0);
+	REQUIRE_TENSOR_EQ(b, &bt, "should be equal");
+	ccv_nnc_tensor_free(a);
+	ccv_nnc_tensor_free(indices);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(ga);
+	ccv_nnc_tensor_free(gindices);
+	ccv_nnc_tensor_free(gb);
+}
+
+TEST_CASE("backward scatter add a tensor view")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_BACKWARD, CCV_NNC_BACKEND_GPU_REF) || ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_BACKWARD, CCV_NNC_BACKEND_MPS));
+	float ap[] = {
+		1, 2, 3, 4,
+		2, 3, 4, 5,
+		3, 4, 5, 6,
+	};
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(ap, CPU_TENSOR_NHWC(32F, 3, 4), 0);
+	ccv_nnc_tensor_view_t* const av = ccv_nnc_tensor_view_new(a, CPU_TENSOR_NHWC(32F, 3, 2), DIM_ALLOC(0, 1), DIM_ALLOC(4, 1));
+	int ip[] = {1, 1};
+	ccv_nnc_tensor_t* const indices = ccv_nnc_tensor_new(ip, CPU_TENSOR_NHWC(32S, 2), 0);
+	ccv_nnc_tensor_t* const b = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 2, 4), 0);
+	memset(b->data.f32, 0, 2 * 4 * sizeof(float));
+	ccv_nnc_tensor_view_t* const bv = ccv_nnc_tensor_view_new(b, CPU_TENSOR_NHWC(32F, 2, 2), DIM_ALLOC(0, 1), DIM_ALLOC(4, 1));
+	ccv_nnc_tensor_t* const ga = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 3, 4), 0);
+	ccv_nnc_tensor_view_t* const gav = ccv_nnc_tensor_view_new(ga, GPU_TENSOR_NHWC(000, 32F, 3, 2), DIM_ALLOC(0, 1), DIM_ALLOC(4, 1));
+	ccv_nnc_tensor_t* const gindices = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32S, 2), 0);
+	ccv_nnc_tensor_t* const gb = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32F, 2, 4), 0);
+	ccv_nnc_tensor_view_t* const gbv = ccv_nnc_tensor_view_new(gb, GPU_TENSOR_NHWC(000, 32F, 2, 2), DIM_ALLOC(0, 1), DIM_ALLOC(4, 1));
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a, indices, b), TENSOR_LIST(ga, gindices, gb), 0);
+	ccv_nnc_cmd_exec(CMD_SCATTER_ADD_BACKWARD(3), ccv_nnc_no_hint, 0, TENSOR_LIST((ccv_nnc_tensor_t*)gav, 0, gindices), TENSOR_LIST((ccv_nnc_tensor_t*)gbv), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(gb), TENSOR_LIST(b), 0);
+	float btp[] = {
+		0, 3, 4, 0,
+		0, 3, 4, 0,
+	};
+	ccv_nnc_tensor_t const bt = ccv_nnc_tensor(btp, CPU_TENSOR_NHWC(32F, 2, 4), 0);
+	REQUIRE_TENSOR_EQ(b, &bt, "should be equal");
+	ccv_nnc_tensor_free(a);
+	ccv_nnc_tensor_view_free(av);
+	ccv_nnc_tensor_free(indices);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_view_free(bv);
+	ccv_nnc_tensor_free(ga);
+	ccv_nnc_tensor_view_free(gav);
+	ccv_nnc_tensor_free(gindices);
+	ccv_nnc_tensor_free(gb);
+	ccv_nnc_tensor_view_free(gbv);
+}
+
+TEST_CASE("scatter add with half precision")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_FORWARD, CCV_NNC_BACKEND_GPU_REF) || ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_FORWARD, CCV_NNC_BACKEND_MPS));
+	dsfmt_t dsfmt;
+	dsfmt_init_gen_rand(&dsfmt, 0);
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 10), 0);
+	int i;
+	for (i = 0; i < 10 * 10; i++)
+		a->data.f32[i] = dsfmt_genrand_open_close(&dsfmt);
+	ccv_nnc_tensor_t* const a16 = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(16F, 10, 10), 0);
+	ccv_nnc_cmd_exec(CMD_DATATYPE_CONVERSION_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a), TENSOR_LIST(a16), 0);
+	ccv_nnc_tensor_t* const indices = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32S, 10), 0);
+	for (i = 0; i < 10; i++)
+		indices->data.i32[i] = i * 9 + 1;
+	ccv_nnc_tensor_t* const b = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(16F, 100, 10), 0);
+	ccv_nnc_tensor_t* const ga = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 16F, 10, 10), 0);
+	ccv_nnc_tensor_t* const gindices = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32S, 10), 0);
+	ccv_nnc_tensor_t* const gb = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 16F, 100, 10), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a16, indices), TENSOR_LIST(ga, gindices), 0);
+	ccv_nnc_cmd_exec(CMD_SCATTER_ADD_FORWARD(100), ccv_nnc_no_hint, 0, TENSOR_LIST(ga, gindices), TENSOR_LIST(gb), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(gb), TENSOR_LIST(b), 0);
+	ccv_nnc_tensor_t* const bt = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(16F, 100, 10), 0);
+	ccv_nnc_cmd_exec(CMD_SCATTER_ADD_FORWARD(100), ccv_nnc_no_hint, 0, TENSOR_LIST(a16, indices), TENSOR_LIST(bt), 0);
+	ccv_nnc_tensor_t* const b32 = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 100, 10), 0);
+	ccv_nnc_tensor_t* const bt32 = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 100, 10), 0);
+	ccv_nnc_cmd_exec(CMD_DATATYPE_CONVERSION_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(b, bt), TENSOR_LIST(b32, bt32), 0);
+	REQUIRE_TENSOR_EQ(b32, bt32, "should be equal");
+	ccv_nnc_tensor_free(a);
+	ccv_nnc_tensor_free(indices);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(ga);
+	ccv_nnc_tensor_free(gindices);
+	ccv_nnc_tensor_free(gb);
+	ccv_nnc_tensor_free(a16);
+	ccv_nnc_tensor_free(bt);
+	ccv_nnc_tensor_free(b32);
+	ccv_nnc_tensor_free(bt32);
+}
+
+TEST_CASE("backward scatter add with half precision")
+{
+	GUARD_ELSE_RETURN(ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_BACKWARD, CCV_NNC_BACKEND_GPU_REF) || ccv_nnc_cmd_ok(CCV_NNC_SCATTER_ADD_BACKWARD, CCV_NNC_BACKEND_MPS));
+	dsfmt_t dsfmt;
+	dsfmt_init_gen_rand(&dsfmt, 0);
+	ccv_nnc_tensor_t* const a = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 100, 10), 0);
+	int i;
+	for (i = 0; i < 100 * 10; i++)
+		a->data.f32[i] = dsfmt_genrand_open_close(&dsfmt);
+	ccv_nnc_tensor_t* const a16 = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(16F, 100, 10), 0);
+	ccv_nnc_cmd_exec(CMD_DATATYPE_CONVERSION_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a), TENSOR_LIST(a16), 0);
+	ccv_nnc_tensor_t* const indices = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32S, 10), 0);
+	for (i = 0; i < 10; i++)
+		indices->data.i32[i] = i * 9 + 1;
+	ccv_nnc_tensor_t* const b = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(16F, 10, 10), 0);
+	ccv_nnc_tensor_t* const ga = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 16F, 100, 10), 0);
+	ccv_nnc_tensor_t* const gindices = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 32S, 10), 0);
+	ccv_nnc_tensor_t* const gb = ccv_nnc_tensor_new(0, GPU_TENSOR_NHWC(000, 16F, 10, 10), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(a16, indices), TENSOR_LIST(ga, gindices), 0);
+	ccv_nnc_cmd_exec(CMD_SCATTER_ADD_BACKWARD(100), ccv_nnc_no_hint, 0, TENSOR_LIST(ga, 0, gindices), TENSOR_LIST(gb), 0);
+	ccv_nnc_cmd_exec(CMD_DATA_TRANSFER_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(gb), TENSOR_LIST(b), 0);
+	ccv_nnc_tensor_t* const bt = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(16F, 10, 10), 0);
+	ccv_nnc_cmd_exec(CMD_SCATTER_ADD_BACKWARD(100), ccv_nnc_no_hint, 0, TENSOR_LIST(a16, 0, indices), TENSOR_LIST(bt), 0);
+	ccv_nnc_tensor_t* const b32 = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 10), 0);
+	ccv_nnc_tensor_t* const bt32 = ccv_nnc_tensor_new(0, CPU_TENSOR_NHWC(32F, 10, 10), 0);
+	ccv_nnc_cmd_exec(CMD_DATATYPE_CONVERSION_FORWARD(), ccv_nnc_no_hint, 0, TENSOR_LIST(b, bt), TENSOR_LIST(b32, bt32), 0);
+	REQUIRE_TENSOR_EQ(b32, bt32, "should be equal");
+	ccv_nnc_tensor_free(a);
+	ccv_nnc_tensor_free(indices);
+	ccv_nnc_tensor_free(b);
+	ccv_nnc_tensor_free(ga);
+	ccv_nnc_tensor_free(gindices);
+	ccv_nnc_tensor_free(gb);
+	ccv_nnc_tensor_free(a16);
+	ccv_nnc_tensor_free(bt);
+	ccv_nnc_tensor_free(b32);
+	ccv_nnc_tensor_free(bt32);
+}
+
+#include "case_main.h"
