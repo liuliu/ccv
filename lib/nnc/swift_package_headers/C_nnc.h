f6eb68981fb4df1c0b1e6141a734251c040f5aa9
weiyanlin117 <weiyanlin117@gmail.com>
2025-12-15 12:22:37 -0800

    another try...

--- lib/nnc/swift_package_headers/C_nnc.h
+++
@@ -1,4 +0,0 @@
-// Umbrella header for C_nnc module
-#include "../ccv_nnc.h"
-#include "../ccv_nnc_internal.h"
-#include "../ccv_nnc_easy.h"
---
+++ lib/nnc/swift_package_headers/nnc.h
@@ -0,0 +1,2 @@
+// Umbrella header for C_nnc module
+#include "../ccv_nnc.h"
