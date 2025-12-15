981d84a57b88fd47c77122966bfc4de78d973c70
weiyanlin117 <weiyanlin117@gmail.com>
2025-12-15 10:47:49 -0800

    update lib/nnc/swift_package_headers for ccv.h

--- lib/nnc/swift_package_headers/C_nnc.h
+++ lib/nnc/swift_package_headers/C_nnc.h
@@ -1,4 +1,6 @@
 // Umbrella header for C_nnc module
+// Include ccv.h first since ccv_nnc.h depends on it
+#include "../../ccv.h"
 #include "../ccv_nnc.h"
 #include "../ccv_nnc_internal.h"
 #include "../ccv_nnc_easy.h"
