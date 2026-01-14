0e0e856039ae8876c65da63363f13ebd664620c7
Liu Liu <i@liuliu.me>
2026-01-14 01:07:21 -0500

    Fix one more rmsnorm related issue.

--- lib/nnc/ccv_cnnp_model_addons.c
+++ lib/nnc/ccv_cnnp_model_addons.c
@@ -2518,17 +2518,28 @@ static void _ccv_cnnp_rmsnorm_build(ccv_cnnp_model_t* const super, ccv_nnc_symbo
 	for (i = 0; i < self->params.rmsnorm.count; i++)
 		scale_params.dim[self->params.rmsnorm.axis[i]] = params.dim[self->params.rmsnorm.axis[i]];
 	// Both scale and bias are shared between if this model is reused.
-	if (!self->scale.graph)
-		self->scale = ccv_nnc_tensor_symbol_new(graph, scale_params, "scale");
+	if (self->params.rmsnorm.elementwise_affine)
+	{
+		if (!self->scale.graph)
+			self->scale = ccv_nnc_tensor_symbol_new(graph, scale_params, "scale");
+	}
 	const ccv_nnc_cmd_t rmsnorm = ccv_nnc_cmd(CCV_NNC_RMSNORM_FORWARD, 0, self->params, 0);
 	ccv_nnc_tensor_param_t output_params[2];
-	ccv_nnc_hint_tensor_auto(rmsnorm, (ccv_nnc_tensor_param_t []){
-			params,
-			scale_params,
-		}, 2, ccv_nnc_no_hint, output_params, 2);
+	if (self->params.rmsnorm.elementwise_affine)
+		ccv_nnc_hint_tensor_auto(rmsnorm, (ccv_nnc_tensor_param_t []){
+				params,
+				scale_params,
+			}, 2, ccv_nnc_no_hint, output_params, 2);
+	else
+		ccv_nnc_hint_tensor_auto(rmsnorm, (ccv_nnc_tensor_param_t []){
+				params,
+			}, 1, ccv_nnc_no_hint, output_params, 2);
 	const ccv_nnc_tensor_symbol_t output = ccv_nnc_tensor_symbol_new(graph, output_params[0], 0);
 	const ccv_nnc_tensor_symbol_t saved_inv_std = ccv_nnc_tensor_symbol_new(graph, output_params[1], "saved_inv_std");
-	ccv_nnc_graph_exec_symbol_new(graph, rmsnorm, TENSOR_SYMBOL_LIST(inputs[0], self->scale), TENSOR_SYMBOL_LIST(output, saved_inv_std), "rmsnorm");
+	if (self->params.rmsnorm.elementwise_affine)
+		ccv_nnc_graph_exec_symbol_new(graph, rmsnorm, TENSOR_SYMBOL_LIST(inputs[0], self->scale), TENSOR_SYMBOL_LIST(output, saved_inv_std), "rmsnorm");
+	else
+		ccv_nnc_graph_exec_symbol_new(graph, rmsnorm, TENSOR_SYMBOL_LIST(inputs[0]), TENSOR_SYMBOL_LIST(output, saved_inv_std), "rmsnorm");
 	outputs[0] = output;
 }
 
