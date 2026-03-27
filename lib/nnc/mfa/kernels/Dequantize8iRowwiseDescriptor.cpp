1c72d95a79729ccbee2ab874ca816bc2673e92df
Liu Liu <i@liuliu.me>
2026-03-27 02:21:53 -0400

    Add new files.

---
+++ lib/nnc/mfa/kernels/Dequantize8iRowwiseDescriptor.cpp
@@ -0,0 +1,66 @@
+#include "Dequantize8iRowwiseDescriptor.hpp"
+#include "Dequantize8iRowwiseKernel.hpp"
+#include "../ccv_nnc_mfa_hash.hpp"
+#include "../ccv_nnc_mfa_error.hpp"
+
+bool Dequantize8iRowwiseDescriptor::operator==(const Dequantize8iRowwiseDescriptor& rhs) const {
+	return
+	memoryPrecision == rhs.memoryPrecision &&
+	rowLength == rhs.rowLength &&
+	length == rhs.length;
+}
+
+bool Dequantize8iRowwiseDescriptor::vectorized() const noexcept {
+	return (rowLength % 4) == 0 && (length % 4) == 0;
+}
+
+std::size_t std::hash<Dequantize8iRowwiseDescriptor>::operator()(const Dequantize8iRowwiseDescriptor& hash) const noexcept {
+	using namespace ccv::nnc::mfa::hash;
+	std::size_t seed = 0;
+	combine_64(seed, pack_64(simd::uint2 { (unsigned int)hash.memoryPrecision.value, hash.rowLength }));
+	combine_32(seed, hash.length);
+	return seed;
+}
+
+std::pair<Dequantize8iRowwiseKernelDescriptor, PipelineValue<Dequantize8iRowwiseKernel>*> Dequantize8iRowwiseDescriptor::findKernel(MTL::Device* const device, const DeviceProperties& dprops, NS::Array* const binaryArchivesToRead, MTL::BinaryArchive* const binaryArchiveToWrite, const std::string& pathToWrite, std::unordered_map<Dequantize8iRowwiseKernelDescriptor, std::unique_ptr<Dequantize8iRowwiseKernel>> *const libraryCache) const noexcept {
+	auto createKernel =
+	[=](Dequantize8iRowwiseKernelDescriptor descriptor) -> Dequantize8iRowwiseKernel* {
+		auto iterator = libraryCache->find(descriptor);
+		if (iterator != libraryCache->end()) {
+			return iterator->second.get();
+		} else {
+			Dequantize8iRowwiseKernel* kernel = new Dequantize8iRowwiseKernel(descriptor, rowLength, length, device);
+			(*libraryCache)[descriptor] = std::unique_ptr<Dequantize8iRowwiseKernel>(kernel);
+			return kernel;
+		}
+	};
+
+	Dequantize8iRowwiseKernelDescriptor kernelDesc;
+	kernelDesc.vectorized = vectorized() ? 1 : 0;
+	kernelDesc.memoryPrecision = memoryPrecision;
+
+	auto createPipeline =
+	[=](MTL::Library* library) -> MTL::ComputePipelineState* {
+		auto constants = NS::TransferPtr(MTL::FunctionConstantValues::alloc()->init());
+		const uint32_t rowUnits = vectorized() ? (rowLength / 4) : rowLength;
+		const uint32_t elementCount = vectorized() ? (length / 4) : length;
+		const uint32_t scaleOffset = (length + 127) & -128;
+		constants->setConstantValue(&rowUnits, MTL::DataTypeUInt, NS::UInteger(0));
+		constants->setConstantValue(&elementCount, MTL::DataTypeUInt, NS::UInteger(1));
+		constants->setConstantValue(&scaleOffset, MTL::DataTypeUInt, NS::UInteger(2));
+
+		NS::String* swiftName = NS::String::string("dequantize_8i_rowwise", NS::UTF8StringEncoding);
+		NS::Error* error = nil;
+		auto function = NS::TransferPtr(library->newFunction(swiftName, constants.get(), &error));
+		CCV_NNC_MFA_CHECK_ERROR(error);
+
+		auto pipeline = device->newComputePipelineState(function.get(), &error);
+		CCV_NNC_MFA_CHECK_ERROR(error);
+		return pipeline;
+	};
+
+	Dequantize8iRowwiseKernel* kernel = createKernel(kernelDesc);
+	auto pipeline = NS::TransferPtr(createPipeline(kernel->library.get()));
+	PipelineValue<Dequantize8iRowwiseKernel>* output = new PipelineValue<Dequantize8iRowwiseKernel> { kernel, pipeline };
+	return std::make_pair(kernelDesc, output);
+}
---
+++ lib/nnc/mfa/kernels/Dequantize8iRowwiseKernel.cpp
@@ -0,0 +1,100 @@
+#include "Dequantize8iRowwiseKernel.hpp"
+#include "../ccv_nnc_mfa.hpp"
+
+Dequantize8iRowwiseKernel::Dequantize8iRowwiseKernel(Dequantize8iRowwiseKernelDescriptor descriptor, uint32_t rowLength, uint32_t length, MTL::Device* const device) {
+	vectorized = descriptor.vectorized;
+	memoryPrecision = descriptor.memoryPrecision;
+	this->rowLength = rowLength;
+	this->length = length;
+
+	source = createSource();
+
+	threadgroupMemoryAllocation = createThreadgroupMemoryAllocation();
+	threadgroupSize = MTL::Size(256, 1, 1);
+
+	{
+		auto string = NS::String::string(source.c_str(), NS::UTF8StringEncoding);
+		NS::Error* error = nil;
+		library = NS::TransferPtr(device->newLibrary(string, nil, &error));
+		CCV_NNC_MFA_CHECK_ERROR(error);
+	}
+}
+
+MTL::Size Dequantize8iRowwiseKernel::gridSize() const noexcept {
+	const uint32_t elementCount = vectorized ? (length / 4) : length;
+	return MTL::Size((elementCount + 255) / 256, 1, 1);
+}
+
+unsigned short Dequantize8iRowwiseKernel::createThreadgroupMemoryAllocation() const noexcept {
+	return 0;
+}
+
+std::string Dequantize8iRowwiseKernel::createSource() const noexcept {
+	std::string shader = createConstants() + "\n";
+	if (vectorized) {
+		shader += R"(
+#include <metal_stdlib>
+using namespace metal;
+
+kernel void dequantize_8i_rowwise(
+  device const char4 *source [[buffer(0)]],
+  device real4 *destination [[buffer(1)]],
+
+  uint3 tgid [[threadgroup_position_in_grid]],
+  ushort lid [[thread_index_in_threadgroup]]
+) {
+  const uint x = tgid.x * threadgroup_size + lid;
+  if (x >= element_count)
+    return;
+  const uint row = x / row_units;
+  device const real *scales = (device const real*)((device const uchar*)source + scale_offset);
+  const real scale = scales[row];
+  const char4 q = source[x];
+  destination[x] = real4((real)q.x, (real)q.y, (real)q.z, (real)q.w) * scale;
+}
+		)";
+	} else {
+		shader += R"(
+#include <metal_stdlib>
+using namespace metal;
+
+kernel void dequantize_8i_rowwise(
+  device const char *source [[buffer(0)]],
+  device real *destination [[buffer(1)]],
+
+  uint3 tgid [[threadgroup_position_in_grid]],
+  ushort lid [[thread_index_in_threadgroup]]
+) {
+  const uint x = tgid.x * threadgroup_size + lid;
+  if (x >= element_count)
+    return;
+  const uint row = x / row_units;
+  device const real *scales = (device const real*)((device const uchar*)source + scale_offset);
+  destination[x] = (real)source[x] * scales[row];
+}
+		)";
+	}
+	return shader;
+}
+
+std::string Dequantize8iRowwiseKernel::createConstants() const noexcept {
+	std::string defines = "";
+	if (memoryPrecision == GEMMOperandPrecision::FP32) {
+		defines += "typedef float real;\n";
+		if (vectorized)
+			defines += "typedef float4 real4;\n";
+	} else if (memoryPrecision == GEMMOperandPrecision::BF16) {
+		defines += "typedef bfloat real;\n";
+		if (vectorized)
+			defines += "typedef bfloat4 real4;\n";
+	} else {
+		defines += "typedef half real;\n";
+		if (vectorized)
+			defines += "typedef half4 real4;\n";
+	}
+	defines += "constant ushort threadgroup_size = 256;\n";
+	defines += "constant uint row_units [[function_constant(0)]];\n";
+	defines += "constant uint element_count [[function_constant(1)]];\n";
+	defines += "constant uint scale_offset [[function_constant(2)]];\n";
+	return defines;
+}
